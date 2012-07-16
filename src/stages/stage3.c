622aef1f2ebb2ffe336b8688c8a62f43c0c57a99
laochailan <laochailan@web.de>
2012-07-16 08:10:54 +0200

    Added mansion entrance

--- src/stages/stage3.c
+++ src/stages/stage3.c
@@ -30,8 +30,40 @@ void stage3_fog(int fbonum) {
 	glUseProgram(0);
 }
 
+Vector **stage3_fountain_pos(Vector pos, float maxrange) {
+	Vector p = {0, 400, 1500};
+	Vector r = {0, 0, 3000};
+	
+	Vector **list = linear3dpos(pos, maxrange, p, r);
+	
+	int i;
+	
+	for(i = 0; list[i] != NULL; i++) {
+		if((*list[i])[2] > 0)
+			(*list[i])[2] = -9000;
+	}
+	
+	return list;
+}
+
+void stage3_fountain_draw(Vector pos) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, get_tex("stage1/border")->gltex);
+	
+	glPushMatrix();
+	glTranslatef(pos[0], pos[1], pos[2]);
+	glRotatef(-90, 1,0,0);
+	glScalef(1000,3010,1);
+		
+	draw_quad();
+	
+	glPopMatrix();
+	
+	glDisable(GL_TEXTURE_2D);
+}
+
 Vector **stage3_lake_pos(Vector pos, float maxrange) {
-	Vector p = {-60, 600, 0};
+	Vector p = {0, 600, 0};
 	Vector d;
 	
 	int i;
@@ -58,12 +90,23 @@ void stage3_lake_draw(Vector pos) {
 	glBindTexture(GL_TEXTURE_2D, get_tex("stage3/lake")->gltex);
 	
 	glPushMatrix();
-	glTranslatef(pos[0], pos[1], pos[2]);
+	glTranslatef(pos[0]-60, pos[1], pos[2]);
 	glScalef(20,20,20);
+		
 	glRotatef(180,1,0,0);
 	
 	draw_model("lake");
 	glPopMatrix();
+	
+	glPushMatrix();
+	glTranslatef(pos[0]+20, pos[1]+300, pos[2]+20);
+	glScalef(20,20,20);
+	
+	glBindTexture(GL_TEXTURE_2D, get_tex("stage3/mansion")->gltex);
+	
+	draw_model("mansion");
+	glPopMatrix();
+	
 	
 	glDisable(GL_TEXTURE_2D);
 }
@@ -71,11 +114,13 @@ void stage3_lake_draw(Vector pos) {
 void stage3_start() {
 	init_stage3d(&bgcontext);
 	
-	bgcontext.cx[2] = 120;
-	bgcontext.cv[1] = 0.1;
-	bgcontext.crot[0] = 90;
+	bgcontext.cx[2] = -10000;
+// 	bgcontext.cv[1] = 0.1;
+	bgcontext.cv[2] = 20;
+	bgcontext.crot[0] = 80;
 	
 	add_model(&bgcontext, stage3_lake_draw, stage3_lake_pos);
+	add_model(&bgcontext, stage3_fountain_draw, stage3_fountain_pos);
 }
 
 void stage3_end() {
@@ -86,6 +131,15 @@ void stage3_draw() {
 	set_perspective(&bgcontext, 200, 10000);
 	
 	draw_stage3d(&bgcontext, 7000);
+	
+	if(bgcontext.cx[2] >= -1000 && bgcontext.cv[2] > 0)
+		bgcontext.cv[2] -= 0.17;
+	
+	if(bgcontext.cx[1] < 100 && bgcontext.cv[2] < 0)
+		bgcontext.cv[2] = 0;
+	
+	if(bgcontext.cx[2] >= 0 && bgcontext.cx[2] <= 10)
+		bgcontext.cv[1] += 0.1;
 }
 
 void stage3_loop() {
