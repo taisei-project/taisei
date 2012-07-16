9d26e50d2b7eefa141c899bb9035a3fb3764d216
laochailan <laochailan@web.de>
2012-07-16 18:13:58 +0200

    Added Stage 4 BG

--- src/stages/stage3.c
+++ src/stages/stage3.c
@@ -19,8 +19,8 @@ void stage3_fog(int fbonum) {
 	glUseProgram(shader->prog);
 	glUniform1i(uniloc(shader, "depth"),2);
 	glUniform4f(uniloc(shader, "fog_color"),0,0,0.1,1.0);
-	glUniform1f(uniloc(shader, "start"),0.2);
-	glUniform1f(uniloc(shader, "end"),0.4);
+	glUniform1f(uniloc(shader, "start"),0.4);
+	glUniform1f(uniloc(shader, "end"),0.8);
 	glUniform1f(uniloc(shader, "exponent"),4.0);
 	glActiveTexture(GL_TEXTURE0 + 2);
 	glBindTexture(GL_TEXTURE_2D, resources.fbg[fbonum].depth);
@@ -90,17 +90,17 @@ void stage3_lake_draw(Vector pos) {
 	glBindTexture(GL_TEXTURE_2D, get_tex("stage3/lake")->gltex);
 	
 	glPushMatrix();
-	glTranslatef(pos[0]-60, pos[1], pos[2]);
-	glScalef(20,20,20);
+	glTranslatef(pos[0], pos[1]+140, pos[2]);
+	glScalef(15,15,15);
 		
-	glRotatef(180,1,0,0);
+// 	glRotatef(180,1,0,0);
 	
 	draw_model("lake");
 	glPopMatrix();
 	
 	glPushMatrix();
-	glTranslatef(pos[0]+20, pos[1]+300, pos[2]+20);
-	glScalef(20,20,20);
+	glTranslatef(pos[0], pos[1]+944, pos[2]+50);
+	glScalef(30,30,30);
 	
 	glBindTexture(GL_TEXTURE_2D, get_tex("stage3/mansion")->gltex);
 	
@@ -111,16 +111,93 @@ void stage3_lake_draw(Vector pos) {
 	glDisable(GL_TEXTURE_2D);
 }
 
+Vector **stage3_corridor_pos(Vector pos, float maxrange) {
+	Vector p = {0, 2400, 50};
+	Vector r = {0, 2000, 0};
+	
+	Vector **list = linear3dpos(pos, maxrange, p, r);
+	
+	int i;
+	
+	for(i = 0; list[i] != NULL; i++) {
+		if((*list[i])[1] < p[1])
+			(*list[i])[1] = -9000;
+	}
+	
+	return list;
+}
+
+void stage3_corridor_draw(Vector pos) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, get_tex("stage3/planks")->gltex);
+	
+	glMatrixMode(GL_TEXTURE);
+	glScalef(1,2,1);
+	glMatrixMode(GL_MODELVIEW);
+	
+	glPushMatrix();
+	glTranslatef(pos[0], pos[1], pos[2]);
+	
+	glPushMatrix();
+	glRotatef(180, 1,0,0);
+	glScalef(300,2000,1);
+	
+	draw_quad();
+	glPopMatrix();
+		
+	glBindTexture(GL_TEXTURE_2D, get_tex("stage3/wall")->gltex);
+	
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	glRotatef(90,0,0,1);
+	glScalef(1,10,1);
+	glMatrixMode(GL_MODELVIEW);
+	
+	glPushMatrix();
+	glTranslatef(100,5,75);
+	glRotatef(90, 0,1,0);
+	glScalef(150,2000,1);
+	draw_quad();
+	glPopMatrix();
+	
+	glPushMatrix();
+	glTranslatef(-100,5,75);
+	glRotatef(180,1,0,0);
+	glRotatef(-90, 0,1,0);
+	glScalef(150,2000,1);
+	draw_quad();
+	glPopMatrix();
+	
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	
+	glDisable(GL_TEXTURE_2D);
+	
+	glColor3f(0.01,0.01,0.01);
+	glPushMatrix();
+	glTranslatef(0,0,150);
+	glScalef(500,2000,1);
+	draw_quad();
+	glPopMatrix();
+	
+	glPopMatrix();
+	glColor3f(1,1,1);
+}
+
 void stage3_start() {
 	init_stage3d(&bgcontext);
 	
-	bgcontext.cx[2] = -10000;
+	bgcontext.cx[2] = -5000;
 // 	bgcontext.cv[1] = 0.1;
-	bgcontext.cv[2] = 20;
+	bgcontext.cv[2] = 19.7;
 	bgcontext.crot[0] = 80;
+	
+// 	bgcontext.projangle = 30;
 	
 	add_model(&bgcontext, stage3_lake_draw, stage3_lake_pos);
 	add_model(&bgcontext, stage3_fountain_draw, stage3_fountain_pos);
+	add_model(&bgcontext, stage3_corridor_draw, stage3_corridor_pos);
 }
 
 void stage3_end() {
@@ -128,9 +205,9 @@ void stage3_end() {
 }
 
 void stage3_draw() {
-	set_perspective(&bgcontext, 200, 10000);
+	set_perspective(&bgcontext, 130, 3000);
 	
-	draw_stage3d(&bgcontext, 7000);
+	draw_stage3d(&bgcontext, 4000);
 	
 	if(bgcontext.cx[2] >= -1000 && bgcontext.cv[2] > 0)
 		bgcontext.cv[2] -= 0.17;
@@ -139,7 +216,10 @@ void stage3_draw() {
 		bgcontext.cv[2] = 0;
 	
 	if(bgcontext.cx[2] >= 0 && bgcontext.cx[2] <= 10)
-		bgcontext.cv[1] += 0.1;
+		bgcontext.cv[1] += 0.5;
+	
+	if(bgcontext.cx[1] >= 1200 && bgcontext.cx[1] <= 1500)
+		bgcontext.cv[1] += 0.02;
 }
 
 void stage3_loop() {
