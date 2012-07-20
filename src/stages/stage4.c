a0e0ba28f8f595446c372dd09835b7d18083e44b
laochailan <laochailan@web.de>
2012-07-20 20:43:48 +0200

    initial stage 5 bg

--- src/stages/stage4.c
+++ src/stages/stage4.c
@@ -15,12 +15,49 @@
 
 static Stage3D bgcontext;
 
+Vector **stage4_stairs_pos(Vector pos, float maxrange) {
+	Vector p = {0, 0, 0};
+	Vector r = {0, 0, 3000};
+	
+	return linear3dpos(pos, maxrange, p, r);
+}
+
+void stage4_stairs_draw(Vector pos) {
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, get_tex("stage4/tower")->gltex);
+	
+	glPushMatrix();
+	glTranslatef(pos[0], pos[1], pos[2]);
+	glScalef(300,300,300);
+	draw_model("tower");
+	
+	glPopMatrix();
+	
+	glDisable(GL_TEXTURE_2D);
+}
+
 void stage4_draw() {
+	set_perspective(&bgcontext, 100, 9000);
+	
 	draw_stage3d(&bgcontext, 7000);
+	float w = 0.005;
+	
+	bgcontext.cx[0] = 2500*cos(-w*global.frames);
+	bgcontext.cx[1] = 2500*sin(-w*global.frames);
+	bgcontext.cx[2] = w*3000/(M_PI)*global.frames;
+	
+// 	bgcontext.cv[2] = w*3000/(2*M_PI);
+	
+// 	bgcontext.crot[0] = cimag(global.plr.pos);
+	bgcontext.crot[2] = 150-180/M_PI*w*global.frames;
 }
 
 void stage4_start() {
 	init_stage3d(&bgcontext);
+	add_model(&bgcontext, stage4_stairs_draw, stage4_stairs_pos);
+	
+	bgcontext.crot[0] = 60;
+	
 }
 
 void stage4_end() {
