7e396cde4a37cf9d0c9a3c490519df6f4fe4c977
laochailan <laochailan@web.de>
2012-07-26 16:50:06 +0200

    removed debug code

--- src/stages/stage4_events.c
+++ src/stages/stage4_events.c
@@ -16,6 +16,5 @@ void stage4_events() {
 	TIMER(&global.timer);
 	
 	FROM_TO(0, 500, 200)
-// 		create_projectile2c("bullet", VIEWPORT_W/2, rgb(0.3,0,1), asymptotic, 2I+1-2*frand(), 5);
-		create_lasercurve1c(VIEWPORT_W/2 + VIEWPORT_H/2*I, 20, 50, rgb(0.3,1,1), las_linear, 3*cexp(2I*M_PI*frand()));
+		create_projectile2c("bullet", VIEWPORT_W/2, rgb(0.3,0,1), asymptotic, 2I+1-2*frand(), 5);
 }
