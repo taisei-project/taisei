c5eba2156d401b3f54afc991486c85f124ac42df
laochailan <laochailan@web.de>
2012-07-28 19:45:51 +0200

    enemy.unbombable, bombs hurt bosses

--- src/enemy.h
+++ src/enemy.h
@@ -40,6 +40,7 @@ struct Enemy {
 	EnemyDrawRule draw_rule;
 	
 	int hp;
+	int unbombable;
 	
 	complex args[RULE_ARGC];
 };
@@ -49,7 +50,7 @@ struct Enemy {
 #define create_enemy2c(p,h,d,l,a1,a2) create_enemy_p(&global.enemies,p,h,d,l,a1,a2,0,0)
 #define create_enemy1c(p,h,d,l,a1) create_enemy_p(&global.enemies,p,h,d,l,a1,0,0,0)
 
-void create_enemy_p(Enemy **enemies, complex pos, int hp, EnemyDrawRule draw_rule, EnemyLogicRule logic_rule,
+Enemy *create_enemy_p(Enemy **enemies, complex pos, int hp, EnemyDrawRule draw_rule, EnemyLogicRule logic_rule,
 				    complex a1, complex a2, complex a3, complex a4);
 void delete_enemy(Enemy **enemies, Enemy* enemy);
 void draw_enemies(Enemy *enemies);
