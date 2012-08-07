4d46a60ba220913f6156da85ba01ef983edea6b8
laochailan <laochailan@web.de>
2012-08-07 22:29:48 +0200

    removed debug logging

--- src/stage.c
+++ src/stage.c
@@ -538,9 +538,7 @@ void stage_loop(StageInfo* info, StageRule start, StageRule end, StageRule draw,
 		tsrand_switch(&global.rand_visual);
 		stage_draw(info, draw, shaderrules, endtime);
 		tsrand_switch(&global.rand_game);
-		
-		print_state_checksum();
-		
+				
 		SDL_GL_SwapBuffers();
 		frame_rate(&global.lasttime);
 	}
