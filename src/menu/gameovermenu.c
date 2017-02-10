808258ad681929d8fb041799a00fc2996c85ef61
Andrei "Akari" Alexeyev <akari@alienslab.net>
2017-02-10 23:07:57 +0200

    Fixed a replay segfault after using a continue

--- src/menu/gameovermenu.c
+++ src/menu/gameovermenu.c
@@ -1,6 +1,6 @@
 /*
  * This software is licensed under the terms of the MIT-License
- * See COPYING for further information. 
+ * See COPYING for further information.
  * ---
  * Copyright (C) 2011, Lukas Weber <laochailan@web.de>
  * Copyright (C) 2011, Alexeyew Andrew <http://akari.thebadasschoobs.org/>
@@ -13,15 +13,17 @@
 void continue_game(void *arg)
 {
 	printf("The game is being continued...\n");
-	
-	if(global.replaymode == REPLAY_RECORD)	// actually... I'd be strange if REPLAY_PLAY ever got there
-		replay_destroy(&global.replay);		// 19:39:29 [@  laochailan] no. no fame for continue users >:D
-		
+
+	if(global.replaymode == REPLAY_RECORD) { // actually... I'd be strange if REPLAY_PLAY ever got there
+		replay_destroy(&global.replay);      // 19:39:29 [@  laochailan] no. no fame for continue users >:D
+		global.replay_stage = NULL;
+	}
+
 	global.plr.lifes = PLR_START_LIVES;
 	global.plr.continues += 1;
 	global.plr.focus = 0;
 	global.plr.fire = 0;
-	
+
 	delete_projectiles(&global.projs);
 	delete_projectiles(&global.particles);
 }
@@ -34,18 +36,18 @@ void restart_game(void *arg);
 
 void create_gameover_menu(MenuData *m) {
 	create_menu(m);
-	
+
 	m->flags = MF_Transient | MF_AlwaysProcessInput;
 	m->transition = NULL;
 	m->context = "Game Over";
-	
+
 	char s[64];
 	int c = MAX_CONTINUES - global.plr.continues;
 	snprintf(s, sizeof(s), "Continue (%i)", c);
 	add_menu_entry(m, s, c? continue_game : NULL, NULL);
 	add_menu_entry(m, "Restart the Game", restart_game, NULL)->transition = TransFadeBlack;
 	add_menu_entry(m, c? "Give up" : "Return to Title", give_up, NULL)->transition = TransFadeBlack;
-	
+
 	if(!c)
 		m->cursor = 1;
 }
