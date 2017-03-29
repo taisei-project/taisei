9f95b4ce910b9dea91bcd7f315f7d15bfe2280bf
Andrei "Akari" Alexeyev <akari@alienslab.net>
2017-03-29 23:14:04 +0300

    progress: preserve unknown commands in the file as-is for forwards compatibility

--- src/progress.h
+++ src/progress.h
@@ -11,6 +11,8 @@
 #include <stdbool.h>
 #include <SDL.h>
 
+#include "list.h"
+
 #define PROGRESS_FILENAME "progress.dat"
 #define PROGRESS_MAXFILESIZE 4096
 
@@ -33,13 +35,17 @@ typedef struct StageProgress {
 	// short num_cleared;
 } StageProgress;
 
+struct UnknownCmd;
+
 typedef struct GlobalProgress {
     uint32_t hiscore;
+    struct UnknownCmd *unknown;
 } GlobalProgress;
 
 extern GlobalProgress progress;
 
 void progress_load(void);
 void progress_save(void);
+void progress_unload(void);
 
 #endif
