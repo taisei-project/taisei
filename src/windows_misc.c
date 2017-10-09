0dbd045a31dd1d692bcb23cddec00f9a2ef98cab
Andrei Alexeyev <0x416b617269@gmail.com>
2017-10-09 10:00:59 +0300

    windows: prefer discrete GPU on hybrid-graphics systems

---
+++ src/windows_misc.c
@@ -0,0 +1,25 @@
+/*
+ * This software is licensed under the terms of the MIT-License
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2017, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2017, Andrei Alexeyev <akari@alienslab.net>.
+ */
+
+#include <windows.h>
+
+/*
+ *  This is here for Windows laptops with hybrid graphics.
+ *  We tell the driver to prefer the fast GPU over the integrated one by default.
+ */
+
+// Nvidia:
+// http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+
+// AMD:
+// https://community.amd.com/thread/169965
+// https://stackoverflow.com/questions/17458803/amd-equivalent-to-nvoptimusenablement
+
+__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001;
