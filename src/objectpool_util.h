bae77d38e8ac75b706189e0b8922e963410ead75
Andrei Alexeyev <0x416b617269@gmail.com>
2017-12-20 20:53:09 +0200

    run update-copyright.py

--- src/objectpool_util.h
+++ src/objectpool_util.h
@@ -1,3 +1,10 @@
+/*
+ * This software is licensed under the terms of the MIT-License
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2017, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2017, Andrei Alexeyev <akari@alienslab.net>.
+ */
 
 #pragma once
 
