b7a9192355f189b0895cbdcb76390c7b77be4c68
Andrei Alexeyev <0x416b617269@gmail.com>
2017-12-21 07:09:22 +0200

    add helper script to set up build directory appropriate for development

---
+++ scripts/setup-devel-build-dir.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dst="$PWD"
+cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit $?
+src="$PWD"
+
+exec meson setup \
+    --buildtype=debug \
+    --prefix="$PWD/install" \
+    -Dstrip=false \
+    -Db_lto=false \
+    -Db_ndebug=false \
+    -Db_sanitize=address,undefined \
+    "$src" "$dst" "$@"
