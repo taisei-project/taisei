2a05d871dd24395f24ee04fd2cfdf11edb3a2404
Andrei Alexeyev <0x416b617269@gmail.com>
2018-01-21 17:59:53 +0200

    fix some lasers calling get_shader instead of get_shader_optional

--- src/stages/stage6_events.c
+++ src/stages/stage6_events.c
@@ -633,7 +633,7 @@ void elly_frequency2(Boss *b, int t) {
 complex maxwell_laser(Laser *l, float t) {
 	if(t == EVENT_BIRTH) {
 		l->unclearable = true;
-		l->shader = get_shader("laser_maxwell");
+		l->shader = get_shader_optional("laser_maxwell");
 		return 0;
 	}
 
