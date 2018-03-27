79dd5e31c54e39b30de847aa92041e1c54a11c96
laochailan <laochailan@web.de>
2018-03-27 18:44:35 +0200

    tower wall fix?

--- resources/shader/tower_wall.frag.glsl
+++ resources/shader/tower_wall.frag.glsl
@@ -9,5 +9,5 @@ out vec4 fragColor;
 void main(void) {
 	vec4 texel = texture2D(tex, texCoord);
 	float f = min(1.0, length(posRaw)/3000.0);
-	gl_FragColor = mix(vec4(1.0), texel, 1.0-f);
+	fragColor = mix(vec4(1.0), texel, 1.0-f);
 }
