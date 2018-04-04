3d29656530aadb0e40e4613b78075ec8c917d684
Andrei Alexeyev <0x416b617269@gmail.com>
2018-04-04 08:02:59 +0300

    revert temporary gl33_debug change

--- src/renderer/gl33/debug.c
+++ src/renderer/gl33/debug.c
@@ -52,10 +52,6 @@ static void APIENTRY gl33_debug(
 		case GL_DEBUG_SEVERITY_NOTIFICATION:    strsev  = "notify";                        break;
 	}
 
-	if(source == GL_DEBUG_SOURCE_SHADER_COMPILER && lvl1 == LOG_FATAL) {
-		lvl1 = LOG_WARN;
-	}
-
 	lvl = lvl1 > lvl0 ? lvl1 : lvl0;
 
 	if(source == GL_DEBUG_SOURCE_SHADER_COMPILER && type == GL_DEBUG_TYPE_ERROR) {
