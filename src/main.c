5b9dc583e2ed9013c0ab4213b3ca4b44e1d32b6b
Andrei Alexeyev <0x416b617269@gmail.com>
2018-04-08 04:24:50 +0300

    Comment out the extra verbose SDL logging for now

--- src/main.c
+++ src/main.c
@@ -127,9 +127,11 @@ static int run_tests(void) {
 	return 0;
 }
 
+/*
 void sdl_log(void *userdata, int category, SDL_LogPriority priority, const char *message) {
 	log_debug("[%i %i] %s", category, priority, message);
 }
+*/
 
 static void init_sdl(void) {
 	SDL_version v;
@@ -137,8 +139,11 @@ static void init_sdl(void) {
 	if(SDL_Init(SDL_INIT_EVENTS) < 0)
 		log_fatal("SDL_Init() failed: %s", SDL_GetError());
 
+	/*
+	 * TODO: refine this and make it optional
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
 	SDL_LogSetOutputFunction(sdl_log, NULL);
+	*/
 
 	log_info("SDL initialized");
 
