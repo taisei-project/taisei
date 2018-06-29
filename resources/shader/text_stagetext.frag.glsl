9072ab39656275700a9d610f86e3396a9bf637c3
laochailan <laochailan@web.de>
2018-06-29 22:56:43 +0200

    small improvement for stagetext

--- resources/shader/text_stagetext.frag.glsl
+++ resources/shader/text_stagetext.frag.glsl
@@ -7,7 +7,7 @@
 UNIFORM(1) sampler2D trans;
 
 float tc_mask(vec2 tc) {
-	return float(tc.x >= 0 && tc.x <= 1 && tc.y >= 0 && tc.y <= 1);
+    return float(tc.x >= 0 && tc.x <= 1 && tc.y >= 0 && tc.y <= 1);
 }
 
 void main(void) {
@@ -33,5 +33,7 @@ void main(void) {
 
     fragColor = textfrag;
     fragColor = mix(shadowfrag, textfrag, sqrt(textfrag.a));
+
+    tc_overlay = clamp(tc_overlay,0.01,0.99); // The overlay coordinates are outside of [0,1] in the padding region, so we make sure there are no wrap around artifacts when a bit of text is distorted to this region.
     fragColor.a *= clamp((texture(trans, tc_overlay).r + 0.5) * 2.5 * t-0.5, 0.0, 1.0);
 }
