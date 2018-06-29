2086704dc87d5cfbed120889331c7783abc2ed5a
Andrei Alexeyev <0x416b617269@gmail.com>
2018-06-29 23:15:50 +0300

    make the stagetext shader look somewhat better

--- resources/shader/text_stagetext.frag.glsl
+++ resources/shader/text_stagetext.frag.glsl
@@ -6,27 +6,32 @@
 
 UNIFORM(1) sampler2D trans;
 
+float tc_mask(vec2 tc) {
+	return float(tc.x >= 0 && tc.x <= 1 && tc.y >= 0 && tc.y <= 1);
+}
+
 void main(void) {
+    float t = customParam;
     vec2 tc = texCoord;
     vec2 tc_overlay = texCoordOverlay;
+    vec2 f = tc_overlay - vec2(0.5);
 
-    float t = customParam;
-
-    vec2 f = tc_overlay-vec2(0.5,0.5);
     tc *= dimensions;
-    tc += 10.*(f*sin(10.0*(length(f)+t)))*(1.0-t);
+    tc += 5 * f * sin(10 * (length(f) + t)) * (1 - t);
     tc /= dimensions;
-    float a = float(tc.x >= 0 && tc.x <= 1 && tc.y >= 0 && tc.y <= 1);
 
-    tc_overlay = clamp(tc_overlay,0.01,0.99);
-    vec4 texel = texture(tex, uv_to_region(texRegion,tc));
-    vec4 textfrag = vec4(color.rgb,a*color.a*texel.r);
+    float a = tc_mask(tc);
 
-    tc -= vec2(1.)/dimensions;
-    a = float(tc.x >= 0 && tc.x <= 1 && tc.y >= 0 && tc.y <= 1);
-    texel = texture(tex, uv_to_region(texRegion,tc));
-    vec4 shadowfrag = vec4(0.,0.,0.,a*texel.r);
+    vec4 texel = texture(tex, uv_to_region(texRegion, tc));
+    vec4 textfrag = vec4(color.rgb, a * color.a * texel.r);
 
-    fragColor = mix(shadowfrag,textfrag,sqrt(textfrag.a));
-    fragColor.a *= clamp((texture(trans, tc_overlay).r+0.5)*2.5*t-0.5, 0.0, 1.0);
+    tc -= vec2(1) / dimensions;
+    a = tc_mask(tc);
+
+    texel = texture(tex, uv_to_region(texRegion, tc));
+    vec4 shadowfrag = vec4(vec3(0), a * color.a * texel.r);
+
+    fragColor = textfrag;
+    fragColor = mix(shadowfrag, textfrag, sqrt(textfrag.a));
+    fragColor.a *= clamp((texture(trans, tc_overlay).r + 0.5) * 2.5 * t-0.5, 0.0, 1.0);
 }
