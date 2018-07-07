c48d017f581d7ed29d43e1101525dd5136aac253
laochailan <laochailan@web.de>
2018-07-07 13:44:16 +0200

    rewrite the YoumuA bomb shader to be more human readable

--- resources/shader/youmua_bomb.frag.glsl
+++ resources/shader/youmua_bomb.frag.glsl
@@ -9,13 +9,18 @@ UNIFORM(1) float tbomb;
 void main(void) {
 	vec2 pos = texCoordRaw;
 
-	float shift = (2.*float(pos.x<0.5)-1.)*0.1*sqrt(tbomb);
-	float f = step(-pos.y*pos.y-abs(pos.x-0.5)+tbomb*(tbomb-0.8)*10.,0.);
-	vec3 clr0 = texture(tex, texCoord).rgb;
-	pos.y = pos.y*(1.0-abs(shift))+float(shift>0.)*shift;
-	vec3 clr = texture(tex, vec2(r_textureMatrix*vec4(pos,0,1))).rgb;
+	// During the end of the bomb, the screen splits and the originalColor is revealed again
+	float inSplitArea = step(-pos.y * pos.y - abs(pos.x - 0.5) + tbomb * (tbomb-0.8) * 10, 0);
+	vec3 originalColor = texture(tex, texCoord).rgb;
 
-	clr = (1.-f)*clr0 + f*(atan((clr-vec3(+shift,0.01*tbomb,-shift))*(1.+4.*tbomb*(1.-pow(abs(pos.x-0.5),3.))))/0.8);
+	// In the remaining area, the left and right half get shifted up/down
+	float shift = (2 * float(pos.x<0.5) - 1) * 0.1 * sqrt(tbomb);
+	pos.y = pos.y * (1 - abs(shift)) + float(shift > 0) * shift;
+	vec3 shiftedColor = texture(tex, vec2(r_textureMatrix * vec4(pos, 0, 1))).rgb;
 
-	fragColor = vec4(clr,1.0);
+	shiftedColor -= vec3(+shift,0.01*tbomb,-shift); // red/blue shift
+	shiftedColor *= 1 + 4 * tbomb * (1 - pow(abs(pos.x - 0.5), 3)); // boost intensity
+
+	// put everything together applying a tanh against overflows.
+	fragColor = vec4(mix(originalColor, tanh(shiftedColor), inSplitArea), 1);
 }
