569ab39fac8c60c165e8378f6ea9623107c7422c
Andrei Alexeyev <0x416b617269@gmail.com>
2018-08-25 07:10:00 +0300

    unflip: fix spellcard_walloftext, text_hud

--- resources/shader/spellcard_walloftext.frag.glsl
+++ resources/shader/spellcard_walloftext.frag.glsl
@@ -6,13 +6,13 @@
 void main(void) {
 	float n = 10; // rough number of rings of text
 
-	vec2 pos = (vec2(texCoordRaw) - origin * vec2(ratio, 1.0)) * n;
+	vec2 pos = (vec2(flip_native_to_topleft(texCoordRaw)) - origin * vec2(ratio, 1.0)) * n;
 	pos.x /= ratio;
 
 	// Slightly distorted mapping to polar coordinates. I wrote this
 	// shortly after hearing a general relativity lecture...
-	
-	pos = vec2(atan(pos.x,pos.y), length(pos) + n * t * 0.1);
+
+	pos = vec2(atan(pos.x, pos.y), length(pos) + n * t * 0.1);
 
 	// The idea is (apart from the aspect factors) to map the
 	// (phi, angle) coordinates to something that loops several
@@ -22,14 +22,15 @@ void main(void) {
 	// Simplified example:
 	// f(x) = x for 0 < x < 2
 	// mod(f(x),1) then goes from 0 to 1 and from 0 to 1 again.
-	// 
+	//
 	// If you use it as a texcoord you get two copies of the texture.
 	//
 	// The complicated example I don’t understand either:
-	
+
 	pos.x *= h/w;
 	pos.x += t * 0.5 * float(2 * int(mod(pos.y, 2)) - 1);
 	pos = mod(pos, vec2(1 + 0.01 / w, 1));
+	pos = flip_topleft_to_native(pos);
 	pos *= vec2(w,h);
 
 	vec4 texel = texture(tex, pos);
--- resources/shader/text_hud.frag.glsl
+++ resources/shader/text_hud.frag.glsl
@@ -1,11 +1,12 @@
 #version 330 core
 
 #include "lib/render_context.glslh"
+#include "lib/util.glslh"
 #include "interface/sprite.glslh"
 
 void main(void) {
 	vec4 texel = texture(tex, texCoord);
-	float gradient = mix(1.0, 0.8, texCoordOverlay.y);
+	float gradient = 0.8 + 0.2 * flip_native_to_bottomleft(texCoordOverlay.y);
 	fragColor = color * texel.r * gradient;
 	fragColor.rgb *= gradient;
 }
