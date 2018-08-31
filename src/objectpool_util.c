3cd4ecfcebf93b757340cf2814aa81a549348dd0
Andrei Alexeyev <0x416b617269@gmail.com>
2018-08-31 06:23:29 +0300

    Remove the obsolete objpool_release_[a]list functions

--- src/objectpool_util.c
+++ src/objectpool_util.c
@@ -11,28 +11,6 @@
 #include "objectpool_util.h"
 #include "util.h"
 
-static void* objpool_release_list_callback(List **dest, List *elem, void *vpool) {
-	list_unlink(dest, elem);
-	objpool_release((ObjectPool*)vpool, (ObjectInterface*)elem);
-	return NULL;
-}
-
-static void* objpool_release_alist_callback(ListAnchor *list, List *elem, void *vpool) {
-	alist_unlink(list, elem);
-	objpool_release((ObjectPool*)vpool, (ObjectInterface*)elem);
-	return NULL;
-}
-
-#undef objpool_release_list
-void objpool_release_list(ObjectPool *pool, List **dest) {
-	list_foreach(dest, objpool_release_list_callback, pool);
-}
-
-#undef objpool_release_alist
-void objpool_release_alist(ObjectPool *pool, ListAnchor *list) {
-	alist_foreach(list, objpool_release_alist_callback, pool);
-}
-
 bool objpool_is_full(ObjectPool *pool) {
 	ObjectPoolStats stats;
 	objpool_get_stats(pool, &stats);
