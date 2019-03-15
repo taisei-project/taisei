b9766fcf591b7b06f1067d0f456038a696780610
Andrei Alexeyev <0x416b617269@gmail.com>
2019-03-15 22:19:49 +0200

    music room improvements: allow playing locked tracks with confirmation

--- src/menu/musicroom.c
+++ src/menu/musicroom.c
@@ -19,9 +19,60 @@
 #include "renderer/api.h"
 #include "video.h"
 
+enum {
+	MSTATE_UNLOCKED = 1,
+	MSTATE_CONFIRM = 2,
+	MSTATE_PLAYING = 4,
+
+	MSTATE_COMMENT_BG_VISIBLE = MSTATE_UNLOCKED | MSTATE_CONFIRM | MSTATE_PLAYING,
+	MSTATE_COMMENT_VISIBLE = MSTATE_UNLOCKED | MSTATE_PLAYING,
+	MSTATE_TITLE_VISIBLE = MSTATE_UNLOCKED | MSTATE_PLAYING,
+};
+
+typedef struct MusicEntryParam {
+	MusicMetadata *bgm_meta;
+	const char *bgm;
+	ShaderProgram *text_shader;
+	uint8_t state;
+} MusicEntryParam;
+
 static void musicroom_logic(MenuData *m) {
+	float prev_selector_x = m->drawdata[0];
+	float prev_selector_w = m->drawdata[1];
 	animate_menu_list(m);
-	fapproach_asymptotic_p(&m->drawdata[3], m->entries[m->cursor].arg != NULL, 0.2, 1e-5);
+
+	if(m->entries[m->cursor].arg) {
+		MusicEntryParam *p = m->entries[m->cursor].arg;
+		float selector_w = SCREEN_W - 200;
+		m->drawdata[0] = prev_selector_x;
+		m->drawdata[1] = prev_selector_w;
+		fapproach_asymptotic_p(&m->drawdata[0], 0.5 * selector_w, 0.1, 1e-5);
+		fapproach_asymptotic_p(&m->drawdata[1], 2 * selector_w, 0.1, 1e-5);
+		fapproach_asymptotic_p(&m->drawdata[3], (p->state & MSTATE_COMMENT_BG_VISIBLE) != 0, 0.2, 1e-5);
+	} else {
+		fapproach_asymptotic_p(&m->drawdata[3], 0, 0.2, 1e-5);
+	}
+
+	for(int i = 0; i < m->ecount; ++i) {
+		MenuEntry *e = m->entries + i;
+		MusicEntryParam *p = e->arg;
+
+		if(p) {
+			if(i != m->cursor && e->drawdata / 10.0 <= 0.02) {
+				p->state &= ~MSTATE_CONFIRM;
+			}
+
+			if(
+				current_bgm.music &&
+				current_bgm.music->meta &&
+				current_bgm.music->meta == p->bgm_meta
+			) {
+				p->state |= MSTATE_PLAYING;
+			} else {
+				p->state &= ~MSTATE_PLAYING;
+			}
+		}
+	}
 }
 
 static void musicroom_draw_item(MenuEntry *e, int i, int cnt) {
@@ -29,22 +80,52 @@ static void musicroom_draw_item(MenuEntry *e, int i, int cnt) {
 		return;
 	}
 
-	text_draw(e->name, &(TextParams) {
-		.pos = { 20 - e->drawdata, 20 * i },
-		.shader = "text_default",
-	});
+	MusicEntryParam *p = e->arg;
 
-	if(
-		e->arg &&
-		current_bgm.music &&
-		current_bgm.music->meta &&
-		current_bgm.music->meta == get_resource_data(RES_BGM_METADATA, e->arg, RESF_OPTIONAL)
-	) {
+	if(!p) {
+		text_draw(e->name, &(TextParams) {
+			.pos = { 20 - e->drawdata, 20 * i },
+			.shader = "text_default",
+		});
+
+		return;
+	}
+
+	char buf[8];
+	const char *title = p->state & MSTATE_TITLE_VISIBLE ? e->name : "???????";
+	Color clr = *r_color_current();
+	TextParams tparams = {
+		.pos = { 20 - e->drawdata, 20 * i },
+		.shader_ptr = p->text_shader,
+		.font_ptr = get_font("standard"),
+		.color = &clr,
+	};
+	bool kerning_saved = font_get_kerning_enabled(tparams.font_ptr);
+
+	tparams.pos.x += text_draw("№ ", &tparams);
+	font_set_kerning_enabled(tparams.font_ptr, false);
+	snprintf(buf, sizeof(buf), " %i  ", i + 1);
+	tparams.pos.x += text_width(tparams.font_ptr, " 99  ", 0);
+	tparams.align = ALIGN_RIGHT;
+	tparams.pos.x += text_draw(buf, &tparams);
+	tparams.align = ALIGN_LEFT;
+	font_set_kerning_enabled(tparams.font_ptr, kerning_saved);
+
+	if(!(p->state & MSTATE_TITLE_VISIBLE)) {
+		clr.r *= 0.5;
+		clr.g *= 0.5;
+		clr.b *= 0.5;
+	}
+
+	tparams.pos.x += text_draw(title, &tparams);
+
+	if(p->state & MSTATE_PLAYING) {
+		color_mul(&clr, RGBA(0.1, 0.6, 0.8, 0.8));
 		text_draw("Now playing", &(TextParams) {
 			.pos = { SCREEN_W - 200, 20 * i },
-			.shader = "text_default",
+			.shader_ptr = p->text_shader,
 			.align = ALIGN_RIGHT,
-			.color = RGBA(0.1, 0.6, 0.8, 0.8),
+			.color = &clr,
 		});
 	}
 }
@@ -82,22 +163,34 @@ static void musicroom_draw(MenuData *m) {
 		}
 
 		const char *comment;
-		MusicMetadata *meta = get_resource_data(RES_BGM_METADATA, e->arg, RESF_OPTIONAL);
+		MusicEntryParam *p = e->arg;
+		MusicMetadata *meta = p->bgm_meta;
+		Color *clr = RGBA(a, a, a, a);
 
-		if(meta && meta->comment) {
+		if(p->state & MSTATE_CONFIRM) {
+			comment = (
+				"\nYou have not unlocked this track yet!\n\n"
+				"If you wish to hear it anyway, please select it again to confirm."
+			);
+			clr->g *= 0.3;
+			clr->b *= 0.2;
+		} else if(!(p->state & MSTATE_COMMENT_VISIBLE)) {
+			continue;
+		} else if(meta && meta->comment) {
 			comment = meta->comment;
 		} else {
-			comment = "No comment available";
+			comment = "\nNo comment available";
 		}
 
 		text_draw_wrapped(comment, SCREEN_W - text_x * 2, &(TextParams) {
-			.pos = { text_x, text_y },
+			.pos = { text_x + 0.5*(SCREEN_W - text_x * 2), text_y },
 			.font_ptr = text_font,
 			.shader_ptr = text_shader,
-			.color = RGBA(a, a, a, a),
+			.color = clr,
+			.align = ALIGN_CENTER,
 		});
 
-		if(meta->artist) {
+		if(meta->artist && (p->state & MSTATE_COMMENT_VISIBLE)) {
 			const char *prefix = "— ";
 			char buf[strlen(prefix) + strlen(meta->artist) + 1];
 			strcpy(buf, prefix);
@@ -115,17 +208,37 @@ static void musicroom_draw(MenuData *m) {
 }
 
 static void action_play_bgm(MenuData *m, void *arg) {
-	preload_resource(RES_BGM, arg, RESF_OPTIONAL);
-	start_bgm(arg);
+	MusicEntryParam *p = arg;
+	assume(p != NULL);
+
+	if(p->state & (MSTATE_CONFIRM | MSTATE_UNLOCKED)) {
+		p->state &= ~MSTATE_CONFIRM;
+		preload_resource(RES_BGM, p->bgm, RESF_OPTIONAL);
+		start_bgm(p->bgm);
+	} else if (!(p->state & MSTATE_PLAYING)) {
+		p->state |= MSTATE_CONFIRM;
+	}
 }
 
 static void add_bgm(MenuData *m, const char *bgm) {
+	MusicMetadata *meta = get_resource_data(RES_BGM_METADATA, bgm, RESF_OPTIONAL | RESF_PRELOAD);
+	const char *title = (meta && meta->title) ? meta->title : "Unknown track";
+	MusicEntryParam *p = calloc(1, sizeof(*p));
+	p->bgm = bgm;
+	p->bgm_meta = meta;
+	p->text_shader = r_shader_get("text_default");
+
 	if(progress_is_bgm_unlocked(bgm)) {
-		MusicMetadata *meta = get_resource_data(RES_BGM_METADATA, bgm, RESF_OPTIONAL | RESF_PRELOAD);
-		MenuEntry *e = add_menu_entry(m, (meta && meta->title) ? meta->title : "Unknown track", action_play_bgm, (void*)bgm);
-		e->transition = NULL;
-	} else {
-		add_menu_entry(m, "???????", NULL, NULL);
+		p->state |= MSTATE_UNLOCKED;
+	}
+
+	MenuEntry *e = add_menu_entry(m, title, action_play_bgm, p);
+	e->transition = NULL;
+}
+
+static void musicroom_free(MenuData *m) {
+	for(MenuEntry *e = m->entries; e < m->entries + m->ecount; ++e) {
+		free(e->arg);
 	}
 }
 
@@ -133,6 +246,7 @@ MenuData* create_musicroom_menu(void) {
 	MenuData *m = alloc_menu();
 	m->logic = musicroom_logic;
 	m->draw = musicroom_draw;
+	m->end = musicroom_free;
 	m->transition = TransMenuDark;
 	m->flags = MF_Abortable;
 
