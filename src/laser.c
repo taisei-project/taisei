taisei-project/taisei

commit eea6aa094ccd63604d0e78790d5bb9189b486434

stagedraw: make postprocessing affect the overlay

src/stagedraw.c
@@ -919,7 +919,7 @@ static void stage_draw_objects(void) {
 	r_shader_standard();
 }
 
-static void stage_draw_overlay(void) {
+void stage_draw_overlay(void) {
 	r_state_push();
 	r_shader("sprite_default");
 	r_blend(BLEND_PREMUL_ALPHA);
@@ -943,42 +943,50 @@ static void postprocess_prepare(Framebuffer *fb, ShaderProgram *s) {
 	r_uniform_vec2("player", creal(global.plr.pos), VIEWPORT_H - cimag(global.plr.pos));
 }
 
-void stage_draw_foreground(void) {
-	float vw, vh;
-	video_get_viewport_size(&vw, &vh);
+static inline void begin_viewport_shake(void) {
+	if(global.shake_view) {
+		r_mat_push();
+		r_mat_translate(
+			global.shake_view * sin(global.frames),
+			global.shake_view * sin(global.frames * 1.1 + 3),
+			0
+		);
+		r_mat_scale(
+			1 + 2 * global.shake_view / VIEWPORT_W,
+			1 + 2 * global.shake_view / VIEWPORT_H,
+			1
+		);
+		r_mat_translate(
+			-global.shake_view,
+			-global.shake_view,
+			0
+		);
+	}
+}
+
+static inline void end_viewport_shake(void) {
+	if(global.shake_view) {
+		r_mat_pop();
+	}
+}
+
+void stage_draw_viewport(void) {
+	FloatRect dest_vp;
+	r_framebuffer_viewport_current(r_framebuffer_current(), &dest_vp);
+	r_uniform_sampler("tex", r_framebuffer_get_attachment(stagedraw.fb_pairs[FBPAIR_FG].front, FRAMEBUFFER_ATTACH_COLOR0));
 
 	// CAUTION: Very intricate pixel perfect scaling that will ruin your day.
-	float facw = (float)vw/SCREEN_W;
-	float fach = (float)vh/SCREEN_H;
+	float facw = dest_vp.w / SCREEN_W;
+	float fach = dest_vp.h / SCREEN_H;
 	// fach is equal to facw up to roundoff error.
 	float scale = fach;
 
-	// draw the foreground Framebuffer
 	r_mat_push();
-		r_mat_scale(1/facw, 1/fach,1);
-		r_mat_translate(roundf(facw*VIEWPORT_X), roundf(fach*VIEWPORT_Y), 0);
-		r_mat_scale(roundf(scale*VIEWPORT_W), roundf(scale*VIEWPORT_H), 1);
-
-		// apply the screenshake effect
-		if(global.shake_view) {
-			r_mat_translate(
-				global.shake_view * sin(global.frames) / VIEWPORT_W,
-				global.shake_view * sin(global.frames * 1.1 + 3) / VIEWPORT_H,
-				0
-			);
-			r_mat_scale(
-				1 + 2 * global.shake_view / VIEWPORT_W,
-				1 + 2 * global.shake_view / VIEWPORT_H,
-				1
-			);
-			r_mat_translate(
-				-global.shake_view / VIEWPORT_W,
-				-global.shake_view / VIEWPORT_H,
-				0
-			);
-		}
-
-		draw_framebuffer_tex(stage_get_fbpair(FBPAIR_FG)->front, 1, 1);
+	r_mat_scale(1/facw, 1/fach, 1);
+	r_mat_translate(roundf(facw * VIEWPORT_X), roundf(fach * VIEWPORT_Y), 0);
+	r_mat_scale(roundf(scale * VIEWPORT_W), roundf(scale * VIEWPORT_H), 1);
+	r_mat_translate(0.5, 0.5, 0);
+	r_draw_quad();
 	r_mat_pop();
 }
 
@@ -1006,6 +1014,8 @@ void stage_draw_scene(StageInfo *stage) {
 	r_cull(CULL_BACK);
 	r_shader_standard();
 
+	begin_viewport_shake();
+
 	if(draw_bg) {
 		// blit the background
 		r_state_push();
@@ -1025,6 +1035,12 @@ void stage_draw_scene(StageInfo *stage) {
 
 	// draw the 2D objects
 	stage_draw_objects();
+
+	end_viewport_shake();
+
+	// draw overlay: in-viewport text and HUD elements, etc.
+	// this stuff is not affected by the screen shake effect
+	stage_draw_overlay();
 
 	// everything drawn, now apply postprocessing
 	fbpair_swap(foreground);
@@ -1054,13 +1070,7 @@ void stage_draw_scene(StageInfo *stage) {
 	set_ortho(SCREEN_W, SCREEN_H);
 
 	// draw viewport contents
-	stage_draw_foreground();
-
-	// draw overlay (in-viewport text and HUD elements, etc.)
-	r_mat_push();
-	r_mat_translate(VIEWPORT_X, VIEWPORT_Y, 0);
-	stage_draw_overlay();
-	r_mat_pop();
+	stage_draw_viewport();
 
 	// draw HUD
 	stage_draw_hud();

src/stagedraw.h
@@ -25,7 +25,8 @@ typedef enum StageFBPair {
 void stage_draw_init(void);
 void stage_draw_shutdown(void);
 void stage_draw_hud(void);
-void stage_draw_foreground(void);
+void stage_draw_viewport(void);
+void stage_draw_overlay(void);
 void stage_draw_scene(StageInfo *stage);
 bool stage_should_draw_particle(Projectile *p);
 

src/menu/ingamemenu.c
@@ -160,11 +160,12 @@ MenuData* create_ingame_menu_replay(void) {
 void draw_ingame_menu_bg(MenuData *menu, float f) {
 	float rad = f*IMENU_BLUR;
 
+	r_state_push();
 	r_shader("ingame_menu");
 	r_uniform_float("rad", rad);
 	r_uniform_float("phase", menu->frames / 100.0);
-	stage_draw_foreground();
-	r_shader_standard();
+	stage_draw_viewport();
+	r_state_pop();
 }
 
 void update_ingame_menu(MenuData *menu) {

commit f554768cfc0ec2dd0fd21c440998d1cf92944fd0

fix rendering of lasers and a few effects during view shake
this is a regression introduced in eea6aa09

resources/00-taisei.pkgdir/shader/lasers/common.vert.glslh
@@ -19,7 +19,7 @@ void main(void) {
 	vec2 d = p - pos_rule(gl_InstanceID * 0.5 + timeshift - 0.1);
 
 	vec2 pos     = laser_vertex_pos(p, d);
-	gl_Position  = r_modelViewMatrix * r_projectionMatrix * vec4(pos, 0.0, 1.0);
+	gl_Position  = r_projectionMatrix * r_modelViewMatrix * vec4(pos, 0.0, 1.0);
 	texCoord     = (r_textureMatrix * vec4(texCoordRawIn, 0.0, 1.0)).xy;
 	texCoordRaw  = texCoordRawIn;
 }

src/laser.c
@@ -285,6 +285,8 @@ static void lasers_ent_predraw_hook(EntityInterface *ent, void *arg) {
 
 		FBPair *fbpair = stage_get_fbpair(FBPAIR_FG_AUX);
 
+		stage_draw_begin_noshake();
+
 		r_framebuffer(lasers.saved_fb);
 		r_state_push();
 
@@ -327,6 +329,7 @@ static void lasers_ent_predraw_hook(EntityInterface *ent, void *arg) {
 		draw_framebuffer_tex(fbpair->front, VIEWPORT_W, VIEWPORT_H);
 
 		r_state_pop();
+		stage_draw_end_noshake();
 		lasers.saved_fb = NULL;
 	}
 }

src/player.c
@@ -642,7 +642,9 @@ static void powersurge_distortion_draw(Projectile *p, int t) {
 	r_shader_standard();
 	r_color4(1.0, 0.9, 0.8, 1.0);
 	r_blend(BLEND_PREMUL_ALPHA);
+	stage_draw_begin_noshake();
 	draw_framebuffer_tex(fb_aux, VIEWPORT_W, VIEWPORT_H);
+	stage_draw_end_noshake();
 }
 
 static void player_powersurge_expired(Player *plr) {

src/plrmodes/marisa_a.c
@@ -230,6 +230,8 @@ static void marisa_laser_renderer_visual(Enemy *renderer, int t, bool render) {
 	r_blend(BLEND_PREMUL_ALPHA);
 	fbpair_swap(fbp_aux);
 
+	stage_draw_begin_noshake();
+
 	r_framebuffer(fbp_aux->back);
 	r_clear(CLEAR_COLOR, RGBA(0, 0, 0, 0), 1);
 	r_shader("max_to_alpha");
@@ -241,6 +243,8 @@ static void marisa_laser_renderer_visual(Enemy *renderer, int t, bool render) {
 	r_color4(1, 1, 1, 1);
 	draw_framebuffer_tex(fbp_aux->front, VIEWPORT_W, VIEWPORT_H);
 	r_shader_ptr(shader);
+
+	stage_draw_end_noshake();
 
 	r_uniform_vec4(u_clr0, 0.5, 0.0, 0.0, 0.0);
 	r_uniform_vec4(u_clr1, 1.0, 0.0, 0.0, 0.0);

src/stagedraw.c
@@ -970,6 +970,42 @@ static inline void end_viewport_shake(void) {
 	}
 }
 
+/*
+ * Small helpers for entities draw code that might want to suppress viewport shake temporarily.
+ * This is mostly useful when multiple framebuffers are involved.
+ */
+static int shake_suppressed = 0;
+
+void stage_draw_begin_noshake(void) {
+	assert(!shake_suppressed);
+	shake_suppressed = 1;
+
+	if(global.shake_view) {
+		shake_suppressed = 2;
+		MatrixMode mm = r_mat_mode_current();
+		r_mat_mode(MM_MODELVIEW);
+		r_mat_push();
+		r_mat_identity();
+		r_mat_mode(mm);
+	}
+}
+
+void stage_draw_end_noshake(void) {
+	assert(shake_suppressed);
+
+	if(global.shake_view) {
+		// make sure shake_view doesn't change in-between the begin/end calls;
+		// that would've been *really* nasty to debug.
+		assert(shake_suppressed == 2);
+		MatrixMode mm = r_mat_mode_current();
+		r_mat_mode(MM_MODELVIEW);
+		r_mat_pop();
+		r_mat_mode(mm);
+	}
+
+	shake_suppressed = 0;
+}
+
 void stage_draw_viewport(void) {
 	FloatRect dest_vp;
 	r_framebuffer_viewport_current(r_framebuffer_current(), &dest_vp);

src/stagedraw.h
@@ -32,6 +32,9 @@ bool stage_should_draw_particle(Projectile *p);
 
 void stage_display_clear_screen(const StageClearBonus *bonus);
 
+void stage_draw_begin_noshake(void);
+void stage_draw_end_noshake(void);
+
 FBPair* stage_get_fbpair(StageFBPair id) attr_returns_nonnull;
 Framebuffer* stage_add_foreground_framebuffer(const char *label, float scale_worst, float scale_best, uint num_attachments, FBAttachmentConfig attachments[num_attachments]);
 Framebuffer* stage_add_background_framebuffer(const char *label, float scale_worst, float scale_best, uint num_attachments, FBAttachmentConfig attachments[num_attachments]);

src/stages/stage6_events.c
@@ -780,7 +780,9 @@ static void BaryonCenter(Enemy *bcenter, int t, bool render) {
 		return;
 	}
 
+	stage_draw_begin_noshake();
 	r_state_push();
+
 	r_shader("baryon_feedback");
 	r_uniform_vec2("blur_resolution", 0.5*VIEWPORT_W, 0.5*VIEWPORT_H);
 	r_uniform_float("hue_shift", 0);
@@ -792,6 +794,7 @@ static void BaryonCenter(Enemy *bcenter, int t, bool render) {
 	// r_uniform_float("hue_shift", 0.04 * sin(global.frames/30.0));
 	r_uniform_float("hue_shift", 0.01);
 	r_color4(0.95, 0.88, 0.9, 0.5);
+
 	draw_framebuffer_tex(baryon_fbpair.front, VIEWPORT_W, VIEWPORT_H);
 
 	fbpair_swap(&baryon_fbpair);
@@ -805,14 +808,19 @@ static void BaryonCenter(Enemy *bcenter, int t, bool render) {
 	// r_shader("sprite_default");
 	// draw_baryons(e, t);
 	r_state_pop();
+	stage_draw_end_noshake();
 
 	r_shader("sprite_default");
 	draw_baryons(bcenter, t);
+
+	stage_draw_begin_noshake();
 
 	r_shader_standard();
 	fbpair_swap(&baryon_fbpair);
 	r_color4(0.7, 0.7, 0.7, 0.7);
 	draw_framebuffer_tex(baryon_fbpair.front, VIEWPORT_W, VIEWPORT_H);
+
+	stage_draw_end_noshake();
 
 	r_color4(1, 1, 1, 1);
 	r_framebuffer(baryon_fbpair.front);