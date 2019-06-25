2fa18dc15946ecbe22eb3d3a7dc934e2a6d3aea0
Andrei Alexeyev <akari@taisei-project.org>
2019-06-25 06:45:32 +0300

    'alphamap' functionality; effects for wriggle and youmu portraits

--- resources/00-taisei.pkgdir/shader/texture_post_load.frag.glsl
+++ resources/00-taisei.pkgdir/shader/texture_post_load.frag.glsl
@@ -2,7 +2,14 @@
 
 #include "interface/standard.glslh"
 
+UNIFORM(1) sampler2D alphamap;
+UNIFORM(2) int have_alphamap;
+
 void main(void) {
 	fragColor = textureLod(tex, texCoordRaw, 0);
 	fragColor.rgb *= fragColor.a;
+
+	if(have_alphamap != 0) {
+		fragColor.a *= textureLod(alphamap, texCoordRaw, 0).r;
+	}
 }
