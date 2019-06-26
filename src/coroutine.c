bdef62d7f4ff6a3114f2f6e23b357985453f5730
Andrei Alexeyev <akari@taisei-project.org>
2019-06-26 13:09:26 +0300

    Initial coroutine/task system implementation

---
+++ src/coroutine.c
@@ -0,0 +1,124 @@
+/*
+ * This software is licensed under the terms of the MIT-License
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@alienslab.net>.
+ */
+
+#include "taisei.h"
+
+#include "coroutine.h"
+#include "util.h"
+
+#define CO_STACK_SIZE (64 * 1024)
+
+struct CoTask {
+	LIST_INTERFACE(CoTask);
+	koishi_coroutine_t ko;
+};
+
+struct CoSched {
+	LIST_ANCHOR(CoTask) tasks, pending_tasks;
+};
+
+static LIST_ANCHOR(CoTask) task_pool;
+static size_t task_pool_size;
+
+CoSched *_cosched_global;
+
+CoTask *cotask_new(CoTaskFunc func) {
+	CoTask *task;
+
+	if((task = alist_pop(&task_pool))) {
+		koishi_recycle(&task->ko, func);
+		log_debug("Recycled task %p for proc %p", (void*)task, *(void**)&func);
+	} else {
+		task = calloc(1, sizeof(*task));
+		koishi_init(&task->ko, CO_STACK_SIZE, func);
+		++task_pool_size;
+		log_debug("Created new task %p for proc %p (%zu tasks in pool)", (void*)task, *(void**)&func, task_pool_size);
+	}
+
+	return task;
+}
+
+void cotask_free(CoTask *task) {
+	alist_push(&task_pool, task);
+}
+
+void *cotask_resume(CoTask *task, void *arg) {
+	return koishi_resume(&task->ko, arg);
+}
+
+void *cotask_yield(void *arg) {
+	return koishi_yield(arg);
+}
+
+CoStatus cotask_status(CoTask *task) {
+	return koishi_state(&task->ko);
+}
+
+CoSched *cosched_new(void) {
+	CoSched *sched = calloc(1, sizeof(*sched));
+	return sched;
+}
+
+void *cosched_new_task(CoSched *sched, CoTaskFunc func, void *arg) {
+	CoTask *task = cotask_new(func);
+	void *ret = cotask_resume(task, arg);
+
+	if(cotask_status(task) == CO_STATUS_DEAD) {
+		cotask_free(task);
+	} else {
+		assert(cotask_status(task) == CO_STATUS_SUSPENDED);
+		alist_append(&sched->pending_tasks, task);
+	}
+
+	return ret;
+}
+
+uint cosched_run_tasks(CoSched *sched) {
+	for(CoTask *t; (t = alist_pop(&sched->pending_tasks));)  {
+		alist_append(&sched->tasks, t);
+	}
+
+	uint ran = 0;
+
+	for(CoTask *t = sched->tasks.first, *next; t; t = next) {
+		next = t->next;
+		assert(cotask_status(t) == CO_STATUS_SUSPENDED);
+		cotask_resume(t, NULL);
+		++ran;
+
+		if(cotask_status(t) == CO_STATUS_DEAD) {
+			alist_unlink(&sched->tasks, t);
+			cotask_free(t);
+		}
+	}
+
+	return ran;
+}
+
+void cosched_free(CoSched *sched) {
+	for(CoTask *t = sched->pending_tasks.first, *next; t; t = next) {
+		next = t->next;
+		cotask_free(t);
+	}
+
+	for(CoTask *t = sched->tasks.first, *next; t; t = next) {
+		next = t->next;
+		cotask_free(t);
+	}
+}
+
+void coroutines_init(void) {
+
+}
+
+void coroutines_shutdown(void) {
+	for(CoTask *task; (task = alist_pop(&task_pool));) {
+		koishi_deinit(&task->ko);
+		free(task);
+	}
+}
---
+++ src/coroutine.h
@@ -0,0 +1,67 @@
+/*
+ * This software is licensed under the terms of the MIT-License
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@alienslab.net>.
+ */
+
+#ifndef IGUARD_coroutine_h
+#define IGUARD_coroutine_h
+
+#include "taisei.h"
+
+#include <koishi.h>
+
+typedef struct CoTask CoTask;
+typedef struct CoSched CoSched;
+typedef void *(*CoTaskFunc)(void *arg);
+
+// target for the INVOKE_TASK macro
+extern CoSched *_cosched_global;
+
+typedef enum CoStatus {
+	CO_STATUS_SUSPENDED = KOISHI_SUSPENDED,
+	CO_STATUS_RUNNING   = KOISHI_RUNNING,
+	CO_STATUS_DEAD      = KOISHI_DEAD,
+} CoStatus;
+
+void coroutines_init(void);
+void coroutines_shutdown(void);
+
+CoTask *cotask_new(CoTaskFunc func);
+void cotask_free(CoTask *task);
+void *cotask_resume(CoTask *task, void *arg);
+void *cotask_yield(void *arg);
+CoStatus cotask_status(CoTask *task);
+
+CoSched *cosched_new(void);
+void *cosched_new_task(CoSched *sched, CoTaskFunc func, void *arg);  // creates and runs the task, returns whatever it yields, schedules it for resume on cosched_run_tasks if it's still alive
+uint cosched_run_tasks(CoSched *sched);  // returns number of tasks ran
+void cosched_free(CoSched *sched);
+
+static inline attr_must_inline void cosched_set_invoke_target(CoSched *sched) { _cosched_global = sched; }
+
+#define TASK(name, argstruct) \
+	typedef struct argstruct COARGS_##name; \
+	static void COTASK_##name(COARGS_##name ARGS); \
+	static void *COTASKTHUNK_##name(void *arg) { \
+		COTASK_##name(*(COARGS_##name*)arg); \
+		return NULL; \
+	} \
+	static void COTASK_##name(COARGS_##name ARGS)
+
+#define INVOKE_TASK(name, ...) do { \
+	COARGS_##name _coargs = { __VA_ARGS__ }; \
+	cosched_new_task(_cosched_global, COTASKTHUNK_##name, &_coargs); \
+} while(0)
+
+#define YIELD         cotask_yield(NULL)
+#define WAIT(delay)   do { int _delay = (delay); while(_delay-- > 0) YIELD; } while(0)
+
+// to use these inside a coroutine, define a BREAK_CONDITION macro and a BREAK label.
+#define CHECK_BREAK   do { if(BREAK_CONDITION) goto BREAK; } while(0)
+#define BYIELD        do { YIELD; CHECK_BREAK; } while(0)
+#define BWAIT(frames) do { WAIT(frames); CHECK_BREAK; } while(0)
+
+#endif // IGUARD_coroutine_h
