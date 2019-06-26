e03033e35ad69a465bfa94b50239e69ce9650089
Andrei Alexeyev <akari@taisei-project.org>
2019-06-26 13:09:26 +0300

    coroutines (proof of concept)

---
+++ src/stages/corotest.h
@@ -0,0 +1,18 @@
+/*
+ * This software is licensed under the terms of the MIT-License
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@alienslab.net>.
+ */
+
+#ifndef IGUARD_stages_corotest_h
+#define IGUARD_stages_corotest_h
+
+#include "taisei.h"
+
+#include "stage.h"
+
+extern StageProcs corotest_procs;
+
+#endif // IGUARD_stages_corotest_h
