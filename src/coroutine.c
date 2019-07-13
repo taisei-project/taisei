3f81afc3009a45609ce93dc118ec32b498d38ccb
Andrei Alexeyev <akari@taisei-project.org>
2019-07-13 09:27:10 +0300

    replace libco with libkoishi

--- src/coroutine.c
+++ src/coroutine.c
@@ -11,84 +11,40 @@
 #include "coroutine.h"
 #include "util.h"
 
-#include <libco.h>
-
-#define CO_STACK_SIZE (4096*2)
+#define CO_STACK_SIZE (64 * 1024)
 
 struct CoTask {
 	LIST_INTERFACE(CoTask);
-	cothread_t cothread;
-	CoTask *caller;
-	CoTaskFunc func;
-	void *arg;
-	CoStatus status;
+	koishi_coroutine_t ko;
 };
 
 struct CoSched {
 	LIST_ANCHOR(CoTask) tasks, pending_tasks;
 };
 
-static CoTask *this_task, main_task;
 CoSched *_cosched_global;
-
-static noreturn void co_thunk(void) {
-	CoTask *task = this_task;
-	task->func(task->arg);
-	task->status = CO_STATUS_DEAD;
-	cotask_resume(&main_task, NULL);
-	UNREACHABLE;
-}
 
 CoTask *cotask_new(CoTaskFunc func) {
 	CoTask *task = calloc(1, sizeof(*task));
-	task->cothread = co_create(CO_STACK_SIZE, co_thunk);
-	task->status = CO_STATUS_SUSPENDED;
-	task->func = func;
+	koishi_init(&task->ko, CO_STACK_SIZE, func);
 	return task;
 }
 
 void cotask_free(CoTask *task) {
-	co_delete(task->cothread);
+	koishi_deinit(&task->ko);
 	free(task);
 }
 
 void *cotask_resume(CoTask *task, void *arg) {
-	assert(task->status == CO_STATUS_SUSPENDED);
-
-	if(this_task->status != CO_STATUS_DEAD) {
-		assert(this_task->status == CO_STATUS_RUNNING);
-		this_task->status = CO_STATUS_SUSPENDED;
-	}
-
-	task->status = CO_STATUS_RUNNING;
-	task->caller = this_task;
-	task->arg = arg;
-	this_task = task;
-
-	co_switch(task->cothread);
-	return this_task->arg;
+	return koishi_resume(&task->ko, arg);
 }
 
 void *cotask_yield(void *arg) {
-	CoTask *task = this_task->caller;
-
-	assert(task->status == CO_STATUS_SUSPENDED);
-
-	if(this_task->status != CO_STATUS_DEAD) {
-		assert(this_task->status == CO_STATUS_RUNNING);
-		this_task->status = CO_STATUS_SUSPENDED;
-	}
-
-	task->status = CO_STATUS_RUNNING;
-	task->arg = arg;
-	this_task = task;
-
-	co_switch(task->cothread);
-	return this_task->arg;
+	return koishi_yield(arg);
 }
 
 CoStatus cotask_status(CoTask *task) {
-	return task->status;
+	return koishi_state(&task->ko);
 }
 
 CoSched *cosched_new(void) {
@@ -100,10 +56,10 @@ void *cosched_new_task(CoSched *sched, CoTaskFunc func, void *arg) {
 	CoTask *task = cotask_new(func);
 	void *ret = cotask_resume(task, arg);
 
-	if(task->status == CO_STATUS_DEAD) {
+	if(cotask_status(task) == CO_STATUS_DEAD) {
 		cotask_free(task);
 	} else {
-		assume(task->status == CO_STATUS_SUSPENDED);
+		assume(cotask_status(task) == CO_STATUS_SUSPENDED);
 		alist_append(&sched->pending_tasks, task);
 	}
 
@@ -119,11 +75,11 @@ uint cosched_run_tasks(CoSched *sched) {
 
 	for(CoTask *t = sched->tasks.first, *next; t; t = next) {
 		next = t->next;
-		assert(t->status == CO_STATUS_SUSPENDED);
-		cotask_resume(t, t->arg);
+		assert(cotask_status(t) == CO_STATUS_SUSPENDED);
+		cotask_resume(t, NULL);
 		++ran;
 
-		if(t->status == CO_STATUS_DEAD) {
+		if(cotask_status(t) == CO_STATUS_DEAD) {
 			alist_unlink(&sched->tasks, t);
 			cotask_free(t);
 		}
@@ -145,8 +101,5 @@ void cosched_free(CoSched *sched) {
 }
 
 void coroutines_init(void) {
-	memset(&main_task, 0, sizeof(main_task));
-	main_task.cothread = co_active();
-	main_task.status = CO_STATUS_RUNNING;
-	this_task = &main_task;
+
 }
