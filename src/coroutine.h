15e527fa43c506140942f0706a00da0ff759631e
Andrei Alexeyev <akari@taisei-project.org>
2019-07-17 05:04:49 +0300

    experimental event system for coroutine-based stages

--- src/coroutine.h
+++ src/coroutine.h
@@ -27,6 +27,12 @@ typedef enum CoStatus {
 	CO_STATUS_DEAD      = KOISHI_DEAD,
 } CoStatus;
 
+typedef struct CoEvent {
+	ListAnchor subscribers;
+	uint32_t unique_id;
+	uint32_t num_signaled;
+} CoEvent;
+
 void coroutines_init(void);
 void coroutines_shutdown(void);
 
@@ -34,9 +40,14 @@ CoTask *cotask_new(CoTaskFunc func);
 void cotask_free(CoTask *task);
 void *cotask_resume(CoTask *task, void *arg);
 void *cotask_yield(void *arg);
+bool cotask_wait_event(CoEvent *evt, void *arg);
 CoStatus cotask_status(CoTask *task);
 CoTask *cotask_active(void);
 void cotask_bind_to_entity(CoTask *task, EntityInterface *ent);
+
+void coevent_init(CoEvent *evt);
+void coevent_signal(CoEvent *evt);
+void coevent_cancel(CoEvent *evt);
 
 CoSched *cosched_new(void);
 void *cosched_new_task(CoSched *sched, CoTaskFunc func, void *arg);  // creates and runs the task, returns whatever it yields, schedules it for resume on cosched_run_tasks if it's still alive
@@ -46,21 +57,38 @@ void cosched_free(CoSched *sched);
 static inline attr_must_inline void cosched_set_invoke_target(CoSched *sched) { _cosched_global = sched; }
 
 #define TASK(name, argstruct) \
+	static char COTASK_UNUSED_CHECK_##name; \
 	typedef struct argstruct COARGS_##name; \
 	static void COTASK_##name(COARGS_##name ARGS); \
-	static void *COTASKTHUNK_##name(void *arg) { \
+	attr_unused static void *COTASKTHUNK_##name(void *arg) { \
 		COTASK_##name(*(COARGS_##name*)arg); \
+		return NULL; \
+	} \
+	typedef struct { CoEvent *event; COARGS_##name args; } COARGSCOND_##name; \
+	attr_unused static void *COTASKTHUNKCOND_##name(void *arg) { \
+		COARGSCOND_##name args = *(COARGSCOND_##name*)arg; \
+		if(WAIT_EVENT(args.event)) { \
+			COTASK_##name(args.args); \
+		} \
 		return NULL; \
 	} \
 	static void COTASK_##name(COARGS_##name ARGS)
 
 #define INVOKE_TASK(name, ...) do { \
+	(void)COTASK_UNUSED_CHECK_##name; \
 	COARGS_##name _coargs = { __VA_ARGS__ }; \
 	cosched_new_task(_cosched_global, COTASKTHUNK_##name, &_coargs); \
 } while(0)
 
+#define INVOKE_TASK_WHEN(event, name, ...) do { \
+	(void)COTASK_UNUSED_CHECK_##name; \
+	COARGSCOND_##name _coargs = { event, { __VA_ARGS__ } }; \
+	cosched_new_task(_cosched_global, COTASKTHUNKCOND_##name, &_coargs); \
+} while(0)
+
 #define YIELD         cotask_yield(NULL)
 #define WAIT(delay)   do { int _delay = (delay); while(_delay-- > 0) YIELD; } while(0)
+#define WAIT_EVENT(e) cotask_wait_event((e), NULL)
 
 // to use these inside a coroutine, define a BREAK_CONDITION macro and a BREAK label.
 #define CHECK_BREAK   do { if(BREAK_CONDITION) goto BREAK; } while(0)
