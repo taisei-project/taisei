6fc14c6c2a14ef38f5ff0b38d78a3947cbe9a6c2
Andrei Alexeyev <akari@taisei-project.org>
2019-08-30 21:24:34 +0300

    Make most bullets smaller; add red channel for glow effects

--- resources/00-taisei.pkgdir/shader/sprite_bullet.frag.glsl
+++ resources/00-taisei.pkgdir/shader/sprite_bullet.frag.glsl
@@ -3,6 +3,11 @@
 #include "lib/sprite_main.frag.glslh"
 
 void spriteMain(out vec4 fragColor) {
-    vec4 texel = texture(tex, texCoord);
-    fragColor = (texel.g * color + vec4(texel.b)) * (1 - customParams.r);
+	vec4 texel = texture(tex, texCoord);
+
+	if(texel.a == 0.0) {
+		discard;
+	}
+
+	fragColor = (color * texel.g + vec4(texel.b)) * (1 - customParams.r);
 }
