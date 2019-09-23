9ce4628440dc99118a30526639b89cb87ae7addb
Andrei Alexeyev <akari@taisei-project.org>
2019-09-24 01:27:57 +0300

    Add support for packaging .tar* binary archives

---
+++ scripts/gen-dist.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from taiseilib.tempinstall import (
+    temp_install,
+)
+
+from taiseilib.common import (
+    add_common_args,
+    run_main,
+)
+
+from pathlib import (
+    Path,
+    PurePosixPath,
+)
+
+from datetime import (
+    datetime,
+)
+
+import argparse
+import os
+import shutil
+
+
+def main(args):
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser(description='Generate a distribution archive.', prog=args[0])
+
+    parser.add_argument('build_dir',
+        help='the build directory (defaults to CWD)',
+        default=Path(os.getcwd()),
+        type=Path,
+        nargs='?',
+    )
+
+    parser.add_argument('output',
+        help='the output file path',
+        type=Path,
+    )
+
+    parser.add_argument('--format',
+        help='the archive format',
+        default='zip',
+        choices=sorted(map(lambda x: x[0], shutil.get_archive_formats())),
+    )
+
+    parser.add_argument('--prefix',
+        help='add a common prefix to all files and directories in the archive',
+        default=None,
+    )
+
+    add_common_args(parser)
+    args = parser.parse_args(args[1:])
+
+    if args.prefix is not None:
+        p = PurePosixPath(args.prefix)
+
+        if p.is_absolute() or p.is_reserved() or '..' in p.parts:
+            raise ValueError('Bad prefix: {}'.format(args.prefix))
+
+        args.prefix = str(p)
+
+        if args.prefix == '.':
+            args.prefix = None
+
+    with temp_install(args.build_dir) as install_dir:
+        if args.prefix is not None:
+            os.chdir(str(install_dir.parent))
+            install_dir.rename(install_dir.parent / args.prefix)
+            archive = shutil.make_archive(args.output, args.format, '.', args.prefix)
+        else:
+            archive = shutil.make_archive(args.output, args.format, install_dir)
+
+        archive = Path(archive)
+        archive.rename(args.output)
+
+    print("Generated distribution archive {}".format(str(args.output)))
+
+
+if __name__ == '__main__':
+    run_main(main)
