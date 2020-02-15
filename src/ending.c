0a4d662f905ab9a8034554d9fe9473403c91f883
Alice D <alice@starwitch.productions>
2020-02-15 15:23:05 -0500

    revert --

--- src/ending.c
+++ src/ending.c
@@ -1,7 +1,7 @@
 /*
  * This software is licensed under the terms of the MIT License.
  * See COPYING for further information.
- * —-
+ * ---
  * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
  * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
  */
