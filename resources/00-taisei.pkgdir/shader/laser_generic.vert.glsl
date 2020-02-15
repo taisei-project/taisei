9e4fa7fc810f52c64dc81597920c328e8bc5aa55
Andrei Alexeyev <akari@taisei-project.org>
2020-02-15 05:23:31 +0200

    fix matrix mult. order in generic laser shader

--- resources/00-taisei.pkgdir/shader/laser_generic.vert.glsl
+++ resources/00-taisei.pkgdir/shader/laser_generic.vert.glsl
@@ -16,7 +16,7 @@ UNIFORM(4) int span;
 
 void main(void) {
     vec2 pos     = laser_vertex_pos(instance_pos_delta.xy, instance_pos_delta.zw, instance_width);
-    gl_Position  = r_modelViewMatrix * r_projectionMatrix * vec4(pos, 0.0, 1.0);
+    gl_Position  = r_projectionMatrix * r_modelViewMatrix * vec4(pos, 0.0, 1.0);
     texCoord     = (r_textureMatrix * vec4(texCoordRawIn, 0.0, 1.0)).xy;
     texCoordRaw  = texCoordRawIn;
 }
