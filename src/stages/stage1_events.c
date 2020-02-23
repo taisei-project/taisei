ca5c0079678426f9d6dc9982735d78b4d8f457f1
Andrei Alexeyev <akari@taisei-project.org>
2020-02-23 02:28:30 +0200

    revamp cirno's second nonspell

--- src/stages/stage1_events.c
+++ src/stages/stage1_events.c
@@ -207,70 +207,147 @@ DEFINE_EXTERN_TASK(stage1_spell_crystal_rain) {
 	}
 }
 
-TASK_WITH_INTERFACE(COARGS_boss_nonspell_2, BossAttack) {
+TASK(cirno_snowburst, { BoxedBoss boss; }) {
+	Boss *boss = TASK_BIND(ARGS.boss);
+	play_sound("shot_special1");
+	aniplayer_queue(&boss->ani, "(9)", 0);
 
+	int rounds = difficulty_value(3, 4, 5, 6);
+	int shots_per_round = difficulty_value(4, 5, 6, 7);
+	real spread = difficulty_value(0.13, 0.16, 0.19, 0.22);
+
+	real angle_ofs = carg(global.plr.pos - boss->pos);
+	for(int round = 0; round < rounds; ++round) {
+		real speed = (3 + round / 3.0);
+		real boost = round * 0.7;
+
+		for(int k = 0; k < 8; ++k) {
+			real base_angle = k * M_TAU / 8 + angle_ofs;
+			int cnt_mod = round & 1;
+			int effective_count = shots_per_round - cnt_mod;
+			real effective_spread = spread * ((real)effective_count / shots_per_round);
+
+			for(int i = 0; i < effective_count; ++i) {
+				real spread_factor = rng_real();
+				spread_factor = 2.0 * (i / (effective_count - 1.0) - 0.5);
+				cmplx dir = cdir(base_angle + effective_spread * spread_factor);
+
+				PROJECTILE(
+					.proto = pp_plainball,
+					.pos = boss->pos,
+					.color = RGB(0, 0, 0.5),
+					.move = move_asymptotic_simple(speed * dir, boost),
+				);
+			}
+		}
+
+		WAIT(2);
+	}
+
+	aniplayer_queue(&boss->ani, "main", 0);
 }
 
-static void cirno_iceplosion1(Boss *c, int time) {
-	int t = time % 300;
-	TIMER(&t);
+TASK(cirno_spiralshot, {
+	cmplx pos;
+	int count;
+	real spread;
+	real winding;
+	int interval;
+	real angle_ofs;
+	real bullet_speed;
+} ) {
+	int count = ARGS.count;
+	real winding = ARGS.winding;
+	real angle_ofs = ARGS.angle_ofs;
+	real dist_per_bullet = ARGS.spread;
+	real angle_per_bullet = winding / (count - 1.0);
+	int interval = ARGS.interval;
+	cmplx pos = ARGS.pos;
 
-	if(time < 0)
-		GO_TO(c, VIEWPORT_W/2.0 + 100.0*I, 0.02);
+	DECLARE_ENT_ARRAY(Projectile, projs, count);
 
-	AT(20) {
-		play_sound("shot_special1");
-	}
-	AT(20)
-		aniplayer_queue(&c->ani,"(9)",0);
-	AT(30)
-		aniplayer_queue(&c->ani,"main",0);
+	for(int b = 0; b < count; ++b) {
+		play_loop("shot1_loop");
 
-	FROM_TO(20,30,2) {
-		int i;
-		for(i = 0; i < 15+global.diff; i++) {
-			PROJECTILE(
-				.proto = pp_plainball,
-				.pos = c->pos,
-				.color = RGB(0,0,0.5),
-				.rule = asymptotic,
-				.args = { (3+_i/3.0)*cexp(I*((2)*M_PI/8.0*i + (0.1+0.03*global.diff) * rng_sreal())), _i*0.7 }
-			);
-		}
-	}
+		real dist = b * dist_per_bullet;
+		real angle = angle_ofs + b * angle_per_bullet;
 
-	FROM_TO_SND("shot1_loop",40,100,2+2*(global.diff<D_Hard)) {
-		for(int i = 1; i >= -1; i -= 2) {
-			PROJECTILE(
-				.proto = pp_crystal,
-				.pos = c->pos + i * 100,
-				.color = RGB(0.3,0.3,0.8),
-				.rule = accelerated,
-				.args = {
-					1.5*rng_dir() - i * 0.4 + 2.0*I*global.diff/4.0,
-					0.002*cexp(I*(M_PI/10.0*(_i%20)))
-				}
-			);
-		}
+		ENT_ARRAY_ADD(&projs, PROJECTILE(
+			.proto = pp_crystal,
+			.pos = pos + dist * cdir(angle),
+			.color = RGB(0.3, 0.3, 0.8),
+			.angle = angle + M_PI,
+		));
+
+		WAIT(interval);
 	}
 
-	FROM_TO(150, 300, 30 - 6 * global.diff) {
-		float dif = rng_angle();
-		int i;
+	WAIT(20);
+	play_sound("shot_special1");
+	play_sound("redirect");
+	ENT_ARRAY_FOREACH(&projs, Projectile *p, {
+		spawn_projectile_highlight_effect(p);
+		p->move = move_linear(cdir(p->angle) * ARGS.bullet_speed);
+	});
+	ENT_ARRAY_CLEAR(&projs);
+}
 
-		if(_i > 15) {
-			_i = 15;
-		}
+TASK_WITH_INTERFACE(boss_nonspell_2, BossAttack) {
+	Boss *boss = INIT_BOSS_ATTACK();
+	boss->move = move_towards(VIEWPORT_W/2.0 + 100.0*I, 0.02);
+	BEGIN_BOSS_ATTACK();
 
-		play_sound("shot1");
-		for(i = 0; i < 20; i++) {
-			PROJECTILE(
-				.proto = pp_plainball,
-				.pos = c->pos,
-				.color = RGB(0.04*_i,0.04*_i,0.4+0.04*_i),
-				.rule = asymptotic,
-				.args = { (3+_i/3.0)*cdir(2*M_PI/8.0*i + dif), 2.5 }
-			);
+	for(;;) {
+		WAIT(20);
+		INVOKE_SUBTASK(cirno_snowburst, ENT_BOX(boss));
+		WAIT(20);
+
+		int spiral_bullets = difficulty_value(40, 80, 100, 100);
+		int turns = difficulty_value(8, 4, 4, 5);
+		int interval = difficulty_value(2, 1, 1, 1);
+		real bullet_speed = difficulty_value(2, 3, 3, 4.5);
+
+		INVOKE_SUBTASK(cirno_spiralshot,
+			.pos = boss->pos + 100,
+			.count = spiral_bullets,
+			.spread = 1,
+			.winding = turns * M_TAU,
+			.interval = interval,
+			.angle_ofs = M_PI,
+			.bullet_speed = bullet_speed
+		);
+
+		INVOKE_SUBTASK(cirno_spiralshot,
+			.pos = boss->pos - 100,
+			.count = spiral_bullets,
+			.spread = 1,
+			.winding = -turns * M_TAU,
+			.interval = interval,
+			.angle_ofs = 0,
+			.bullet_speed = bullet_speed
+		);
+
+		WAIT(140);
+
+		interval = difficulty_value(24, 18, 12, 6);
+		for(int t = 0, i = 0; t < 150;) {
+			float dif = rng_angle();
+
+			play_sound("shot1");
+			for(int j = 0; j < 20; ++j) {
+				PROJECTILE(
+					.proto = pp_plainball,
+					.pos = boss->pos,
+					.color = RGB(0.04*i, 0.04*i, 0.4+0.04*i),
+					.move = move_asymptotic_simple((3+i/3.0)*cdir(M_TAU/8.0*j + dif), 2.5),
+				);
+			}
+
+			t += WAIT(interval);
+
+			if(i < 15) {
+				++i;
+			}
 		}
 	}
 }
@@ -1542,7 +1619,7 @@ TASK(spawn_boss, NO_ARGS) {
 
 	boss_add_attack_task(boss, AT_Normal, "Iceplosion 0", 20, 23000, TASK_INDIRECT(BossAttack, boss_nonspell_1), NULL);
 	boss_add_attack_from_info(boss, &stage1_spells.boss.crystal_rain, false);
-	boss_add_attack(boss, AT_Normal, "Iceplosion 1", 20, 24000, cirno_iceplosion1, NULL);
+	boss_add_attack_task(boss, AT_Normal, "Iceplosion 1", 20, 24000, TASK_INDIRECT(BossAttack, boss_nonspell_2), NULL);
 
 	if(global.diff > D_Normal) {
 		boss_add_attack_from_info(boss, &stage1_spells.boss.snow_halation, false);
