taisei-project/taisei

commit 0cbc86c66e1f8e352b3f7e3ae96554bdf3324dc7

Add generic type-safe facility for dynamic arrays (#207)
Replace most ad-hoc opencoded dynamic arrays across the codebase

src/vfs/public.c
@@ -198,37 +198,30 @@ const char* vfs_dir_read(VFSDir *dir) {
 }
 
 char** vfs_dir_list_sorted(const char *path, size_t *out_size, int (*compare)(const void*, const void*), bool (*filter)(const char*)) {
-	char **results = NULL;
 	VFSDir *dir = vfs_dir_open(path);
 
 	if(!dir) {
-		return results;
+		return NULL;
 	}
 
-	size_t real_size = 8;
-	results = malloc(sizeof(char*) * real_size);
-	*out_size = 0;
+	DYNAMIC_ARRAY(char*) results = { 0 };
 
 	for(const char *e; (e = vfs_dir_read(dir));) {
 		if(filter && !filter(e)) {
 			continue;
 		}
 
-		results[(*out_size)++] = strdup(e);
-
-		if(*out_size >= real_size) {
-			real_size *= 2;
-			results = realloc(results, sizeof(char*) * real_size);
-		}
+		*dynarray_append(&results) = strdup(e);
 	}
 
 	vfs_dir_close(dir);
 
-	if(*out_size) {
-		qsort(results, *out_size, sizeof(char*), compare);
+	if(results.num_elements) {
+		dynarray_qsort(&results, compare);
 	}
 
-	return results;
+	*out_size = results.num_elements;
+	return results.data;
 }
 
 void vfs_dir_list_free(char **list, size_t size) {

src/coroutine.c
@@ -648,48 +648,26 @@ int cotask_wait(int delay) {
 	return cotask_wait_init(task_data, COTASK_WAIT_NONE).frames;
 }
 
+static bool subscribers_array_predicate(const void *pelem, void *userdata) {
+	return cotask_unbox(*(const BoxedTask*)pelem);
+}
+
 static void coevent_cleanup_subscribers(CoEvent *evt) {
-	if(evt->num_subscribers == 0) {
+	if(evt->subscribers.num_elements == 0) {
 		return;
 	}
 
-	uint num_subs = evt->num_subscribers;
-	BoxedTask new_subs[num_subs];
-	BoxedTask *p_new_subs = new_subs;
-
-	for(uint i = 0; i < num_subs; ++i) {
-		if(cotask_unbox(evt->subscribers[i])) {
-			*p_new_subs++ = evt->subscribers[i];
-		}
-	}
-
-	num_subs = p_new_subs - new_subs;
-
-	EVT_DEBUG("Event %p num_subscribers %u -> %u", (void*)evt, evt->num_subscribers, num_subs);
-
-	memcpy(evt->subscribers, new_subs, sizeof(new_subs[0]) * num_subs);
-	evt->num_subscribers = num_subs;
+	attr_unused uint prev_num_subs = evt->subscribers.num_elements;
+	dynarray_filter(&evt->subscribers, subscribers_array_predicate, NULL);
+	attr_unused uint new_num_subs = evt->subscribers.num_elements;
+	EVT_DEBUG("Event %p num subscribers %u -> %u", (void*)evt, prev_num_subs, new_num_subs);
 }
 
 static void coevent_add_subscriber(CoEvent *evt, CoTask *task) {
 	EVT_DEBUG("Event %p (num_subscribers=%u; num_subscribers_allocated=%u)", (void*)evt, evt->num_subscribers, evt->num_subscribers_allocated);
 	EVT_DEBUG("Subscriber: %s", task->debug_label);
 
-	evt->num_subscribers++;
-	assert(evt->num_subscribers != 0);
-
-	if(evt->num_subscribers >= evt->num_subscribers_allocated) {
-		if(!evt->num_subscribers_allocated) {
-			evt->num_subscribers_allocated = 4;
-		} else {
-			evt->num_subscribers_allocated *= 2;
-			assert(evt->num_subscribers_allocated != 0);
-		}
-
-		evt->subscribers = realloc(evt->subscribers, sizeof(*evt->subscribers) * evt->num_subscribers_allocated);
-	}
-
-	evt->subscribers[evt->num_subscribers - 1] = cotask_box(task);
+	*dynarray_append_with_min_capacity(&evt->subscribers, 4) = cotask_box(task);
 }
 
 CoWaitResult cotask_wait_event(CoEvent *evt, void *arg) {
@@ -771,10 +749,10 @@ void coevent_signal(CoEvent *evt) {
 	EVT_DEBUG("Signal event %p (uid = %u; num_signaled = %u)", (void*)evt, evt->unique_id, evt->num_signaled);
 	assert(evt->num_signaled != 0);
 
-	if(evt->num_subscribers) {
-		BoxedTask subs_snapshot[evt->num_subscribers];
-		memcpy(subs_snapshot, evt->subscribers, sizeof(subs_snapshot));
-		evt->num_subscribers = 0;
+	if(evt->subscribers.num_elements) {
+		BoxedTask subs_snapshot[evt->subscribers.num_elements];
+		memcpy(subs_snapshot, evt->subscribers.data, sizeof(subs_snapshot));
+		evt->subscribers.num_elements = 0;
 		coevent_wake_subscribers(evt, ARRAY_SIZE(subs_snapshot), subs_snapshot);
 	}
 }
@@ -798,28 +776,14 @@ void coevent_cancel(CoEvent *evt) {
 	evt->num_signaled = 0;
 	evt->unique_id = 0;
 
-	if(evt->num_subscribers) {
-		assume(evt->subscribers != NULL);
-		assume(evt->num_subscribers_allocated > 0);
-		BoxedTask subs_snapshot[evt->num_subscribers];
-		memcpy(subs_snapshot, evt->subscribers, sizeof(subs_snapshot));
-		EVT_DEBUG("[%lu] Snapshot %zu subscribers at %p", ev, ARRAY_SIZE(subs_snapshot), (void*)subs_snapshot);
-		evt->num_subscribers = 0;
-		evt->num_subscribers_allocated = 0;
-		EVT_DEBUG("[%lu] FREE (*%p)->subscribers (%p)", ev, (void*)evt, (void*)evt->subscribers);
-		free(evt->subscribers);
-		evt->subscribers = NULL;
+	if(evt->subscribers.num_elements) {
+		BoxedTask subs_snapshot[evt->subscribers.num_elements];
+		memcpy(subs_snapshot, evt->subscribers.data, sizeof(subs_snapshot));
+		dynarray_free_data(&evt->subscribers);
 		coevent_wake_subscribers(evt, ARRAY_SIZE(subs_snapshot), subs_snapshot);
 		// CAUTION: no modifying evt after this point, it may be invalidated
-	} else if(evt->subscribers) {
-		assume(evt->subscribers != NULL);
-		assume(evt->num_subscribers_allocated > 0);
-		evt->num_subscribers_allocated = 0;
-		free(evt->subscribers);
-		evt->subscribers = NULL;
 	} else {
-		assume(evt->num_subscribers_allocated == 0);
-		assume(evt->subscribers == NULL);
+		dynarray_free_data(&evt->subscribers);
 	}
 
 	EVT_DEBUG("[%lu] END Cancel event %p", ev, (void*)evt);

src/coroutine.h
@@ -13,6 +13,7 @@
 
 #include "entity.h"
 #include "util/debug.h"
+#include "dynarray.h"
 
 #include <koishi.h>
 
@@ -46,16 +47,14 @@ typedef struct CoEvent {
 	// ListAnchor subscribers;
 	// FIXME: Is there a better way than a dynamic array?
 	// An intrusive linked list just isn't robust enough.
-	BoxedTask *subscribers;
+	DYNAMIC_ARRAY(BoxedTask) subscribers;
 	uint32_t unique_id;
-	uint16_t num_signaled;
-	uint8_t num_subscribers;
-	uint8_t num_subscribers_allocated;
+	uint32_t num_signaled;
 } CoEvent;
 
 typedef struct CoEventSnapshot {
 	uint32_t unique_id;
-	uint16_t num_signaled;
+	uint32_t num_signaled;
 } CoEventSnapshot;
 
 #define COEVENTS_ARRAY(...) \

src/credits.c
@@ -15,6 +15,7 @@
 #include "resource/model.h"
 #include "renderer/api.h"
 #include "util/glm.h"
+#include "dynarray.h"
 
 typedef struct CreditsEntry {
 	char **data;
@@ -23,8 +24,7 @@ typedef struct CreditsEntry {
 } CreditsEntry;
 
 static struct {
-	CreditsEntry *entries;
-	int ecount;
+	DYNAMIC_ARRAY(CreditsEntry) entries;
 	float panelalpha;
 	int end;
 	bool skipable;
@@ -48,6 +48,8 @@ static void credits_fill(void) {
 	// In case the shortened URLs break,
 	// Tuck V's YouTube: https://www.youtube.com/channel/UCaw73cuHLnFCSpjOtt_9pyg
 	// InsideI's bandcamp: https://vnutriya.bandcamp.com/
+
+	dynarray_ensure_capacity(&credits.entries, 24);
 
 	credits_add("Taisei Project\nbrought to you by…", HEADER_TIME);
 
@@ -183,8 +185,7 @@ static void credits_add(char *data, int time) {
 
 	assert(time > CREDITS_ENTRY_FADEOUT);
 
-	credits.entries = realloc(credits.entries, (++credits.ecount) * sizeof(CreditsEntry));
-	e = &(credits.entries[credits.ecount-1]);
+	e = dynarray_append(&credits.entries);
 	e->time = time - CREDITS_ENTRY_FADEOUT;
 	e->lines = 1;
 
@@ -290,7 +291,7 @@ static void credits_draw_entry(CreditsEntry *e) {
 	int time = global.frames - 200;
 	float fadein = 1, fadeout = 1;
 
-	for(CreditsEntry *o = credits.entries; o != e; ++o) {
+	for(CreditsEntry *o = credits.entries.data; o != e; ++o) {
 		time -= o->time + CREDITS_ENTRY_FADEOUT;
 	}
 
@@ -398,9 +399,9 @@ static void credits_draw(void) {
 
 	r_shader_standard();
 
-	for(int i = 0; i < credits.ecount; ++i) {
-		credits_draw_entry(&(credits.entries[i]));
-	}
+	dynarray_foreach_elem(&credits.entries, CreditsEntry *e, {
+		credits_draw_entry(e);
+	});
 
 	r_mat_mv_pop();
 
@@ -432,15 +433,14 @@ static void credits_process(void) {
 }
 
 static void credits_free(void) {
-	int i, j;
-	for(i = 0; i < credits.ecount; ++i) {
-		CreditsEntry *e = &(credits.entries[i]);
-		for(j = 0; j < e->lines; ++j)
-			free(e->data[j]);
+	dynarray_foreach_elem(&credits.entries, CreditsEntry *e, {
+		for(int i = 0; i < e->lines; ++i) {
+			free(e->data[i]);
+		}
 		free(e->data);
-	}
+	});
 
-	free(credits.entries);
+	dynarray_free_data(&credits.entries);
 	stage3d_shutdown(&stage_3d_context);
 }
 

src/dynarray.c
@@ -1,0 +1,96 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
+*/
+
+#include "taisei.h"
+
+#include "dynarray.h"
+#include "util.h"
+
+// #define DYNARRAY_DEBUG
+
+#ifdef DYNARRAY_DEBUG
+	#undef DYNARRAY_DEBUG
+	#define DYNARRAY_DEBUG(darr, fmt, ...) log_debug("[%p] " fmt, (void*)(darr), __VA_ARGS__)
+#else
+	#define DYNARRAY_DEBUG(...) ((void)0)
+#endif
+
+void _dynarray_free_data(dynarray_size_t sizeof_element, DynamicArray *darr) {
+	free(darr->data);
+	if(darr->capacity) {
+		DYNARRAY_DEBUG(darr, "%u/%u", darr->num_elements, darr->capacity);
+	}
+	memset(darr, 0, sizeof(*darr));
+}
+
+INLINE void _dynarray_resize(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t capacity) {
+	DYNARRAY_DEBUG(darr, "capacity change: %u --> %u", darr->capacity, capacity);
+	darr->capacity = capacity;
+	darr->data = realloc(darr->data, sizeof_element * capacity);
+}
+
+void _dynarray_ensure_capacity(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t capacity) {
+	if(darr->capacity < capacity) {
+		_dynarray_resize(sizeof_element, darr, capacity);
+	}
+}
+
+dynarray_size_t _dynarray_prepare_append_with_min_capacity(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t min_capacity) {
+	dynarray_size_t num_elements = darr->num_elements;
+	dynarray_size_t capacity = darr->capacity;
+
+	assume(num_elements >= 0);
+	assume(num_elements <= capacity);
+	assume(min_capacity >= 2);
+
+	if(capacity < min_capacity) {
+		_dynarray_resize(sizeof_element, darr, min_capacity);
+	} else if(num_elements == capacity) {
+		capacity += capacity >> 1;
+		_dynarray_resize(sizeof_element, darr, capacity);
+	}
+
+	++darr->num_elements;
+	memset((char*)darr->data + num_elements * sizeof_element, 0, sizeof_element);
+
+	DYNARRAY_DEBUG(darr, "elements: %u/%u", darr->num_elements, darr->capacity);
+	return num_elements;  // insertion index
+}
+
+void _dynarray_compact(dynarray_size_t sizeof_element, DynamicArray *darr) {
+	if(darr->capacity > darr->num_elements) {
+		_dynarray_resize(sizeof_element, darr, darr->num_elements);
+	}
+}
+
+void _dynarray_set_elements(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t num_elements, void *elements) {
+	_dynarray_ensure_capacity(sizeof_element, darr, num_elements);
+	memcpy(&darr->data, elements, num_elements * sizeof_element);
+	darr->num_elements = num_elements;
+}
+
+void _dynarray_filter(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_filter_predicate_t predicate, void *userdata) {
+	char *p = darr->data;
+	char *end = p + sizeof_element * darr->num_elements;
+	dynarray_size_t shift = 0;
+
+	while(p < end) {
+		if(!predicate(p, userdata)) {
+			shift += sizeof_element;
+			--darr->num_elements;
+		} else if(shift) {
+			memmove(p - shift, p, sizeof_element);
+		}
+
+		p += sizeof_element;
+	}
+}
+
+#if 0
+
+#endif

src/dynarray.h
@@ -1,0 +1,187 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
+*/
+
+#ifndef IGUARD_dynarray_h
+#define IGUARD_dynarray_h
+
+#include "taisei.h"
+
+#include "util/macrohax.h"
+
+// TODO document this mess
+
+// NOTE/FIXME: this should be signed to avoid some subtle problems when
+// interacting with other signed values. In the future we should enable implicit
+// signedness conversion warnings and deal with the issue globally.
+typedef int32_t dynarray_size_t;
+
+#define DYNAMIC_ARRAY_BASE(elem_type) struct { \
+	elem_type *data; \
+	dynarray_size_t num_elements; \
+	dynarray_size_t capacity; \
+} \
+
+typedef DYNAMIC_ARRAY_BASE(void) DynamicArray;
+
+#define DYNAMIC_ARRAY(elem_type) union { \
+	DYNAMIC_ARRAY_BASE(elem_type); \
+	DynamicArray dyn_array; \
+}
+
+#ifdef USE_GNU_EXTENSIONS
+	#define DYNARRAY_ASSERT_VALID(darr) do { \
+		static_assert( \
+			__builtin_types_compatible_p(DynamicArray, __typeof__((darr)->dyn_array)), \
+			"x->dyn_array must be of DynamicArray type"); \
+		static_assert( \
+			__builtin_offsetof(__typeof__(*(darr)), dyn_array) == 0, \
+			"x->dyn_array must be the first member in struct"); \
+	} while(0)
+#else
+	#define DYNARRAY_ASSERT_VALID(darr) ((void)0)
+#endif
+
+#ifdef USE_GNU_EXTENSIONS
+	#define DYNARRAY_CAST_TO_BASE(darr) (__extension__ ({ \
+		DYNARRAY_ASSERT_VALID(darr); \
+		&NOT_NULL(darr)->dyn_array; \
+	}))
+#else
+	#define DYNARRAY_CAST_TO_BASE(darr) (&(darr)->dyn_array)
+#endif
+
+#ifdef USE_GNU_EXTENSIONS
+	#define DYNARRAY_CHECK_ELEMENT_TYPE(darr, elem) do { \
+		static_assert( \
+			__builtin_types_compatible_p(__typeof__(elem), __typeof__(*(darr)->data)), \
+			"Dynamic array element type mismatch" \
+		); \
+	} while(0)
+#else
+	#define DYNARRAY_CHECK_ELEMENT_TYPE(darr, elem) do { \
+		static_assert( \
+			sizeof(elem) == sizeof(*(darr)->data), \
+			"Dynamic array element size mismatch" \
+		); \
+	} while(0)
+#endif
+
+#define DYNARRAY_ELEM_SIZE(darr) sizeof(*(darr)->data)
+
+#define _dynarray_dispatch_func(func, darr, ...) \
+	_dynarray_##func(DYNARRAY_ELEM_SIZE(darr), DYNARRAY_CAST_TO_BASE(darr), __VA_ARGS__)
+
+#define _dynarray_dispatch_func_noargs(func, darr) \
+	_dynarray_##func(DYNARRAY_ELEM_SIZE(darr), DYNARRAY_CAST_TO_BASE(darr))
+
+void _dynarray_free_data(dynarray_size_t sizeof_element, DynamicArray *darr) attr_nonnull_all;
+#define dynarray_free_data(darr) \
+	_dynarray_dispatch_func_noargs(free_data, darr)
+
+void _dynarray_ensure_capacity(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t capacity) attr_nonnull_all;
+#define dynarray_ensure_capacity(darr, capacity) \
+	_dynarray_dispatch_func(ensure_capacity, darr, capacity)
+
+dynarray_size_t _dynarray_prepare_append_with_min_capacity(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t min_capacity) attr_nonnull_all;
+
+/*
+ * NOTE: unfortunately this evaluates `darr` more than once, but when compiling
+ * with NDEBUG the assume() macro should trip a clang warning if there are
+ * potential side-effects, and we always treat that warning as an error.
+ */
+#define _dynarray_append_with_min_capacity(darr, min_capacity) \
+	dynarray_get_ptr(darr, _dynarray_dispatch_func(prepare_append_with_min_capacity, darr, min_capacity))
+
+#ifdef USE_GNU_EXTENSIONS
+	#define dynarray_append_with_min_capacity(darr, min_capacity) (__extension__ ({ \
+		assume((darr) != NULL); \
+		_dynarray_append_with_min_capacity((darr), min_capacity); \
+	}))
+#else
+	#define dynarray_append_with_min_capacity(darr, min_capacity) \
+		_dynarray_append_with_min_capacity((darr), min_capacity)
+#endif
+
+#define dynarray_append(darr) \
+	dynarray_append_with_min_capacity(darr, 2)
+
+void _dynarray_compact(dynarray_size_t sizeof_element, DynamicArray *darr) attr_nonnull_all;
+#define dynarray_compact(darr) \
+	_dynarray_dispatch_func_noargs(compact, darr)
+
+#define dynarray_qsort(darr, compare_func) do { \
+	DynamicArray *_darr = DYNARRAY_CAST_TO_BASE(darr); \
+	if(_darr->data) { \
+		qsort(_darr->data, _darr->num_elements, DYNARRAY_ELEM_SIZE(darr), (compare_func)); \
+	} \
+} while(0)
+
+#ifdef USE_GNU_EXTENSIONS
+	#define dynarray_get_ptr(darr, idx) (__extension__ ({ \
+		DYNARRAY_ASSERT_VALID(darr); \
+		__auto_type _darr = NOT_NULL(darr); \
+		dynarray_size_t _darr_idx = (idx); \
+		assume(_darr_idx >= 0); \
+		assume(_darr_idx < _darr->num_elements); \
+		NOT_NULL((darr)->data + _darr_idx); \
+	}))
+#else
+	#define dynarray_get_ptr(darr, idx) ((darr)->data + (idx))
+#endif
+
+#define dynarray_get(darr, idx) (*dynarray_get_ptr(darr, idx))
+#define dynarray_set(darr, idx, ...) ((*dynarray_get_ptr(darr, idx)) = (__VA_ARGS__))
+
+void _dynarray_set_elements(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_size_t num_elements, void *elements) attr_nonnull_all;
+#define dynarray_set_elements(darr, num_elements, elements) do { \
+	DYNARRAY_ASSERT_VALID(darr); \
+	DYNARRAY_CHECK_ELEMENT_TYPE(darr, *(elements)); \
+	_dynarray_dispatch_func(set_elements, darr, num_elements, elements); \
+} while(0)
+
+typedef bool (*dynarray_filter_predicate_t)(const void *pelem, void *userdata);
+void _dynarray_filter(dynarray_size_t sizeof_element, DynamicArray *darr, dynarray_filter_predicate_t predicate, void *userdata) attr_nonnull(2, 3);
+#define dynarray_filter(darr, predicate, userdata) \
+	_dynarray_dispatch_func(filter, darr, predicate, userdata)
+
+#ifdef USE_GNU_EXTENSIONS
+	#define dynarray_indexof(darr, pelem) (__extension__ ({ \
+		DYNARRAY_ASSERT_VALID(darr); \
+		__auto_type _darr = NOT_NULL(darr); \
+		__auto_type _darr_pelem = NOT_NULL(pelem); \
+		DYNARRAY_CHECK_ELEMENT_TYPE(_darr, *(_darr_pelem)); \
+		intptr_t _darr_idx = (intptr_t)(_darr_pelem - _darr->data); \
+		assume(_darr_idx >= 0); \
+		assume(_darr_idx < _darr->num_elements); \
+		(dynarray_size_t)_darr_idx; \
+	}))
+#else
+	#define dynarray_indexof(darr, pelem) ((intptr_t)((pelem) - (darr)->data))
+#endif
+
+#define _dynarray_foreach_iter MACROHAX_ADDLINENUM(_dynarray_foreach_iter)
+#define _dynarray_foreach_ignored MACROHAX_ADDLINENUM(_dynarray_foreach_ignored)
+
+#define dynarray_foreach(_darr, _cntr_var, _pelem_var, ...) do { \
+	for(dynarray_size_t _dynarray_foreach_iter = 0; \
+		_dynarray_foreach_iter < NOT_NULL(_darr)->num_elements; \
+		++_dynarray_foreach_iter \
+	) { \
+		_cntr_var = _dynarray_foreach_iter; \
+		_pelem_var = dynarray_get_ptr((_darr), _dynarray_foreach_iter); \
+		__VA_ARGS__ \
+	} \
+} while(0)
+
+#define dynarray_foreach_elem(_darr, _pelem_var, ...) \
+	dynarray_foreach(_darr, attr_unused dynarray_size_t _dynarray_foreach_ignored, _pelem_var, __VA_ARGS__)
+
+#define dynarray_foreach_idx(_darr, _cntr_var, ...) \
+	dynarray_foreach(_darr, _cntr_var, attr_unused void *_dynarray_foreach_ignored, __VA_ARGS__)
+
+#endif // IGUARD_dynarray_h

src/ending.c
@@ -12,16 +12,16 @@
 #include "global.h"
 #include "video.h"
 #include "progress.h"
+#include "dynarray.h"
 
 typedef struct EndingEntry {
-	char *msg;
+	const char *msg;
 	Sprite *sprite;
 	int time;
 } EndingEntry;
 
 typedef struct Ending {
-	EndingEntry *entries;
-	int count;
+	DYNAMIC_ARRAY(EndingEntry) entries;
 	int duration;
 	int pos;
 	CallChain cc;
@@ -33,18 +33,14 @@ static void track_ending(int ending) {
 }
 
 static void add_ending_entry(Ending *e, int dur, const char *msg, const char *sprite) {
-	EndingEntry *entry;
-	e->entries = realloc(e->entries, (++e->count)*sizeof(EndingEntry));
-	entry = &e->entries[e->count-1];
-
+	EndingEntry *entry = dynarray_append(&e->entries);
 	entry->time = 0;
 
-	if(e->count > 1) {
+	if(e->entries.num_elements > 1) {
 		entry->time = 1<<23; // nobody will ever! find out
 	}
 
-	entry->msg = NULL;
-	stralloc(&entry->msg, msg);
+	entry->msg = msg;
 
 	if(sprite) {
 		entry->sprite = get_sprite(sprite);
@@ -330,6 +326,8 @@ void good_ending_reimu(Ending *e) {
 }
 
 static void init_ending(Ending *e) {
+	dynarray_ensure_capacity(&e->entries, 32);
+
 	if(global.plr.continues_used) {
 		global.plr.mode->character->ending.bad(e);
 	} else {
@@ -337,22 +335,19 @@ static void init_ending(Ending *e) {
 		add_ending_entry(e, 400, "-UNDER CONSTRUCTION-\nExtra Stage coming soon! Please wait warmly for it!", NULL);
 	}
 
-	add_ending_entry(e, 400, "", NULL); // this is important
+	add_ending_entry(e, 400, "", NULL);  // this is important   <-- TODO explain *why* this is important?
 	e->duration = 1<<23;
-}
-
-static void free_ending(Ending *e) {
-	int i;
-	for(i = 0; i < e->count; i++)
-		free(e->entries[i].msg);
-	free(e->entries);
 }
 
 static void ending_draw(Ending *e) {
 	float s, d;
 
-	int t1 = global.frames-e->entries[e->pos].time;
-	int t2 = e->entries[e->pos+1].time-global.frames;
+	EndingEntry *e_this, *e_next;
+	e_this = dynarray_get_ptr(&e->entries, e->pos);
+	e_next = dynarray_get_ptr(&e->entries, e->pos + 1);
+
+	int t1 = global.frames - e_this->time;
+	int t2 = e_next->time - global.frames;
 
 	d = 1.0/ENDING_FADE_TIME;
 
@@ -365,9 +360,9 @@ static void ending_draw(Ending *e) {
 
 	r_color4(s, s, s, s);
 
-	if(e->entries[e->pos].sprite) {
+	if(e_this->sprite) {
 		r_draw_sprite(&(SpriteParams) {
-			.sprite_ptr = e->entries[e->pos].sprite,
+			.sprite_ptr = e_this->sprite,
 			.pos = { SCREEN_W/2, SCREEN_H/2 },
 			.shader = "sprite_default",
 			.color = r_color_current(),
@@ -375,7 +370,7 @@ static void ending_draw(Ending *e) {
 	}
 
 	r_shader("text_default");
-	text_draw_wrapped(e->entries[e->pos].msg, SCREEN_W * 0.85, &(TextParams) {
+	text_draw_wrapped(e_this->msg, SCREEN_W * 0.85, &(TextParams) {
 		.pos = { SCREEN_W/2, VIEWPORT_H*4/5 },
 		.align = ALIGN_CENTER,
 	});
@@ -389,12 +384,17 @@ void ending_preload(void) {
 }
 
 static void ending_advance(Ending *e) {
+	EndingEntry *e_this, *e_next;
+	e_this = dynarray_get_ptr(&e->entries, e->pos);
+	e_next = dynarray_get_ptr(&e->entries, e->pos + 1);
+
 	if(
-		e->pos < e->count-1 &&
-		e->entries[e->pos].time + ENDING_FADE_TIME < global.frames &&
-		global.frames < e->entries[e->pos+1].time - ENDING_FADE_TIME
+		e->pos < e->entries.num_elements - 1 &&
+		e_this->time + ENDING_FADE_TIME < global.frames &&
+		global.frames < e_next->time - ENDING_FADE_TIME
 	) {
-		e->entries[e->pos+1].time = global.frames+(e->pos != e->count-2)*ENDING_FADE_TIME;
+		// TODO find out wtf that last part is for
+		e_next->time = global.frames + (e->pos != e->entries.num_elements - 2) * ENDING_FADE_TIME;
 	}
 }
 
@@ -427,9 +427,9 @@ static LogicFrameAction ending_logic_frame(void *arg) {
 
 	global.frames++;
 
-	if(e->pos < e->count-1 && global.frames >= e->entries[e->pos+1].time) {
+	if(e->pos < e->entries.num_elements - 1 && global.frames >= dynarray_get(&e->entries, e->pos + 1).time) {
 		e->pos++;
-		if(e->pos == e->count-1) {
+		if(e->pos == e->entries.num_elements - 1) {
 			fade_bgm((FPS * ENDING_FADE_OUT) / 4000.0);
 			set_transition(TransFadeWhite, ENDING_FADE_OUT, ENDING_FADE_OUT);
 			e->duration = global.frames+ENDING_FADE_OUT;
@@ -452,8 +452,9 @@ static LogicFrameAction ending_logic_frame(void *arg) {
 static RenderFrameAction ending_render_frame(void *arg) {
 	Ending *e = arg;
 
-	if(e->pos < e->count-1)
+	if(e->pos < e->entries.num_elements - 1) {
 		ending_draw(e);
+	}
 
 	draw_transition();
 	return RFRAME_SWAP;
@@ -462,7 +463,7 @@ static RenderFrameAction ending_render_frame(void *arg) {
 static void ending_loop_end(void *ctx) {
 	Ending *e = ctx;
 	CallChain cc = e->cc;
-	free_ending(e);
+	dynarray_free_data(&e->entries);
 	free(e);
 	progress_unlock_bgm("ending");
 	run_call_chain(&cc, NULL);

src/entity.c
@@ -12,6 +12,7 @@
 #include "util.h"
 #include "renderer/api.h"
 #include "global.h"
+#include "dynarray.h"
 
 typedef struct EntityDrawHook EntityDrawHook;
 typedef LIST_ANCHOR(EntityDrawHook) EntityDrawHookList;
@@ -23,9 +24,7 @@ struct EntityDrawHook {
 };
 
 static struct {
-	EntityInterface **array;
-	uint num;
-	uint capacity;
+	DYNAMIC_ARRAY(EntityInterface*) registered;
 	uint32_t total_spawns;
 
 	struct {
@@ -59,20 +58,17 @@ static void call_hooks(EntityDrawHookList *list, EntityInterface *ent) {
 	}
 }
 
-#define FOR_EACH_ENT(ent) for(EntityInterface **_ent = entities.array, *ent = *entities.array; _ent < entities.array + entities.num; ent = *(++_ent))
-
 void ent_init(void) {
 	memset(&entities, 0, sizeof(entities));
-	entities.capacity = 4096;
-	entities.array = calloc(entities.capacity, sizeof(EntityInterface*));
+	dynarray_ensure_capacity(&entities.registered, 1024);
 }
 
 void ent_shutdown(void) {
-	if(entities.num) {
-		log_fatal_if_debug("%u entities were not properly unregistered, this is a bug!", entities.num);
+	if(entities.registered.num_elements) {
+		log_fatal_if_debug("%u entities were not properly unregistered, this is a bug!", entities.registered.num_elements);
 	}
 
-	free(entities.array);
+	dynarray_free_data(&entities.registered);
 
 	assert(entities.hooks.post_draw.first == NULL);
 	assert(entities.hooks.pre_draw.first == NULL);
@@ -81,29 +77,22 @@ void ent_shutdown(void) {
 void ent_register(EntityInterface *ent, EntityType type) {
 	assert(type > _ENT_TYPE_ENUM_BEGIN && type < _ENT_TYPE_ENUM_END);
 	ent->type = type;
-	ent->index = entities.num++;
 	ent->spawn_id = ++entities.total_spawns;
-
-	assert(ent->spawn_id > 0);
-
-	if(entities.capacity < entities.num) {
-		entities.capacity *= 2;
-		entities.array = realloc(entities.array, entities.capacity * sizeof(EntityInterface*));
-	}
-
-	entities.array[ent->index] = ent;
-
-	assert(ent->index < entities.num);
-	assert(entities.array[ent->index] == ent);
+	ent->index = entities.registered.num_elements;
+	assume(ent->spawn_id > 0);
+	*dynarray_append(&entities.registered) = ent;
 }
 
 void ent_unregister(EntityInterface *ent) {
 	ent->spawn_id = 0;
-	EntityInterface *sub = entities.array[--entities.num];
-	assert(ent->index <= entities.num);
-	assert(entities.array[ent->index] == ent);
+
+	// Fast non-order-preserving removal by moving the last element into the removed element's position.
+
+	assert(ent->index < entities.registered.num_elements);
+	assert(dynarray_get(&entities.registered, ent->index) == ent);
+	EntityInterface *sub = entities.registered.data[--entities.registered.num_elements];
+	entities.registered.data[sub->index = ent->index] = sub;
 	del_ref(ent);
-	entities.array[sub->index = ent->index] = sub;
 }
 
 static int ent_cmp(const void *ptr1, const void *ptr2) {
@@ -126,12 +115,12 @@ static inline bool ent_is_drawable(EntityInterface *ent) {
 
 void ent_draw(EntityPredicate predicate) {
 	call_hooks(&entities.hooks.pre_draw, NULL);
-	qsort(entities.array, entities.num, sizeof(EntityInterface*), ent_cmp);
+	dynarray_qsort(&entities.registered, ent_cmp);
 
 	if(predicate) {
-		FOR_EACH_ENT(ent) {
-			ent->index = _ent - entities.array;
-			assert(entities.array[ent->index] == ent);
+		dynarray_foreach(&entities.registered, int i, EntityInterface **pent, {
+			EntityInterface *ent = *pent;
+			ent->index = i;
 
 			if(ent_is_drawable(ent) && predicate(ent)) {
 				call_hooks(&entities.hooks.pre_draw, ent);
@@ -140,11 +129,11 @@ void ent_draw(EntityPredicate predicate) {
 				r_state_pop();
 				call_hooks(&entities.hooks.post_draw, ent);
 			}
-		}
+		});
 	} else {
-		FOR_EACH_ENT(ent) {
-			ent->index = _ent - entities.array;
-			assert(entities.array[ent->index] == ent);
+		dynarray_foreach(&entities.registered, int i, EntityInterface **pent, {
+			EntityInterface *ent = *pent;
+			ent->index = i;
 
 			if(ent_is_drawable(ent)) {
 				call_hooks(&entities.hooks.pre_draw, ent);
@@ -153,7 +142,7 @@ void ent_draw(EntityPredicate predicate) {
 				r_state_pop();
 				call_hooks(&entities.hooks.post_draw, ent);
 			}
-		}
+		});
 	}
 
 	call_hooks(&entities.hooks.post_draw, NULL);

commit 72c61462a3f8f220d0350a40ba8f8b8c0d7efee7

Fix vfs_dir_list_sorted reporting bogus error when dir is empty
Regression introduced in 0cbc86c6

Fixes #210

src/vfs/public.c
@@ -205,6 +205,7 @@ char** vfs_dir_list_sorted(const char *path, size_t *out_size, int (*compare)(co
 	}
 
 	DYNAMIC_ARRAY(char*) results = { 0 };
+	dynarray_ensure_capacity(&results, 2);
 
 	for(const char *e; (e = vfs_dir_read(dir));) {
 		if(filter && !filter(e)) {