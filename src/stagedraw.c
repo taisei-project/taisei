8db277d7c56adfe82366caf13bb4dbbf7818df52
Andrei Alexeyev <akari@taisei-project.org>
2020-04-25 03:02:36 +0300

    Yumemi slaves/familiars concept

--- src/stagedraw.c
+++ src/stagedraw.c
@@ -615,6 +615,10 @@ static bool powersurge_draw_predicate(EntityInterface *ent) {
 		return i->type == ITEM_VOLTAGE;
 	}
 
+	if(ent->type == ENT_TYPE_ID(YumemiSlave)) {
+		return true;
+	}
+
 	return false;
 }
 
