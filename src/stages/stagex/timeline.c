e5be332ab6a70ac0eb4ebb1d0e3eb605f860ccde
Andrei Alexeyev <akari@taisei-project.org>
2020-04-30 21:48:15 +0300

    Yumemi opening non-spell prototype

--- src/stages/stagex/timeline.c
+++ src/stages/stagex/timeline.c
@@ -86,6 +86,92 @@ TASK(glider_fairy, {
 	}
 }
 
+static void yumemi_slave_aimed_burst(YumemiSlave *slave, real *a, real s) {
+	for(int burst = 0; burst < 25; ++burst) {
+		int cnt = 3;
+		for(int i = 0; i < cnt; ++i) {
+			cmplx pos = slave->pos + 32 * cdir(s * (*a + i*M_TAU/cnt));
+			cmplx aim = cnormalize(global.plr.pos - pos);
+			aim *= 10;
+
+			PROJECTILE(
+				.proto = pp_rice,
+				.color = RGBA(1, 0, 0.25, 0),
+				.pos = pos,
+				.move = move_asymptotic_simple(aim, -2),
+			);
+
+			YIELD;
+		}
+
+		*a += M_PI/32;
+	}
+
+	WAIT(20);
+}
+
+static void yumemi_slave_aimed_lasers(YumemiSlave *slave, real s) {
+	int cnt = 32;
+	int t = 2;
+	real g = carg(global.plr.pos - slave->pos);
+
+	INVOKE_TASK_DELAYED(50, common_play_sfx, "laser1");
+
+	for(int i = 0; i < cnt; ++i) {
+		real x = i/(real)cnt;
+		cmplx o = 32 * cdir(s * x * M_TAU + g + M_PI/2);
+		cmplx pos = slave->pos + o;
+		cmplx aim = cnormalize(global.plr.pos - pos + o);
+		// cmplx aim = I;
+		Color *c = RGBA(0.1 + 0.9 * x * x, 1 - 0.9 * (1 - pow(1 - x, 2)), 0.1, 0);
+		create_laserline(pos, 40 * aim, 60 + i * t, 80 + i * t, c);
+	}
+
+	WAIT(10 + t * cnt);
+}
+
+TASK(yumemi_opening_slave, {
+	cmplx pos;
+	int type;
+	CoEvent *sync_event;
+}) {
+	YumemiSlave *slave = stagex_host_yumemi_slave(ARGS.pos, ARGS.type);
+	WAIT_EVENT_ONCE(ARGS.sync_event);
+
+	WAIT(ARGS.type * 60);
+
+	real s = 1 - 2 * ARGS.type;
+	real a = 0;
+
+	for(;;) {
+		yumemi_slave_aimed_burst(slave, &a, s);
+		yumemi_slave_aimed_lasers(slave, s);
+		WAIT(40);
+	}
+}
+
+TASK_WITH_INTERFACE(yumemi_opening, BossAttack) {
+	Boss *boss = INIT_BOSS_ATTACK(&ARGS);
+	CoEvent *sync_event = &ARGS.attack->events.started;
+
+	cmplx p = VIEWPORT_W/2 + 100*I;
+	real xofs = 140;
+
+	INVOKE_SUBTASK_DELAYED(20, yumemi_opening_slave,
+		.pos = p - xofs,
+		.type = 0,
+		.sync_event = sync_event
+	);
+
+	INVOKE_SUBTASK_DELAYED(40, yumemi_opening_slave,
+		.pos = p + xofs,
+		.type = 1,
+		.sync_event = sync_event
+	);
+
+	STALL;
+}
+
 DEFINE_EXTERN_TASK(stagex_timeline) {
 	YIELD;
 
@@ -94,22 +180,25 @@ DEFINE_EXTERN_TASK(stagex_timeline) {
 	WAIT(3900);
 	STAGE_BOOKMARK(boss);
 
-	global.boss = stagex_spawn_yumemi(VIEWPORT_W/2 + 180*I);
+	Boss *boss = stagex_spawn_yumemi(VIEWPORT_W/2 + 180*I);
+	global.boss = boss;
 
-	boss_add_attack_task(global.boss, AT_SurvivalSpell, "Obliteration “Infinity Network”",
+	Attack *opening_attack = boss_add_attack(boss, AT_Normal, "Opening", 60, 40000, NULL);
+
+	boss_add_attack_task(boss, AT_SurvivalSpell, "Obliteration “Infinity Network”",
 		90, 80000, TASK_INDIRECT(BossAttack, stagex_spell_infinity_network), NULL);
-
-	STALL;
 
 	PlayerMode *pm = global.plr.mode;
 	StageExPreBossDialogEvents *e;
 	INVOKE_TASK_INDIRECT(StageExPreBossDialog, pm->dialog->StageExPreBoss, &e);
 	INVOKE_TASK_WHEN(&e->music_changes, common_start_bgm, "stagexboss");
+	INVOKE_TASK_WHEN(&e->music_changes, yumemi_opening, ENT_BOX(boss), opening_attack);
+
 	WAIT_EVENT(&global.dialog->events.fadeout_began);
 
 	boss_engage(global.boss);
 
-	WAIT_EVENT(&global.boss->events.defeated);
+	WAIT_EVENT(&boss->events.defeated);
 
 enemies:
 	for(int i = 0;;i++) {
