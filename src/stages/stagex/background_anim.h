936ac6147bb32cf0cba1108af32b9f7f1c56a837
Andrei Alexeyev <akari@taisei-project.org>
2020-05-03 03:26:14 +0300

    Add a spell bg for Yumemi

--- src/stages/stagex/background_anim.h
+++ src/stages/stagex/background_anim.h
@@ -13,6 +13,6 @@
 
 #include "coroutine.h"
 
-DECLARE_EXTERN_TASK(stagex_animate_background, NO_ARGS);
+DECLARE_EXTERN_TASK(stagex_animate_background);
 
 #endif // IGUARD_stages_stagex_background_anim_h
