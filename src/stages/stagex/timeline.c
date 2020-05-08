6a9d741766f9a49d3622abf8f0ececaa91fdd433
Andrei Alexeyev <akari@taisei-project.org>
2020-05-08 14:29:00 +0300

    prototype next 2 yumemi nons

--- src/stages/stagex/timeline.c
+++ src/stages/stagex/timeline.c
@@ -11,6 +11,7 @@
 #include "timeline.h"
 #include "yumemi.h"
 #include "stagex.h"
+#include "draw.h"
 #include "stage.h"
 #include "global.h"
 #include "common_tasks.h"
@@ -90,10 +91,13 @@ TASK(glider_fairy, {
 	}
 }
 
-static void yumemi_slave_aimed_burst(YumemiSlave *slave, real *a, real s) {
-	for(int burst = 0; burst < 25; ++burst) {
+static void yumemi_slave_aimed_burst(YumemiSlave *slave, int count, real *a, real s) {
+	for(int burst = 0; burst < count; ++burst) {
 		int cnt = 3;
+
 		for(int i = 0; i < cnt; ++i) {
+			play_sfx_loop("shot1_loop");
+
 			cmplx pos = slave->pos + 32 * cdir(s * (*a + i*M_TAU/cnt));
 			cmplx aim = cnormalize(global.plr.pos - pos);
 			aim *= 10;
@@ -134,6 +138,47 @@ static void yumemi_slave_aimed_lasers(YumemiSlave *slave, real s) {
 	WAIT(10 + t * cnt);
 }
 
+static void yumemi_slave_aimed_funnel(YumemiSlave *slave, int count, real *a, real s) {
+	real max_angle = M_PI/2;
+	real min_angle = M_PI/24;
+
+	for(int burst = 0; burst < count; ++burst) {
+		real bf = burst / (count - 1.0);
+		cmplx rot = cdir(lerp(max_angle, min_angle, bf) * 0.03);
+
+		int cnt = 3;
+		for(int i = 0; i < cnt; ++i) {
+			play_sfx_loop("shot1_loop");
+
+			cmplx pos = slave->pos + 32 * cdir(s * (*a + i*M_TAU/cnt));
+			cmplx aim = cnormalize(global.plr.pos - pos);
+			aim *= 10;
+
+			PROJECTILE(
+				.proto = pp_crystal,
+				.color = RGBA(1, 0.4, 1 - bf, 0),
+				.pos = pos,
+				// .move = move_asymptotic_simple(aim * rot, -2),
+				.move = { .velocity = aim, .retention = rot, },
+			);
+
+			PROJECTILE(
+				.proto = pp_crystal,
+				.color = RGBA(1, 0.4, 1 - bf, 0),
+				.pos = pos,
+				// .move = move_asymptotic_simple(aim / rot, -2),
+				.move = { .velocity = aim, .retention = 1/rot, },
+			);
+
+			YIELD;
+		}
+
+		*a += M_PI/32;
+	}
+
+	WAIT(20);
+}
+
 TASK(yumemi_opening_slave, {
 	cmplx pos;
 	int type;
@@ -148,13 +193,15 @@ TASK(yumemi_opening_slave, {
 	real a = 0;
 
 	for(;;) {
-		yumemi_slave_aimed_burst(slave, &a, s);
+		yumemi_slave_aimed_burst(slave, 25, &a, s);
 		yumemi_slave_aimed_lasers(slave, s);
 		WAIT(40);
 	}
 }
 
 TASK_WITH_INTERFACE(yumemi_opening, BossAttack) {
+	STAGE_BOOKMARK(non1);
+
 	Boss *boss = INIT_BOSS_ATTACK(&ARGS);
 	CoEvent *sync_event = &ARGS.attack->events.started;
 
@@ -179,6 +226,100 @@ TASK_WITH_INTERFACE(yumemi_opening, BossAttack) {
 	STALL;
 }
 
+TASK(yumemi_non2_slave, {
+	cmplx pos;
+	int type;
+	CoEvent *sync_event;
+}) {
+	YumemiSlave *slave = stagex_host_yumemi_slave(ARGS.pos, ARGS.type);
+	WAIT_EVENT_ONCE(ARGS.sync_event);
+
+	WAIT(ARGS.type * 60);
+
+	real s = 1 - 2 * ARGS.type;
+	real a = 0;
+
+	for(;;) {
+		yumemi_slave_aimed_funnel(slave, 20, &a, s);
+		WAIT(10);
+		yumemi_slave_aimed_lasers(slave, s);
+		WAIT(40);
+		// yumemi_slave_aimed_burst(slave, 15, &a, s);
+	}
+}
+
+TASK_WITH_INTERFACE(yumemi_non2, BossAttack) {
+	STAGE_BOOKMARK(non2);
+
+	Boss *boss = INIT_BOSS_ATTACK(&ARGS);
+	CoEvent *sync_event = &ARGS.attack->events.started;
+
+	cmplx p = VIEWPORT_W/2 + 100*I;
+	real xofs = 140;
+
+	INVOKE_SUBTASK_DELAYED(20, yumemi_non2_slave,
+		.pos = p - xofs,
+		.type = 0,
+		.sync_event = sync_event
+	);
+
+	INVOKE_SUBTASK_DELAYED(45, yumemi_non2_slave,
+		.pos = p + xofs,
+		.type = 1,
+		.sync_event = sync_event
+	);
+
+	boss->move = move_towards(VIEWPORT_W/2 + 180*I, 0.015);
+	STALL;
+}
+
+TASK(yumemi_non3_slave, {
+	cmplx pos;
+	int type;
+	CoEvent *sync_event;
+}) {
+	YumemiSlave *slave = stagex_host_yumemi_slave(ARGS.pos, ARGS.type);
+	WAIT_EVENT_ONCE(ARGS.sync_event);
+
+	WAIT(ARGS.type * 60);
+
+	real s = 1 - 2 * ARGS.type;
+	real a = 0;
+
+	for(;;) {
+		yumemi_slave_aimed_funnel(slave, 20, &a, s);
+		WAIT(10);
+		yumemi_slave_aimed_lasers(slave, s);
+		WAIT(40);
+		yumemi_slave_aimed_burst(slave, 15, &a, s);
+	}
+}
+
+TASK_WITH_INTERFACE(yumemi_non3, BossAttack) {
+	STAGE_BOOKMARK(non3);
+
+	Boss *boss = INIT_BOSS_ATTACK(&ARGS);
+	CoEvent *sync_event = &ARGS.attack->events.started;
+
+	cmplx p = VIEWPORT_W/2 + 100*I;
+	real xofs = 140;
+
+	INVOKE_SUBTASK_DELAYED(20, yumemi_non3_slave,
+		.pos = p - xofs,
+		.type = 0,
+		.sync_event = sync_event
+	);
+
+	INVOKE_SUBTASK_DELAYED(45, yumemi_non3_slave,
+		.pos = p + xofs,
+		.type = 1,
+		.sync_event = sync_event
+	);
+
+	boss->move = move_towards(VIEWPORT_W/2 + 180*I, 0.015);
+	STALL;
+}
+
 TASK(yumemi_appear, { BoxedBoss boss; }) {
 	Boss *boss = TASK_BIND(ARGS.boss);
 	boss->move = move_towards(VIEWPORT_W/2 + 180*I, 0.015);
@@ -188,20 +329,27 @@ TASK(boss, { Boss **out_boss; }) {
 	STAGE_BOOKMARK(boss);
 
 	Player *plr = &global.plr;
+	PlayerMode *pm = plr->mode;
+
 	Boss *boss = stagex_spawn_yumemi(5*VIEWPORT_W/4 - 200*I);
 	*ARGS.out_boss = global.boss = boss;
 
+#if 0
 	Attack *opening_attack = boss_add_attack(boss, AT_Normal, "Opening", 60, 40000, NULL);
 	boss_add_attack_from_info(boss, &stagex_spells.boss.sierpinski, false);
 	boss_add_attack_from_info(boss, &stagex_spells.boss.infinity_network, false);
 
-	PlayerMode *pm = plr->mode;
 	StageExPreBossDialogEvents *e;
 	INVOKE_TASK_INDIRECT(StageExPreBossDialog, pm->dialog->StageExPreBoss, &e);
 	INVOKE_TASK_WHEN(&e->boss_appears, yumemi_appear, ENT_BOX(boss));
 	INVOKE_TASK_WHEN(&e->music_changes, common_start_bgm, "stagexboss");
 	INVOKE_TASK_WHEN(&e->music_changes, yumemi_opening, ENT_BOX(boss), opening_attack);
 	WAIT_EVENT_OR_DIE(&global.dialog->events.fadeout_began);
+#else
+	boss_add_attack_task(boss, AT_Normal, "non1", 60, 40000, TASK_INDIRECT(BossAttack, yumemi_opening), NULL);
+	boss_add_attack_task(boss, AT_Normal, "non2", 60, 40000, TASK_INDIRECT(BossAttack, yumemi_non2), NULL);
+	boss_add_attack_task(boss, AT_Normal, "non3", 60, 40000, TASK_INDIRECT(BossAttack, yumemi_non3), NULL);
+#endif
 
 	boss_engage(boss);
 	WAIT_EVENT_OR_DIE(&boss->events.defeated);
@@ -219,7 +367,8 @@ DEFINE_EXTERN_TASK(stagex_timeline) {
 
 	// goto enemies;
 
-	WAIT(3900);
+	// WAIT(3900);
+	stagex_get_draw_data()->tower_global_dissolution = 1;
 
 	Boss *boss;
 	INVOKE_TASK(boss, &boss);
