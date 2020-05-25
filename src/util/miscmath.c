149c40e34a9ac746b6c84cc2d44dc87bd98e783f
Andrei Alexeyev <akari@taisei-project.org>
2020-05-25 06:21:42 +0300

    Add basic functions for cubic Bezier curves

--- src/util/miscmath.c
+++ src/util/miscmath.c
@@ -134,6 +134,18 @@ cmplx cdir(double angle) {
 	#endif
 }
 
+cmplxf cdirf(float angle) {
+	// this is faster than cexp(I*angle)
+
+	#ifdef TAISEI_BUILDCONF_HAVE_SINCOSF
+	float s, c;
+	sincosf(angle, &s, &c);
+	return CMPLXF(c, s);
+	#else
+	return CMPLXF(cosf(angle), sinf(angle));
+	#endif
+}
+
 cmplx cwmul(cmplx c0, cmplx c1) {
 	return CMPLX(creal(c0)*creal(c1), cimag(c0)*cimag(c1));
 }
