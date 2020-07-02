c62d6cd2fcf1a37f31c10bb0fb92fce3c8b4c139
Andrei Alexeyev <akari@taisei-project.org>
2020-07-02 06:07:45 +0300

    stagex: move Yumemi's nons into separate file and extend them (WIP)

---
+++ src/stages/stagex/nonspells/nonspells.h
@@ -0,0 +1,20 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2024, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2024, Andrei Alexeyev <akari@taisei-project.org>.
+ */
+
+#pragma once
+#include "taisei.h"
+
+#include "stages/common_imports.h"   // IWYU pragma: export
+#include "../yumemi.h"   // IWYU pragma: export
+
+DECLARE_EXTERN_TASK_WITH_INTERFACE(stagex_boss_nonspell_1, BossAttack);
+DECLARE_EXTERN_TASK_WITH_INTERFACE(stagex_boss_nonspell_2, BossAttack);
+DECLARE_EXTERN_TASK_WITH_INTERFACE(stagex_boss_nonspell_3, BossAttack);
+DECLARE_EXTERN_TASK_WITH_INTERFACE(stagex_boss_nonspell_4, BossAttack);
+DECLARE_EXTERN_TASK_WITH_INTERFACE(stagex_boss_nonspell_5, BossAttack);
+DECLARE_EXTERN_TASK_WITH_INTERFACE(stagex_boss_nonspell_6, BossAttack);
