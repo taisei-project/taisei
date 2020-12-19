60cbdd818f67fb7b88ff7eea0662ca135dee9bfa
laochailan <laochailan@web.de>
2020-12-19 08:55:26 +0100

    stage2: new PBR background

--- src/stages/stage2/background_anim.c
+++ src/stages/stage2/background_anim.c
@@ -12,19 +12,37 @@
 
 #include "stageutils.h"
 #include "coroutine.h"
+#include "util/glm.h"
+
+TASK(easing_animate, { float *val; float to; int time; glm_ease_t ease; }) {
+
+    float from = *ARGS.val;
+    float scale = ARGS.to - from;
+    float ftime = ARGS.time;
+
+    for(int t = 0; t < ARGS.time;t++) {
+        YIELD;
+        *ARGS.val = from + scale * ARGS.ease(t / ftime);
+    }
+}
 
 TASK(animate_bg_fullstage, NO_ARGS) {
-	stage_3d_context.cx[2] = 1000;
-	stage_3d_context.cx[0] = -850;
-	stage_3d_context.crot[0] = 60;
-	stage_3d_context.crot[2] = -90;
-	stage_3d_context.cv[0] = 9;
+	Camera3D *cam = &stage_3d_context.cam;
+
+	cam->pos[2] = 8;
+	cam->pos[0] = 10;
+	cam->rot.v[0] = 50;
+	cam->rot.v[2] = -90;
+
+	real dur = 600;
+	glm_ease_t func = glm_ease_quad_inout;
+	INVOKE_TASK(easing_animate,&cam->rot.v[0], 40, dur, glm_ease_quad_out);
+	INVOKE_TASK(easing_animate,&cam->rot.v[2], -2.5f, dur, glm_ease_quad_out);
+	INVOKE_TASK(easing_animate,&cam->pos[0], 0, dur, func);
+	INVOKE_TASK(easing_animate,&cam->vel[1], 0.05f, dur, func);
 
 	for(;;) {
 		YIELD;
-		fapproach_p(&stage_3d_context.cv[0], 0.0f, 0.05f);
-		fapproach_p(&stage_3d_context.cv[1], 9.0f, 0.05f);
-		stage_3d_context.crot[2] += fmin(0.5f, -stage_3d_context.crot[2] * 0.02f);
 		stage3d_update(&stage_3d_context);
 	}
 }
@@ -34,10 +52,12 @@ void stage2_bg_init_fullstage(void) {
 }
 
 TASK(animate_bg_spellpractice, NO_ARGS) {
-	stage_3d_context.cx[2] = 1000;
-	stage_3d_context.cx[0] = -44.5;
-	stage_3d_context.crot[0] = 60;
-	stage_3d_context.cv[1] = 9;
+	Camera3D *cam = &stage_3d_context.cam;
+
+	cam->pos[2] = 8;
+	cam->pos[0] = 0;
+	cam->rot.v[0] = 40;
+	cam->vel[1] = 0.05f;
 
 	for(;;) {
 		YIELD;
