60c48e2254c98903c82c090335e433a4832ecf3b
laochailan <laochailan@web.de>
2021-01-03 16:29:00 +0100

    add the pipes to pipe dream (no animation yet)

--- src/stages/stagex/spells/pipe_dream.c
+++ src/stages/stagex/spells/pipe_dream.c
@@ -12,8 +12,46 @@
 #include "global.h"
 #include "common_tasks.h"
 
-TASK(pipe_conversions, { BoxedBoss bbox; BoxedProjectileArray *projs; }) {
+TASK(animate_radii, { real *radius_inner; real *radius_outer; }) {
+	*radius_inner = 100;
+	*radius_outer = 200;
 
+	// TODO when animate easing from pbr_bg is somehow merged
+}
+
+TASK(pipe_conversions, { BoxedBoss bbox; BoxedProjectileArray *projs; }) {
+	Boss *boss = TASK_BIND(ARGS.bbox);
+	real radius_inner = 100;
+	real radius_outer = 200;
+
+	INVOKE_SUBTASK(animate_radii, &radius_inner, &radius_outer);
+
+	for(;;) {
+		ENT_ARRAY_FOREACH(ARGS.projs, Projectile *p, {
+			real r = cabs(boss->pos - p->pos);
+			if(r < radius_inner) {
+				p->move.retention = 1;
+				p->move.attraction = 0;
+				projectile_set_prototype(p, pp_wave);
+				p->color = *RGB(1, 1, 0);
+			} else if(r < radius_outer) {
+				p->move.retention = cdir(0.01);
+				p->move.attraction = 0.0;
+
+				p->move.attraction_point = global.plr.pos;
+				projectile_set_prototype(p, pp_ball);
+				p->color = *RGB(1, 0, 1);
+			} else {
+				p->move.retention = 1.01*cdir(-0.02);
+				p->move.attraction = 0.0;
+
+				projectile_set_prototype(p, pp_bigball);
+				p->color = *RGB(0, 0.5, 1);
+			}
+		});
+		YIELD;
+	}			
+	
 }
 
 DEFINE_EXTERN_TASK(stagex_spell_pipe_dream) {
@@ -31,8 +69,8 @@ DEFINE_EXTERN_TASK(stagex_spell_pipe_dream) {
 		ENT_ARRAY_ADD(&projs, PROJECTILE(
 			.proto = pp_wave,
 			.pos = boss->pos,
-			.color = RGB(1, 0, 0),
-			.move = move_linear(5*cdir(M_TAU/golden/golden*i)),
+			.color = RGB(1, 1, 0),
+			.move = move_linear(3*cdir(M_TAU/golden/golden*i)),
 		));
 		YIELD;
 		if(i % 100 == 0) {
