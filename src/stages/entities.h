d625db78f7a3827c0d6962c1f28f279520b67ae0
Andrei Alexeyev <akari@taisei-project.org>
2021-02-04 07:31:58 +0200

    stage3: basic port of wriggle slaves

---
+++ src/stages/entities.h
@@ -0,0 +1,19 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
+*/
+
+#ifndef IGUARD_stages_entities_h
+#define IGUARD_stages_entities_h
+
+#include "taisei.h"
+
+#include "stage3/entities.h"
+
+#define ENTITIES_STAGES(X, ...) \
+	ENTITIES_STAGE3(X, __VA_ARGS__) \
+
+#endif // IGUARD_stages_entities_h
