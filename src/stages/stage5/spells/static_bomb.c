aa9a1638777426527bd81359a1ee1afe97bf1d88
Alice D <alice@starwitch.productions>
2021-03-28 20:39:48 -0400

    figured out why it didn't work

--- src/stages/stage5/spells/static_bomb.c
+++ src/stages/stage5/spells/static_bomb.c
@@ -10,12 +10,13 @@
 
 #include "spells.h"
 
-TASK(slave_ball_shot, { cmplx pos; }) {
+TASK(slave_ball_shot, { BoxedIkuSlave slave; }) {
+	IkuSlave *slave = TASK_BIND(ARGS.slave);
 	int difficulty = difficulty_value(6, 5, 4, 3);
 	for(int i = 0; i < (210 / difficulty); i++) {
 		PROJECTILE(
 			.proto = pp_soul,
-			.pos = ARGS.pos,
+			.pos = slave->pos,
 			.color = RGBA(0, 0, 1, 0),
 			.move = move_asymptotic_simple(4 * cdir(0.5 * i), 3)
 		);
@@ -24,26 +25,28 @@ TASK(slave_ball_shot, { cmplx pos; }) {
 	}
 }
 
-TASK(slave_laser_shot, { cmplx pos; int delay; }) {
+TASK(slave_laser_shot, { BoxedIkuSlave slave; int delay; }) {
+	IkuSlave *slave = TASK_BIND(ARGS.slave);
 	WAIT(ARGS.delay);
 	int difficulty = difficulty_value(90, 80, 70, 60);
 	int amount = 800 - ARGS.delay;
 	for(int x = 0; x <  (amount / difficulty); x++) {
 		int rand = rng_real();
-		create_laserline(ARGS.pos, 10 * cdir(carg(global.plr.pos - ARGS.pos) + 0.04 * I * (1 - 2 * rand)), 60, 120, RGBA(1, 0.3, 1, 0));
+		create_laserline(slave->pos, 10 * cdir(carg(global.plr.pos - slave->pos) + 0.04 * I * (1 - 2 * rand)), 60, 120, RGBA(1, 0.3, 1, 0));
 		play_sfx_delayed("laser1", 0, true, 45);
 		WAIT(difficulty);
 	}
 }
 
-TASK(slave_bullet_shot, { cmplx pos; }) {
+TASK(slave_bullet_shot, { BoxedIkuSlave slave; }) {
+	IkuSlave *slave = TASK_BIND(ARGS.slave);
 	int difficulty = difficulty_value(5, 4, 3, 2);
 	for(int x = 0; x < (520 / difficulty); x++) {
 		for(int i = 1; i >= -1; i -= 2) {
 			int rand = rng_real();
 			PROJECTILE(
 				.proto = pp_rice,
-				.pos = ARGS.pos,
+				.pos = slave->pos,
 				.color = RGB(1,0,0),
 				.move = move_asymptotic_simple(i * 2 * cdir(-0.3 * x + rand * I), 3)
 			);
@@ -53,12 +56,14 @@ TASK(slave_bullet_shot, { cmplx pos; }) {
 	}
 }
 
-TASK(slave_explode, { cmplx pos; }) {
+TASK(slave_explode, { BoxedIkuSlave slave; }) {
+	IkuSlave *slave = TASK_BIND(ARGS.slave);
+
 	RNG_ARRAY(ray_rand, 5);
 	PARTICLE(
 		.sprite = "blast_huge_rays",
 		.color = color_add(RGBA(0, 0.2 + 0.5 * vrng_real(ray_rand[0]), 0.5 + 0.5 * vrng_real(ray_rand[1]), 0.0), RGBA(1, 1, 1, 0)),
-		.pos = ARGS.pos,
+		.pos = slave->pos,
 		.timeout = 60 + 10 * vrng_real(ray_rand[2]),
 		.draw_rule = pdraw_timeout_fade(1, 0),
 		.angle = vrng_real(ray_rand[4]) * M_TAU,
@@ -69,7 +74,7 @@ TASK(slave_explode, { cmplx pos; }) {
 	RNG_ARRAY(halo_rand, 5);
 	PARTICLE(
 		.sprite = "blast_huge_halo",
-		.pos = ARGS.pos,
+		.pos = slave->pos,
 		.color = RGBA(0.3 * vrng_real(halo_rand[0]), 0.3 * vrng_real(halo_rand[1]), 1.0, 0),
 		.timeout = 200 + 24 * vrng_real(halo_rand[2]),
 		.draw_rule = pdraw_timeout_fade(1, 0),
@@ -93,16 +98,15 @@ TASK(slave, { cmplx pos; int number; }) {
 		.move = move,
 	});
 
-	INVOKE_SUBTASK_DELAYED(100, slave_ball_shot, { .pos = slave->pos });
+	INVOKE_SUBTASK_DELAYED(100, slave_ball_shot, { .slave = ENT_BOX(slave) });
 
-	INVOKE_SUBTASK_DELAYED(200, slave_bullet_shot, { .pos = slave->pos });
+	INVOKE_SUBTASK_DELAYED(200, slave_bullet_shot, { .slave = ENT_BOX(slave) });
 
-	INVOKE_SUBTASK(slave_laser_shot, { .pos = slave->pos, .delay = difficulty_value(500, 470, 440, 410)});
+	INVOKE_SUBTASK(slave_laser_shot, { .slave = ENT_BOX(slave), .delay = difficulty_value(500, 470, 440, 410)});
 
-	INVOKE_SUBTASK_DELAYED(820, slave_explode, { .pos = slave->pos });
+	INVOKE_SUBTASK_DELAYED(880, slave_explode, { .slave = ENT_BOX(slave) });
 
-	WAIT(820);
-
+	WAIT(890);
 	coevent_signal_once(&slave->events.despawned);
 }
 
@@ -119,5 +123,4 @@ DEFINE_EXTERN_TASK(stage5_midboss_iku_explosion) {
 		});
 		WAIT(10);
 	}
-
 }
