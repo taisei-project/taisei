960698c9bbf8444a92a36b817ab03a5c0d20bc0c
Andrei Alexeyev <akari@taisei-project.org>
2021-04-29 20:43:57 +0300

    blind fix for macos crap

--- src/util/platform_posix.c
+++ src/util/platform_posix.c
@@ -14,7 +14,16 @@
 
 void get_system_time(SystemTime *systime) {
 	#if defined(TAISEI_BUILDCONF_HAVE_TIMESPEC)
-	timespec_get(systime, TIME_UTC);
+		#if defined(TAISEI_BUILDCONF_HAVE_BUILTIN_AVAILABLE)
+		if(__builtin_available(macOS 10.15, *)) {
+			timespec_get(systime, TIME_UTC);
+		} else {
+			systime->tv_sec = time(NULL);
+			systime->tv_nsec = 0;
+		}
+		#else
+		timespec_get(systime, TIME_UTC);
+		#endif
 	#else
 	systime->tv_sec = time(NULL);
 	systime->tv_nsec = 0;
