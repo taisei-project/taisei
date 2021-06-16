06384a215e4ec01c61411f7cb48e5cd6cbd9d518
Alice D <alice@starwitch.productions>
2021-06-16 17:14:23 -0400

    attempt to rebase Yumemi branch - several things don't work, but the broad strokes are there, and the level 'plays'

--- src/stages/stagex/stagex.h
+++ src/stages/stagex/stagex.h
@@ -11,9 +11,6 @@
 
 #include "stageinfo.h"
 
-extern StageProcs stagex_procs;
-extern StageProcs stagex_spell_procs;
-
 extern struct stagex_spells_s {
 	// this struct must contain only fields of type AttackInfo
 	// order of fields affects the visual spellstage number, but not its real internal ID
@@ -33,6 +30,9 @@ extern struct stagex_spells_s {
 	// required for iteration
 	AttackInfo null;
 } stagex_spells;
+
+extern StageProcs stagex_procs;
+extern StageProcs stagex_spell_procs;
 
 Boss *stagex_spawn_yumemi(cmplx pos);
 void stagex_draw_yumemi_portrait_overlay(SpriteParams *sp);
