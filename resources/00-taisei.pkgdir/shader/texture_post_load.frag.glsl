b3cd5906735b215da0758b3219cf97c48556cfb4
Lukas Weber <laochailan@web.de>
2021-07-03 14:25:28 +0300

    shaders: remove duplicate color conv functions from texture_post_load

--- resources/00-taisei.pkgdir/shader/texture_post_load.frag.glsl
+++ resources/00-taisei.pkgdir/shader/texture_post_load.frag.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 
 #include "interface/standard.glslh"
+#include "lib/util.glslh"
 
 UNIFORM(1) sampler2D alphamap;
 
@@ -8,28 +9,6 @@ UNIFORM(2) int linearize;
 UNIFORM(3) int multiply_alpha;
 UNIFORM(4) int apply_alphamap;
 UNIFORM(5) int write_srgb;
-
-/*
- * Adapted from https://github.com/tobspr/GLSL-Color-Spaces/blob/master/ColorSpaces.inc.glsl
- */
-
-const float SRGB_ALPHA = 0.055;
-
-float srgb_to_linear(float channel) {
-	if(channel <= 0.04045) {
-		return channel / 12.92;
-	} else {
-		return pow((channel + SRGB_ALPHA) / (1.0 + SRGB_ALPHA), 2.4);
-	}
-}
-
-float linear_to_srgb(float channel) {
-	if(channel <= 0.0031308) {
-		return 12.92 * channel;
-	} else {
-		return (1.0 + SRGB_ALPHA) * pow(channel, 1.0/2.4) - SRGB_ALPHA;
-	}
-}
 
 void main(void) {
 	fragColor = textureLod(tex, texCoordRaw, 0);
