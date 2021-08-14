e3c301fc2222f439e88acdfc84f36a951b0e9819
Andrei Alexeyev <akari@taisei-project.org>
2021-08-15 01:24:53 +0300

    lasers: correct "time" coordinate mapping for texturing

--- resources/00-taisei.pkgdir/shader/lasers/sdf_generate.vert.glsl
+++ resources/00-taisei.pkgdir/shader/lasers/sdf_generate.vert.glsl
@@ -11,7 +11,9 @@ void main(void) {
 	vec2 timeAB = instance_widthAB_timeAB.zw;
 
 	vec2 mid = (a + b) * 0.5;
-	vec2 size = vec2(distance(a, b), 0) + (sdf_range * 2.0 + max(widthAB.x, widthAB.y));
+	float len = distance(a, b);
+	float oversize = sdf_range * 2.0 + max(widthAB.x, widthAB.y);
+	vec2 size = vec2(len, 0) + oversize;
 	float ang = -angle(b - a);
 	mat2 r = mat2(cos(ang), -sin(ang), sin(ang), cos(ang));
 	vec2 pos = r * (position.xy * size) + mid;
@@ -19,7 +21,7 @@ void main(void) {
 	coord = pos;
 	segment = instance_posAB;
 	segment_width = widthAB * 0.5;
-	segment_time = mix(timeAB.x, timeAB.y, position.x + 0.5);
+	segment_time = mix(timeAB.x, timeAB.y, position.x * size.x / len + 0.5);
 
 	gl_Position  = r_projectionMatrix * r_modelViewMatrix * vec4(pos, 0.0, 1.0);
 }
