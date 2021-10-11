a435ea440d18ed86a62a17fdb03c43d417ad102b
Andrei Alexeyev <akari@taisei-project.org>
2021-10-11 16:20:46 +0300

    stage3: use new stage3d pos functions; optimize bg draw order

--- src/stages/stage3/draw.c
+++ src/stages/stage3/draw.c
@@ -15,24 +15,17 @@
 #include "global.h"
 #include "util/glm.h"
 
-MODERNIZE_THIS_FILE_AND_REMOVE_ME
-
 static Stage3DrawData *stage3_draw_data;
 
 Stage3DrawData *stage3_get_draw_data(void) {
 	return NOT_NULL(stage3_draw_data);
 }
 
-static uint stage3_bg_pos(Stage3D *s3d, vec3 pos, float maxrange) {
-	vec3 p = {
-		0,
-		0,
-		0,
-	};
-	// minus sign for drawing order
-	vec3 r = {0, -20, -10};
+static uint stage3_bg_pos(Stage3D *s3d, vec3 cam, float maxrange) {
+	vec3 orig = { 0, 0, 0, };
+	vec3 step = { 0, 20, 10 };
 
-	return linear3dpos(s3d, pos, maxrange, p, r);
+	return stage3d_pos_ray_nearfirst_nsteps(s3d, cam, orig, step, 2, 0);
 }
 
 static void stage3_bg_setup_pbr_lighting(Camera3D *cam) {
@@ -84,9 +77,9 @@ static void stage3_bg_ground_draw(vec3 pos) {
 	PBREnvironment env = { 0 };
 	stage3_bg_setup_pbr_env(&stage_3d_context.cam, &env);
 
-	pbr_draw_model(&stage3_draw_data->models.ground, &env);
 	pbr_draw_model(&stage3_draw_data->models.trees, &env);
 	pbr_draw_model(&stage3_draw_data->models.rocks, &env);
+	pbr_draw_model(&stage3_draw_data->models.ground, &env);
 
 	r_mat_mv_pop();
 	r_state_pop();
@@ -153,7 +146,7 @@ static bool stage3_glitch(Framebuffer *fb) {
 	if(strength > 0) {
 		r_shader("glitch");
 		r_uniform_float("strength", strength);
-		r_uniform_float("frames", global.frames + 15 * nfrand());
+		r_uniform_float("frames", global.frames + 15 * rng_sreal());
 	} else {
 		return false;
 	}
@@ -165,9 +158,10 @@ static bool stage3_glitch(Framebuffer *fb) {
 
 void stage3_draw(void) {
 	Stage3DSegment segments[] = {
-		{ stage3_bg_ground_draw, stage3_bg_pos },
 		{ stage3_bg_leaves_draw, stage3_bg_pos },
+		{ stage3_bg_ground_draw, stage3_bg_pos },
 	};
+	r_clear(CLEAR_COLOR, RGB(0.12, 0.11, 0.10), 1);
 	stage3d_draw(&stage_3d_context, 120, ARRAY_SIZE(segments), segments);
 }
 
