dafcd7606e2439ceb29f5bd5bac8fc2ecda4997f
Andrei Alexeyev <akari@taisei-project.org>
2021-10-20 23:57:28 +0300

    shaders: add standalone tonemapping shader
    This currently uses Uchimura tonemapping, rather than Reinhard as in the
    standard PBR shader. We may switch the PBR shader to Uchimura as well
    later.

---
+++ resources/00-taisei.pkgdir/shader/tonemap.frag.glsl
@@ -0,0 +1,14 @@
+
+#version 330 core
+
+#include "lib/pbr.glslh"
+#include "interface/standard.glslh"
+
+UNIFORM(1) vec3 exposure;
+
+void main(void) {
+	vec4 c = texture(tex, texCoord);
+	c.rgb = PBR_TonemapUchimura(exposure * c.rgb);
+	c.rgb = PBR_GammaCorrect(c.rgb);
+	fragColor = c;
+}
