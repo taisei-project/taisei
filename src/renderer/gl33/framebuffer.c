3fd3c33371b0a7a7ec9e9b1e97fa25deefbfa0bb
Andrei Alexeyev <akari@taisei-project.org>
2022-09-05 06:23:35 +0200

    gl33: fix inverted logic in gl33_framebuffer_attach

--- src/renderer/gl33/framebuffer.c
+++ src/renderer/gl33/framebuffer.c
@@ -74,7 +74,7 @@ void gl33_framebuffer_attach(Framebuffer *framebuffer, Texture *tex, uint mipmap
 	framebuffer->attachment_mipmaps[attachment] = mipmap;
 
 	// need to update draw buffers
-	framebuffer->draw_buffers_dirty = false;
+	framebuffer->draw_buffers_dirty = true;
 
 	IF_DEBUG(if(tex) {
 		log_debug("%s %s = %s (%ux%u mip %u)", framebuffer->debug_label, attachment_str(attachment), tex->debug_label, tex->params.width, tex->params.height, mipmap);
