072a9823f932a07e85a6ae6e87e6f0677707851b
Andrei Alexeyev <akari@taisei-project.org>
2022-12-30 18:48:41 +0100

    coroutine: fix missing license header

--- src/coroutine/taskdsl.c
+++ src/coroutine/taskdsl.c
@@ -1,3 +1,12 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2019, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
+*/
+
+#include "taisei.h"
 
 #include "taskdsl.h"
 
