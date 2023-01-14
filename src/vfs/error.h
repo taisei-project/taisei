2b30973e92aab5fcf37c89c651a36acafc02d1fe
Andrei Alexeyev <akari@taisei-project.org>
2023-01-14 14:59:07 +0100

    vfs: disable logging of vfs_set_error() calls by default
    This is very verbose and only useful for VFS debugging.

--- src/vfs/error.h
+++ src/vfs/error.h
@@ -11,10 +11,14 @@
 
 #include "log.h"
 
+// #define VFS_LOG_ERRORS
+
 void vfs_set_error(char *fmt, ...) attr_printf(1, 2);
 void vfs_set_error_from_sdl(void);
 
+#ifdef VFS_LOG_ERRORS
 #define vfs_set_error(fmt, ...) ({ \
 	log_debug("vfs_set_error: " fmt, ##__VA_ARGS__); \
 	vfs_set_error(fmt, ##__VA_ARGS__); \
 })
+#endif
