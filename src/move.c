taisei-project/taisei

commit 672ea9969196c8d0aa2088986d9b946b5d68f274

move: dubious move_update optimizations

src/move.c
@@ -12,18 +12,25 @@
 #include "util/miscmath.h"
 
 cmplx move_update(cmplx *restrict pos, MoveParams *restrict p) {
-	cmplx v = p->velocity;
+	MoveParams o = *p;
+	cmplx orig_velocity = o.velocity;
+	*pos += orig_velocity;
+	o.velocity = o.acceleration + o.retention * o.velocity;
 
-	*pos += v;
-	p->velocity = p->acceleration + p->retention * v;
+	if(o.attraction) {
+		cmplx av = o.attraction_point - *pos;
 
-	if(p->attraction) {
-		cmplx av = p->attraction_point - *pos;
-
-		p->velocity += p->attraction * cnormalize(av) * pow(cabs(av), p->attraction_exponent);
+		if(LIKELY(o.attraction_exponent == 1)) {
+			o.velocity += o.attraction * av;
+		} else {
+			real m = sqrt(creal(av) * creal(av) + cimag(av) * cimag(av));
+			m = pow(m, o.attraction_exponent) / m;
+			o.velocity += o.attraction * (av * m);
+		}
 	}
 
-	return v;
+	p->velocity = o.velocity;
+	return orig_velocity;
 }
 
 cmplx move_update_multiple(uint times, cmplx *restrict pos, MoveParams *restrict p) {

commit 14a370e9404d2600d0fd26a4c3d8adc2ead6e873

move: fix attraction_exponent != 1 case
Was broken by 672ea996. The optimizations were indeed dubious after all.

src/move.c
@@ -23,8 +23,10 @@ cmplx move_update(cmplx *restrict pos, MoveParams *restrict p) {
 		if(LIKELY(o.attraction_exponent == 1)) {
 			o.velocity += o.attraction * av;
 		} else {
-			real m = sqrt(creal(av) * creal(av) + cimag(av) * cimag(av));
-			m = pow(m, o.attraction_exponent) / m;
+			real m = cabs2(av);
+			assume(m >= 0);
+			m = pow(m, o.attraction_exponent - 0.5);
+			assume(isfinite(m));
 			o.velocity += o.attraction * (av * m);
 		}
 	}