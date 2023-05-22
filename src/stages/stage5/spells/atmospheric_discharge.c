46a4733b90488f1b97f71794277065cdbc90c100
Andrei Alexeyev <akari@taisei-project.org>
2023-05-22 21:31:09 +0200

    stage5: redesign atmospheric discharge

--- src/stages/stage5/spells/atmospheric_discharge.c
+++ src/stages/stage5/spells/atmospheric_discharge.c
@@ -10,47 +10,33 @@
 
 #include "spells.h"
 
-TASK(cloud_shoot) {
-	int delay = difficulty_value(19, 17, 15, 13);
-	for(;;WAIT(delay)) {
-		if(global.diff >= D_Hard) {
-			PROJECTILE(
-				.proto = pp_thickrice,
-				.pos = VIEWPORT_W * rng_real(),
-				.color = RGB(0, 0.3, 0.7),
-				.move = move_accelerated(0, 0.01 * I),
-			);
-		} else {
-			PROJECTILE(
-				.proto = pp_rice,
-				.pos = VIEWPORT_W * rng_real(),
-				.color = RGB(0, 0.3, 0.7),
-				.move = move_linear(2 * I),
-			);
-		}
-	}
-}
-
-TASK(ball_shoot) {
-	int shot_angle = difficulty_value(140, 160, 180, 200);
+TASK(ball_shoot, { cmplx p1; cmplx p2; }) {
 	int shot_count = difficulty_value(7, 8, 9, 10);
 	int shot_accel = difficulty_value(5, 6, 7, 8);
-	int delay = difficulty_value(19, 17, 15, 13);
-	for(;;WAIT(delay)) {
-		RNG_ARRAY(rand, 4);
-		cmplx p1 = VIEWPORT_W * vrng_real(rand[0]) + VIEWPORT_H/2 * I * vrng_real(rand[1]);
-		cmplx p2 = p1 + shot_angle * cdir(0.5 * vrng_sreal(rand[2])) * (1 - 2 * (vrng_chance(rand[3], 0.5)));
+	int delay = 10;
+	int waves = difficulty_value(7, 8, 9, 10);
 
-		for(int i = -shot_count * 0.5; i <= shot_count * 0.5; i++) {
+	cmplx p1 = ARGS.p1;
+	cmplx p2 = ARGS.p2;
+
+	for(int n = 0; n < waves * 2; ++n) {
+		for(real i = -shot_count * 0.5; i <= shot_count * 0.5; i++) {
 			PROJECTILE(
 				.proto = pp_ball,
 				.pos = p1 + (p2 - p1) / shot_count * i,
-				.color = RGBA(1 - 1 / (1 + fabs(0.1 * i)), 0.5 - 0.1 * abs(i), 1, 0),
-				.move = move_accelerated(0, (0.001 * shot_accel) * cnormalize(p2 - p1) * cdir(M_PI/2 + 0.2 * i)),
+				.color = RGBA(1 - 1 / (1 + fabs(0.1 * i)), 0.5 - 0.1 * fabs(i), 1, 0),
+				.move = move_accelerated(0,
+					(1 - 2 * (n & 1)) *
+					(0.001 * shot_accel) *
+					cnormalize(p2 - p1) * cdir(M_PI/2 + 0.2 * i)
+				),
 			);
 		}
+
 		play_sfx("shot_special1");
 		play_sfx("redirect");
+
+		WAIT(delay);
 	}
 }
 
@@ -59,15 +45,17 @@ DEFINE_EXTERN_TASK(stage5_spell_atmospheric_discharge) {
 	boss->move = move_from_towards(boss->pos, VIEWPORT_W/2 + 200.0 * I, 0.06);
 	BEGIN_BOSS_ATTACK(&ARGS);
 
-	Rect wander_bounds = viewport_bounds(64);
-	wander_bounds.top += 64;
-	wander_bounds.bottom = VIEWPORT_H * 0.4;
-
-	INVOKE_SUBTASK(ball_shoot);
-	INVOKE_SUBTASK(cloud_shoot);
+	Rect wander_bounds = viewport_bounds(32);
+	wander_bounds.bottom = VIEWPORT_H * 0.5;
 
 	boss->move.attraction = 0.03;
-	for(;;WAIT(100)) {
-		boss->move.attraction_point = common_wander(boss->pos, VIEWPORT_W * 0.3, wander_bounds);
+
+	int mindelay = 30;
+	int delay_step = 6;
+	int delay = mindelay + 10 * delay_step;
+
+	for(;;WAIT(delay)) {
+		boss->move.attraction_point = common_wander(boss->pos, VIEWPORT_W * 0.4, wander_bounds);	INVOKE_SUBTASK_DELAYED(10, ball_shoot, boss->pos, boss->move.attraction_point);
+		delay = imax(delay - delay_step, mindelay);
 	}
 }
