83708429af67554e0ae4794077f363ede0696099
Andrei Alexeyev <akari@taisei-project.org>
2023-05-26 02:27:14 +0200

    util/geometry: remove bogus attr_pure from rect_join

--- src/util/geometry.h
+++ src/util/geometry.h
@@ -155,5 +155,5 @@ bool point_in_rect(cmplx p, Rect r);
 bool rect_in_rect(Rect inner, Rect outer) attr_const;
 bool rect_rect_intersect(Rect r1, Rect r2, bool edges, bool corners) attr_const;
 bool rect_rect_intersection(Rect r1, Rect r2, bool edges, bool corners, Rect *out) attr_nonnull(5);
-bool rect_join(Rect *r1, Rect r2) attr_pure attr_nonnull(1);
+bool rect_join(Rect *r1, Rect r2) attr_nonnull(1);
 void rect_set_xywh(Rect *rect, double x, double y, double w, double h) attr_nonnull(1);
