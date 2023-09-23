1b5feddd97afa27a11c4f3818e9a5114e381650e
Andrei Alexeyev <akari@taisei-project.org>
2023-09-23 13:59:51 +0200

    move: use cmul_finite in move_update

--- src/move.c
+++ src/move.c
@@ -15,19 +15,19 @@ cmplx move_update(cmplx *restrict pos, MoveParams *restrict p) {
 	MoveParams o = *p;
 	cmplx orig_velocity = o.velocity;
 	*pos += orig_velocity;
-	o.velocity = o.acceleration + o.retention * o.velocity;
+	o.velocity = o.acceleration + cmul_finite(o.retention, o.velocity);
 
 	if(o.attraction) {
 		cmplx av = o.attraction_point - *pos;
 
 		if(LIKELY(o.attraction_exponent == 1)) {
-			o.velocity += o.attraction * av;
+			o.velocity += cmul_finite(o.attraction, av);
 		} else {
 			real m = cabs2(av);
 			assume(m >= 0);
 			m = pow(m, o.attraction_exponent - 0.5);
 			assert(isfinite(m));
-			o.velocity += o.attraction * (av * m);
+			o.velocity += cmul_finite(o.attraction, av * m);
 		}
 	}
 
