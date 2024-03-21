e28aa512b66bb0ee7748a64e875f3168c5367aeb
Andrei Alexeyev <akari@taisei-project.org>
2024-03-21 23:24:07 +0100

    lasers/draw: sort before packing and allow rotation in sdf texture
    Slightly improves packing efficiency

--- resources/00-taisei.pkgdir/shader/lasers/sdf_apply.vert.glsl
+++ resources/00-taisei.pkgdir/shader/lasers/sdf_apply.vert.glsl
@@ -7,9 +7,22 @@
 void main(void) {
 	vec2 midpoint = instance_bbox.xy;
 	vec2 size = instance_bbox.zw;
+
+	bool rotated = size.x < 0;
+
+	if(rotated) {
+		size.x = -size.x;
+	}
+
 	vec2 vert_pos = vertex_position * size + midpoint;
 	vec2 vert_tc = flip_native_to_topleft(vertex_uv);
-	vert_tc = (vert_tc * size + instance_texofs) / texsize;
+	vert_tc = vert_tc * size;
+
+	if(rotated) {
+		vert_tc = vert_tc.yx;
+	}
+
+	vert_tc = (vert_tc + instance_texofs) / texsize;
 	vert_tc = flip_topleft_to_native(vert_tc);
 
 	gl_Position = r_projectionMatrix * r_modelViewMatrix * vec4(vert_pos, 0.0, 1.0);
