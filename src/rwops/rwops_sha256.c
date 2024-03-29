e0a708986974db711d218af92707bfbe112e83e8
Andrei Alexeyev <akari@taisei-project.org>
2024-03-29 03:07:41 +0100

    rwops: convert sha256

--- src/rwops/rwops_sha256.c
+++ src/rwops/rwops_sha256.c
@@ -8,85 +8,84 @@
 
 #include "rwops_sha256.h"
 
-struct sha256_data {
+struct sha256_ctx {
 	SDL_IOStream *src;
 	SHA256State *sha256;
 	bool autoclose;
 };
 
-#define DATA(rw) ((struct sha256_data*)((rw)->hidden.unknown.data1))
+static int rwsha256_close(void *ctx) {
+	struct sha256_ctx *sctx = ctx;
 
-static int rwsha256_close(SDL_IOStream *rw) {
-	if(DATA(rw)->autoclose) {
-		SDL_CloseIO(DATA(rw)->src);
+	if(sctx->autoclose) {
+		SDL_CloseIO(sctx->src);
 	}
 
-	mem_free(DATA(rw));
-	SDL_FreeRW(rw);
+	mem_free(sctx);
 	return 0;
 }
 
-static int64_t rwsha256_seek(SDL_IOStream *rw, int64_t offset, int whence) {
+static int64_t rwsha256_seek(void *ctx, int64_t offset, int whence) {
+	struct sha256_ctx *sctx = ctx;
+
 	if(!offset && whence == SDL_IO_SEEK_CUR) {
-		return SDL_SeekIO(DATA(rw)->src, offset, whence);
+		return SDL_SeekIO(sctx->src, offset, whence);
 	}
 
 	SDL_SetError("Stream is not seekable");
 	return -1;
 }
 
-static int64_t rwsha256_size(SDL_IOStream *rw) {
-	return SDL_SizeIO(DATA(rw)->src);
+static int64_t rwsha256_size(void *ctx) {
+	struct sha256_ctx *sctx = ctx;
+	return SDL_GetIOSize(sctx->src);
 }
 
-static void rwsha256_update_sha256(SDL_IOStream *rw, const void *data,
-				   size_t size, size_t maxnum) {
-	assert(size <= UINT32_MAX);
-	assert(maxnum <= UINT32_MAX);
-	assert(size * maxnum <= UINT32_MAX);
-	sha256_update(DATA(rw)->sha256, data, size * maxnum);
+static void rwsha256_update_sha256(struct sha256_ctx *sctx, const void *data, size_t size) {
+	sha256_update(sctx->sha256, data, size);
 }
 
-static size_t rwsha256_read(SDL_IOStream *rw, void *ptr, size_t size,
-			    size_t maxnum) {
-	size_t result = /* FIXME MIGRATION: double-check if you use the returned value of SDL_ReadIO() */
-		SDL_ReadIO(DATA(rw)->src, ptr, size * maxnum);
-	rwsha256_update_sha256(rw, ptr, size, result);
+static size_t rwsha256_read(void *ctx, void *ptr, size_t size, SDL_IOStatus *status) {
+	struct sha256_ctx *sctx = ctx;
+	size_t result = SDL_ReadIO(sctx->src, ptr, size);
+	*status = SDL_GetIOStatus(sctx->src);
+	rwsha256_update_sha256(sctx, ptr, result);
 	return result;
 }
 
-static size_t rwsha256_write(SDL_IOStream *rw, const void *ptr, size_t size,
-			     size_t maxnum) {
-	rwsha256_update_sha256(rw, ptr, size, maxnum);
-	return /* FIXME MIGRATION: double-check if you use the returned value of SDL_WriteIO() */
-	SDL_WriteIO(DATA(rw)->src, ptr, size * maxnum);
+static size_t rwsha256_write(void *ctx, const void *ptr, size_t size, SDL_IOStatus *status) {
+	struct sha256_ctx *sctx = ctx;
+	size_t result = SDL_WriteIO(sctx->src, ptr, size);
+	*status = SDL_GetIOStatus(sctx->src);
+	rwsha256_update_sha256(sctx, ptr, result);
+	return result;
 }
 
-SDL_IOStream *SDL_RWWrapSHA256(SDL_IOStream *src, SHA256State *sha256,
-			       bool autoclose) {
+SDL_IOStream *SDL_RWWrapSHA256(SDL_IOStream *src, SHA256State *sha256, bool autoclose) {
 	if(UNLIKELY(!src)) {
 		return NULL;
 	}
 
-	SDL_IOStream *rw = SDL_AllocRW();
+	SDL_IOStreamInterface iface = {
+		.size = rwsha256_size,
+		.seek = rwsha256_seek,
+		.close = rwsha256_close,
+		.read = rwsha256_read,
+		.write = rwsha256_write,
+	};
 
-	if(UNLIKELY(!rw)) {
+	auto sctx = ALLOC(struct sha256_ctx, {
+		.autoclose = autoclose,
+		.sha256 = sha256,
+		.src = src,
+	});
+
+	SDL_IOStream *io = SDL_OpenIO(&iface, sctx);
+
+	if(UNLIKELY(!io)) {
+		mem_free(sctx);
 		return NULL;
 	}
 
-	memset(rw, 0, sizeof(SDL_IOStream));
-
-	rw->type = SDL_RWOPS_UNKNOWN;
-	rw->hidden.unknown.data1 = ALLOC(struct sha256_data);
-	DATA(rw)->src = src;
-	DATA(rw)->sha256 = sha256;
-	DATA(rw)->autoclose = autoclose;
-
-	rw->size = rwsha256_size;
-	rw->seek = rwsha256_seek;
-	rw->close = rwsha256_close;
-	rw->read = rwsha256_read;
-	rw->write = rwsha256_write;
-
-	return rw;
+	return io;
 }
