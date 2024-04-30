9bcd32da16aa8a38666925f2796b120b2037b241
Andrei Alexeyev <akari@taisei-project.org>
2024-05-01 01:20:34 +0200

    resources/shader/extra_glitch: fix gles30 compat and clean up

--- resources/00-taisei.pkgdir/shader/extra_glitch.frag.glsl
+++ resources/00-taisei.pkgdir/shader/extra_glitch.frag.glsl
@@ -3,14 +3,12 @@
 #include "lib/render_context.glslh"
 #include "interface/standard.glslh"
 
-// UNIFORM(1) vec3 time_offsets = vec3(0.143133, 0.53434, 0.25332);
-// UNIFORM(2) vec3 time_scales = vec3(1.0);
-
 UNIFORM(1) vec3 times;
-UNIFORM(2) vec3 cutoffs = vec3(0.53, 0.56, 0.51);
-UNIFORM(3) vec2 grid = vec2(8.0, 16.0);
-UNIFORM(4) vec3 waverectfactors = vec3(7.0);
-UNIFORM(5) sampler2D mask;
+UNIFORM(2) sampler2D mask;
+
+const vec3 cutoffs = vec3(0.53, 0.56, 0.51);
+const vec2 grid = vec2(8.0, 16.0);
+const vec3 waverectfactors = vec3(7.0);
 
 float hash(vec2 co) {
 	float a = 12.9898;
@@ -71,34 +69,7 @@ vec3 glitch(vec2 uv, vec3 time, vec2 grid, vec3 cutoffs, vec3 waverectfactors) {
 	// mask *= v;
 	return mask;
 }
-#if 0
-void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 uv = fragCoord/iResolution.xy;
 
-    vec2 grid = vec2(8.0, 16.0);
-    vec3 cutoffs = vec3(0.53, 0.56, 0.51) * 2.0 * iMouse.x/iResolution.x;
-    vec3 timeofs = vec3(0.143133, 0.53434, 0.25332) * 0.1;
-    vec3 waverectfactors = vec3(7.0);
-    vec3 k = glitch(uv, iTime, grid, cutoffs, timeofs, waverectfactors);
-
-    vec4 tex = texture(iChannel1, uv);
-    vec4 tex_ofs = vec4(1);// = texture(iChannel1, uv - 1.0 / grid);
-
-    vec3 o = sin(3.14*2.0*k);
-
-    tex_ofs.r = texture(iChannel1, uv - o.r / grid).r;
-    tex_ofs.g = texture(iChannel1, uv - o.g / grid).g;
-    tex_ofs.b = texture(iChannel1, uv - o.b / grid).b;
-
-    tex.rgb = mix(tex.rgb, tex_ofs.rgb, k);
-    // tex.rgb = k;
-
-
-    // tex.r = A;
-
-    fragColor = tex;
-}
-#endif
 void main(void) {
 	vec2 uv = texCoord;
 
@@ -109,12 +80,8 @@ void main(void) {
 	if(a == 0) {
 		fragColor = texNormal;
 		return;
-	} else {
-		// fragColor = vec4(a,0,0,1);
-		// return;
 	}
 
-	// vec3 t = float(frames) / 60.0 * time_scales + time_offsets;
 	vec3 k = glitch(uv, times, grid, cutoffs, waverectfactors);
 	vec3 o = k * a * 0.25;
 
