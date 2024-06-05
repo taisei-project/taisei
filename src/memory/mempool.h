d7cce0bf782b4ff7273194826f5d003af619fb79
Andrei Alexeyev <akari@taisei-project.org>
2024-06-05 22:07:17 +0200

    memory/mempool: pass arena pointer to acquire() instead of storing in struct

--- src/memory/mempool.h
+++ src/memory/mempool.h
@@ -16,7 +16,6 @@ typedef struct MemPoolObjectHeader {
 } MemPoolObjectHeader;
 
 #define MEMPOOL_BASE(elem_type) struct { \
-	MemArena *arena; \
 	union { \
 		MemPoolObjectHeader *as_generic; \
 		elem_type *as_specific; \
@@ -49,22 +48,17 @@ typedef MEMPOOL_BASE(max_align_t) MemPool;
 #define MEMPOOL_OBJTYPE(mpool) \
 	typeof(*(mpool)->free_objects.as_specific)
 
-void mempool_generic_init(MemPool *pool, MemArena *arena)
-	attr_nonnull(1, 2);
-
-void *mempool_generic_acquire(MemPool *pool, size_t size, size_t align)
-	attr_returns_allocated attr_hot attr_nonnull(1);
+void *mempool_generic_acquire(MemPool *pool, MemArena *arena, size_t size, size_t align)
+	attr_returns_allocated attr_hot attr_nonnull(1, 2);
 
 void mempool_generic_release(MemPool *pool, void *object)
 	attr_hot attr_nonnull(1, 2);
 
-#define mempool_init(mpool, arena) \
-	mempool_generic_init(MEMPOOL_CAST_TO_BASE(mpool), (arena));
-
-#define mempool_acquire(mpool) ({ \
+#define mempool_acquire(mpool, arena) ({ \
 	auto _mpool = mpool; \
 	MEMPOOL_OBJTYPE(_mpool) *_obj = mempool_generic_acquire(\
 		MEMPOOL_CAST_TO_BASE(_mpool), \
+		(arena), \
 		sizeof(MEMPOOL_OBJTYPE(_mpool)), \
 		alignof(MEMPOOL_OBJTYPE(_mpool))); \
 	*_obj = (typeof(*_obj)) {} ; \
