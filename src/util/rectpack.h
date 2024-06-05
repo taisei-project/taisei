3281af2b6219a3c9efaa9f005d51de48c5eb905a
Andrei Alexeyev <akari@taisei-project.org>
2024-06-05 23:53:30 +0200

    util/rectpack: refactor to use standard mempools

--- src/util/rectpack.h
+++ src/util/rectpack.h
@@ -11,50 +11,47 @@
 
 #include "geometry.h"
 #include "list.h"
-#include "memory/allocator.h"
+#include "memory/mempool.h"
 
 typedef struct RectPack RectPack;
 typedef struct RectPackSection RectPackSection;
+typedef MEMPOOL(RectPackSection) RectPackSectionPool;
+typedef struct RectPackSectionSource RectPackSectionSource;
 
 struct RectPackSection {
 	LIST_INTERFACE(RectPackSection);
-	Rect rect;
 	RectPackSection *parent;
 	RectPackSection *sibling;
 	RectPackSection *children[2];
+	Rect rect;
 };
 
 struct RectPack {
 	RectPackSection root;
 	RectPackSection *unused_sections;
-	RectPackSection *sections_freelist;
-	Allocator *allocator;
 };
 
-void rectpack_init(RectPack *rp, Allocator *alloc, double width, double height)
+struct RectPackSectionSource {
+	RectPackSectionPool *pool;
+	MemArena *arena;
+};
+
+void rectpack_init(RectPack *rp, double width, double height)
 	attr_nonnull_all;
 
-void rectpack_reset(RectPack *rp)
-	attr_nonnull(1);
-
-void rectpack_deinit(RectPack *rp)
-	attr_nonnull(1);
-
-RectPackSection *rectpack_add(RectPack *rp, double width, double height, bool allow_rotation)
-	attr_nonnull(1);
-
-#define _rectpack_add_3(rp, width, height) \
-	rectpack_add(rp, width, height, false)
-#define _rectpack_add_4(rp, width, height, allow_rotation) \
-	rectpack_add(rp, width, height, allow_rotation)
-#define rectpack_add(...) \
-	MACROHAX_OVERLOAD_NARGS(_rectpack_add_, __VA_ARGS__)(__VA_ARGS__)
+RectPackSection *rectpack_add(
+	RectPack *rp,
+	RectPackSectionSource secsrc,
+	double width,
+	double height,
+	bool allow_rotation
+) attr_nonnull(1);
 
 Rect rectpack_section_rect(RectPackSection *s)
 	attr_nonnull(1);
 
-void rectpack_reclaim(RectPack *rp, RectPackSection *s)
-	attr_nonnull(1, 2);
+void rectpack_reclaim(RectPack *rp, RectPackSectionSource secsrc, RectPackSection *s)
+	attr_nonnull(1, 3);
 
 bool rectpack_is_empty(RectPack *rp)
 	attr_nonnull(1);
