c9ab93c7ee1c7d70b8b2ea136d0e352d8befbdaf
Andrei Alexeyev <akari@taisei-project.org>
2024-07-22 21:36:39 +0200

    shaderlib: compile vulkan spirv with relaxed option and update cache structure

--- src/renderer/common/shaderlib/lang_spirv_aux.c
+++ src/renderer/common/shaderlib/lang_spirv_aux.c
@@ -12,32 +12,103 @@
 
 #include "log.h"
 #include "util.h"
+#include "util/stringops.h"
+
+typedef struct CacheEntryMetadata {
+	ShaderLangInfo lang;
+	ShaderStage stage;
+
+	union {
+		struct {
+			const SPIRVCompileOptions *compile_options;
+		} spirv;
+
+		struct {
+		} glsl;
+	};
+} CacheEntryMetadata;
+
+static size_t shader_cache_serialize_entry_metadata_glsl(
+	const CacheEntryMetadata *meta, size_t size, uint8_t buf[size]
+) {
+	uint8_t data[] = {
+		meta->stage,
+		meta->lang.glsl.version.version / 10,
+		meta->lang.glsl.version.profile,
+	};
+
+	assume(sizeof(data) <= size);
+	memcpy(buf, data, sizeof(data));
+	return sizeof(data);
+}
+
+static size_t shader_cache_serialize_entry_metadata_spirv(
+	const CacheEntryMetadata *meta, size_t size, uint8_t buf[size]
+) {
+	uint8_t data[] = {
+		meta->stage,
+		meta->lang.spirv.target,
+		meta->spirv.compile_options->optimization_level,
+		meta->spirv.compile_options->flags,
+	};
+
+	assume(sizeof(data) <= size);
+	memcpy(buf, data, sizeof(data));
+	return sizeof(data);
+}
+
+static size_t shader_cache_serialize_entry_metadata(
+	const CacheEntryMetadata *meta, size_t size, uint8_t buf[size]
+) {
+
+	switch(meta->lang.lang) {
+		case SHLANG_GLSL:
+			return shader_cache_serialize_entry_metadata_glsl(meta, size, buf);
+
+		case SHLANG_SPIRV:
+			return shader_cache_serialize_entry_metadata_spirv(meta, size, buf);
+
+		default:
+			log_error("Unknown shading language %u", meta->lang.lang);
+			return 0;
+	}
+}
 
 static bool shader_cache_entry_name(
-	const ShaderLangInfo *lang,
-	ShaderStage stage,
-	SPIRVOptimizationLevel optlvl,
+	const CacheEntryMetadata *meta,
 	size_t bufsize,
 	char buf[bufsize]
 ) {
-	// TODO: somehow get rid of this ugly optlvl parameter and generalize external metadata?
+	uint8_t temp[bufsize];
+	size_t meta_size = shader_cache_serialize_entry_metadata(meta, sizeof(temp), temp);
 
-	switch(lang->lang) {
-		case SHLANG_GLSL: {
-			snprintf(buf, bufsize, "glsl_%u_%u_%u", stage, lang->glsl.version.version, lang->glsl.version.profile);
-			return true;
-		}
-
-		case SHLANG_SPIRV: {
-			snprintf(buf, bufsize, "spirv_%u_%u_%u", stage, lang->spirv.target, optlvl);
-			return true;
-		}
-
-		default: {
-			log_error("Unhandled shading language id=%u", lang->lang);
-			return false;
-		}
+	if(UNLIKELY(meta_size == 0)) {
+		return false;
 	}
+
+	const char *const prefixmap[] = {
+		[SHLANG_GLSL - SHLANG_FIRST] = "glsl_",
+		[SHLANG_SPIRV - SHLANG_FIRST] = "spirv_",
+	};
+
+	uint idx = meta->lang.lang - SHLANG_FIRST;
+	assume(idx < sizeof(prefixmap));
+	const char *prefix = prefixmap[idx];
+
+	size_t prefix_size = strlen(prefix);
+	assume(bufsize > prefix_size);
+	memcpy(buf, prefix, prefix_size);
+	buf += prefix_size;
+	bufsize -= prefix_size;
+
+	if(bufsize < meta_size * 2 + 1) {
+		log_error("Filename is too long: buffer size is %zu, need at least %zu",
+			bufsize + prefix_size, (size_t)(meta_size * 2 + 1 + prefix_size));
+		return false;
+	}
+
+	hexdigest(temp, meta_size, buf, bufsize);
+	return true;
 }
 
 bool spirv_compile(const ShaderSource *in, ShaderSource *out, const SPIRVCompileOptions *options) {
@@ -47,7 +118,18 @@ bool spirv_compile(const ShaderSource *in, ShaderSource *out, const SPIRVCompile
 	target_lang.lang = SHLANG_SPIRV;
 	target_lang.spirv.target = options->target;
 
-	if(!shader_cache_entry_name(&target_lang, in->stage, options->optimization_level, sizeof(name), name)) {
+	CacheEntryMetadata m = {
+		.stage = in->stage,
+		.lang = {
+			.lang = SHLANG_SPIRV,
+			.spirv.target = options->target,
+		},
+		.spirv = {
+			.compile_options = options,
+		},
+	};
+
+	if(!shader_cache_entry_name(&m, sizeof(name), name)) {
 		return _spirv_compile(in, out, options);
 	}
 
@@ -77,7 +159,12 @@ bool spirv_compile(const ShaderSource *in, ShaderSource *out, const SPIRVCompile
 bool spirv_decompile(const ShaderSource *in, ShaderSource *out, const SPIRVDecompileOptions *options) {
 	char name[256], hash[SHADER_CACHE_HASH_BUFSIZE];
 
-	if(!shader_cache_entry_name(options->lang, in->stage, 0, sizeof(name), name)) {
+	CacheEntryMetadata m = {
+		.stage = in->stage,
+		.lang = *options->lang,
+	};
+
+	if(!shader_cache_entry_name(&m, sizeof(name), name)) {
 		return _spirv_decompile(in, out, options);
 	}
 
@@ -178,15 +265,21 @@ bool spirv_transpile(const ShaderSource *in, ShaderSource *out, const SPIRVTrans
 		target = options->lang->spirv.target;
 	}
 
+	uint cflags = 0;
+	// Preserve names of declarations.
+	// This can be vital for shader interface matching.
+	cflags |= SPIRV_CFLAG_DEBUG_INFO;
+
+	if(target != SPIRV_TARGET_OPENGL_450) {
+		cflags |= SPIRV_CFLAG_VULKAN_RELAXED;
+	}
+
 	result = spirv_compile(in, &spirv, &(SPIRVCompileOptions) {
 		.target = target,
 		.optimization_level = options->optimization_level,
 		.filename = options->filename,
 		.macros = macros,
-
-		// Preserve names of declarations.
-		// This can be vital for shader interface matching.
-		.debug_info = true,
+		.flags = cflags,
 	});
 
 	if(result) {
