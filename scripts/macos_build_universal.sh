64744d092baa158a5f75b37e829df9068f236cc2
Intel A80486DX2-66 <larry.holst@disroot.org>
2024-07-26 05:10:54 +0300

    scripts: all: put double quotes where they're needed
    Put double quotation marks where they're needed to prevent word splitting of
    strings and paths

--- scripts/macos_build_universal.sh
+++ scripts/macos_build_universal.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-source $(pwd)/.mac_env
+source "$(pwd)/.mac_env"
 
-if [[ -z $1 ]]; then
+if [[ -z "$1" ]]; then
 	printf -- "Usage: macos_build_universal.sh VERSION [--integrity-files]\n\n"
 	exit 0
 fi
 
-meson install -C $MESON_BUILD_ROOT_MACOS_X64
-meson install -C $MESON_BUILD_ROOT_MACOS_AARCH64
+meson install -C "$MESON_BUILD_ROOT_MACOS_X64"
+meson install -C "$MESON_BUILD_ROOT_MACOS_AARCH64"
 
-cp -a $MESON_BUILD_ROOT_MACOS_X64_COMPILED/ $MESON_BUILD_ROOT_MACOS_COMBINED
+cp -a "$MESON_BUILD_ROOT_MACOS_X64_COMPILED/" "$MESON_BUILD_ROOT_MACOS_COMBINED"
 printf -- "Combining x64 and AArch64 binaries...\n\n"
-lipo -create -output $MESON_BUILD_ROOT_MACOS_COMBINED/$TAISEI_BIN_PATH $MESON_BUILD_ROOT_MACOS_X64_COMPILED/$TAISEI_BIN_PATH $MESON_BUILD_ROOT_MACOS_AARCH64_COMPILED/$TAISEI_BIN_PATH
+lipo -create -output "$MESON_BUILD_ROOT_MACOS_COMBINED/$TAISEI_BIN_PATH" "$MESON_BUILD_ROOT_MACOS_X64_COMPILED/$TAISEI_BIN_PATH" "$MESON_BUILD_ROOT_MACOS_AARCH64_COMPILED/$TAISEI_BIN_PATH"
 
-$TAISEI_ROOT/scripts/macos-gen-dmg.py $MAC_BUILD_DIR/compiled/Taisei-$1-universal.dmg $MESON_BUILD_ROOT_MACOS_X64 $MESON_BUILD_ROOT_MACOS_COMBINED $2
+"$TAISEI_ROOT/scripts/macos-gen-dmg.py" "$MAC_BUILD_DIR/compiled/Taisei-$1-universal.dmg" "$MESON_BUILD_ROOT_MACOS_X64" "$MESON_BUILD_ROOT_MACOS_COMBINED" "$2"
