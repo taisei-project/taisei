ec350bdf3e3097852e0674bfb77a69ed37086f5b
Intel A80486DX2-66 <larry.holst@disroot.org>
2024-07-26 05:10:54 +0300

    scripts: all: put double quotes where they're needed
    Put double quotation marks where they're needed to prevent word splitting of
    strings and paths

--- scripts/macos_configure_env.sh
+++ scripts/macos_configure_env.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ -z $1 || -z $2 ]]; then
+if [[ -z "$1" || -z "$2" ]]; then
 	printf -- "Usage: macos_configure_env.sh /path/to/taisei/build /path/to/taisei\n\n"
 	exit 0
 fi
@@ -10,7 +10,7 @@ fi
 printf -- "export MAC_BUILD_DIR=%s\n" "$1" >> "$(pwd)/.mac_env"
 printf -- "export TAISEI_ROOT=%s\n" "$2" >> "$(pwd)/.mac_env"
 
-source $(pwd)/.mac_env
+source "$(pwd)/.mac_env"
 
 printf -- "export MESON_BUILD_ROOT_MACOS_X64=%s/x64\n" "$MAC_BUILD_DIR" >> "$(pwd)/.mac_env"
 printf -- "export MESON_BUILD_ROOT_MACOS_AARCH64=%s/aarch64\n" "$MAC_BUILD_DIR" >> "$(pwd)/.mac_env"
--- scripts/optimize-all-img.sh
+++ scripts/optimize-all-img.sh
@@ -5,4 +5,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit $?
 script_dir="$PWD"
 cd "$opwd" || exit $?
 
-find "$@" -type f -name "*.png" -or -name '*.webp' | parallel -j$(nproc) "$script_dir"/optimize-img.sh
+find "$@" -type f -name "*.png" -or -name '*.webp' | parallel "-j$(nproc)" "$script_dir/optimize-img.sh"
