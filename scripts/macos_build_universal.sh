a0b313e081c72aab3f95ed3d50387c02135426e7
Intel A80486DX2-66 <larry.holst@disroot.org>
2024-07-26 05:06:56 +0300

    scripts: all: guard all `echo` uses
    Use `printf --` instead

--- scripts/macos_build_universal.sh
+++ scripts/macos_build_universal.sh
@@ -5,7 +5,7 @@ set -e
 source $(pwd)/.mac_env
 
 if [[ -z $1 ]]; then
-	echo "Usage: macos_build_universal.sh VERSION [--integrity-files]\n"
+	printf -- "Usage: macos_build_universal.sh VERSION [--integrity-files]\n\n"
 	exit 0
 fi
 
@@ -13,7 +13,7 @@ meson install -C $MESON_BUILD_ROOT_MACOS_X64
 meson install -C $MESON_BUILD_ROOT_MACOS_AARCH64
 
 cp -a $MESON_BUILD_ROOT_MACOS_X64_COMPILED/ $MESON_BUILD_ROOT_MACOS_COMBINED
-echo "Combining x64 and AArch64 binaries...\n"
+printf -- "Combining x64 and AArch64 binaries...\n\n"
 lipo -create -output $MESON_BUILD_ROOT_MACOS_COMBINED/$TAISEI_BIN_PATH $MESON_BUILD_ROOT_MACOS_X64_COMPILED/$TAISEI_BIN_PATH $MESON_BUILD_ROOT_MACOS_AARCH64_COMPILED/$TAISEI_BIN_PATH
 
 $TAISEI_ROOT/scripts/macos-gen-dmg.py $MAC_BUILD_DIR/compiled/Taisei-$1-universal.dmg $MESON_BUILD_ROOT_MACOS_X64 $MESON_BUILD_ROOT_MACOS_COMBINED $2
