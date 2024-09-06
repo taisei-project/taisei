787773a53b51669e7ecae87df5357bc1c96e2a31
Andrei Alexeyev <akari@taisei-project.org>
2024-09-06 02:04:16 +0200

    memory/arena: add snapshot and rollback APIs

--- src/memory/arena.h
+++ src/memory/arena.h
@@ -13,6 +13,7 @@
 
 typedef struct MemArena MemArena;
 typedef struct MemArenaPage MemArenaPage;
+typedef struct MemArenaSnapshot MemArenaSnapshot;
 
 struct MemArena {
 	LIST_ANCHOR(MemArenaPage) pages;
@@ -27,6 +28,11 @@ struct MemArenaPage {
 	size_t size;
 	alignas(alignof(max_align_t)) char data[];
 };
+
+typedef struct MemArenaSnapshot {
+	MemArenaPage *page;
+	size_t page_offset;
+} MemArenaSnapshot;
 
 void marena_init(MemArena *arena, size_t min_size)
 	attr_nonnull_all;
@@ -81,6 +87,12 @@ void *marena_realloc_aligned(MemArena *restrict arena, void *restrict p, size_t 
 	attr_alloc_align(5)
 	attr_nonnull(1)
 	attr_returns_allocated;
+
+MemArenaSnapshot marena_snapshot(MemArena *arena)
+	attr_nonnull_all;
+
+bool marena_rollback(MemArena *restrict arena, const MemArenaSnapshot *restrict snapshot)
+	attr_nonnull_all;
 
 INLINE void *marena_memdup(MemArena *arena, const void *buf, size_t size) {
 	return memcpy(marena_alloc(arena, size), buf, size);
