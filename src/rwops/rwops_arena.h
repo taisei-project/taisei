7a53a6acad85465123676bad8a38ab48da722877
Andrei Alexeyev <akari@taisei-project.org>
2024-09-07 11:11:55 +0200

    rwops/rwops_arena: arena-backed auto-resizing IO buffer

---
+++ src/rwops/rwops_arena.h
@@ -0,0 +1,23 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2024, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2024, Andrei Alexeyev <akari@taisei-project.org>.
+ */
+
+#pragma once
+#include "taisei.h"
+
+#include "memory/arena.h"
+
+#include <SDL3/SDL.h>
+
+typedef struct RWArenaState {
+	MemArena *arena;
+	char *buffer;
+	uint32_t buffer_size;
+	uint32_t io_offset;
+} RWArenaState;
+
+SDL_IOStream *SDL_RWArena(MemArena *arena, uint32_t init_buffer_size, RWArenaState *state);
