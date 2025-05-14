895af91f28f0cba70e68f0344bd5ed12e0157400
Andrei Alexeyev <akari@taisei-project.org>
2025-05-14 20:57:50 +0200

    scripts/macos_setup_universal: remove obsolete ANGLE stuff

--- scripts/macos_setup_universal.sh
+++ scripts/macos_setup_universal.sh
@@ -4,15 +4,7 @@ set -e
 
 source "$(pwd)/.mac_env"
 
-mkdir -p "$MAC_BUILD_DIR/compiled" "$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib" "$MESON_BUILD_ROOT_MACOS_COMBINED"
-
-# combine dylibs
-lipo "$TAISEI_ROOT/angle-compiled/lib/macOS-arm64-dylib/libEGL.dylib" "$TAISEI_ROOT/angle-compiled/lib/macOS-x64-dylib/libEGL.dylib" -output "$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib/libEGL.dylib" -create
-lipo "$TAISEI_ROOT/angle-compiled/lib/macOS-arm64-dylib/libGLESv2.dylib" "$TAISEI_ROOT/angle-compiled/lib/macOS-x64-dylib/libGLESv2.dylib" -output "$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib/libGLESv2.dylib" -create
-
 meson setup \
-	-Dangle_libegl=$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib/libEGL.dylib \
-	-Dangle_libgles=$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib/libGLESv2.dylib \
 	--cross-file "$TAISEI_ROOT/misc/ci/common-options.ini" \
 	--cross-file "$TAISEI_ROOT/misc/ci/forcefallback.ini" \
 	--cross-file "$TAISEI_ROOT/misc/ci/macos-ugly-sdl3-hack.ini" \
@@ -22,8 +14,6 @@ meson setup \
 	"$TAISEI_ROOT" "$@"
 
 meson setup \
-	-Dangle_libegl=$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib/libEGL.dylib \
-	-Dangle_libgles=$TAISEI_ROOT/angle-compiled/lib/macOS-universal-dylib/libGLESv2.dylib \
 	--native-file "$TAISEI_ROOT/misc/ci/common-options.ini" \
 	--native-file "$TAISEI_ROOT/misc/ci/forcefallback.ini" \
 	--native-file "$TAISEI_ROOT/misc/ci/macos-ugly-sdl3-hack.ini" \
