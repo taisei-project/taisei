a741688395165cf2beaaf189170df0f9c42295de
Andrei Alexeyev <akari@taisei-project.org>
2025-08-31 14:18:00 +0200

    memory/backend_mimalloc: force align to at least max_align_t
    This is the expected behavior for standard malloc(). For some reason the
    default configuration underaligns on some systems.

--- src/memory/backend_mimalloc.c
+++ src/memory/backend_mimalloc.c
@@ -10,26 +10,31 @@
 #include "util.h"
 
 #include "../util.h"
+#include "util/miscmath.h"
 
 #include <mimalloc.h>
+
+#define MIN_ALIGNMENT alignof(max_align_t)
 
 void mem_free(void *ptr) {
 	mi_free(ptr);
 }
 
 void *mem_alloc(size_t size) {
-	return mi_calloc(1, size);
+	return mem_alloc_aligned(size, MIN_ALIGNMENT);
 }
 
 void *mem_alloc_array(size_t num_members, size_t size) {
-	return mi_calloc(num_members, size);
+	size_t alloc_size = mem_util_calc_array_size(num_members, size);
+	return mem_alloc_aligned(alloc_size, MIN_ALIGNMENT);
 }
 
 void *mem_realloc(void *ptr, size_t size) {
 	assert_nolog(size > 0);
-	return mi_realloc(ptr, size);
+	return mi_realloc_aligned(ptr, size, MIN_ALIGNMENT);
 }
 
 void *mem_alloc_aligned(size_t size, size_t alignment) {
+	alignment = max(MIN_ALIGNMENT, alignment);
 	return mi_calloc_aligned(1, size, alignment);
 }
