f031c18803dd1885184a63ebf282557e8b1e6969
laochailan <239670+laochailan@users.noreply.github.com>
2025-08-31 14:33:34 -0400

    intl: simplify implementation by reading complete file

--- src/intl/intl.c
+++ src/intl/intl.c
@@ -7,66 +7,168 @@
  */
 
 #include "hashtable.h"
-#include "taisei.h"
-
 #include "intl.h"
+#include "util.h"
+#include "log.h"
+#include "util/miscmath.h"
+#include "vfs/public.h"
+#include <SDL3/SDL_iostream.h>
+
+typedef struct Translation {
+	char *text;
+	char *plural;
+} Translation;
+
+#define HT_SUFFIX                      str2trans
+#define HT_KEY_TYPE                    char*
+#define HT_VALUE_TYPE                  Translation
+#define HT_FUNC_FREE_KEY(key)          mem_free(key)
+#define HT_FUNC_KEYS_EQUAL(key1, key2) (!strcmp(key1, key2))
+#define HT_FUNC_HASH_KEY(key)          htutil_hashfunc_string(key)
+#define HT_FUNC_COPY_KEY(dst, src)     (*(dst) = mem_strdup(src))
+#define HT_KEY_FMT                     "s"
+#define HT_KEY_PRINTABLE(key)          (key)
+#define HT_VALUE_FMT                   "s"
+#define HT_VALUE_PRINTABLE(val)        (none)
+#define HT_KEY_CONST
+#define HT_DECL
+#define HT_IMPL
+#include "hashtable_incproxy.inc.h"
 
 static IntlTextDomain *_current_textdomain = NULL;
 
-void intl_translation_create(IntlTranslation *translation, const char *text, int length) {
-	int textlength = strlen(text);
-	char *buf = memdup(text, length+1);
-		assert(textlength == length);
+typedef struct MoHeader {
+	union {
+		struct {
+			uint32_t magic;
+			uint32_t revision;
+			uint32_t number_of_strings;
+			uint32_t original_strings_offset;
+			uint32_t translated_strings_offset;
+			uint32_t hashtable_offset;
+		};
+		uint32_t u32_array[6];
+	};
+} MoHeader;
 
-	if(textlength != length) {
-		assert(textlength < length);
-		translation->plural = buf;
-		translation->text = buf + textlength + 1;
-	} else {
-		translation->text = buf;
+typedef struct MoStringDescriptor {
+	union {
+		struct {
+			uint32_t length;
+			uint32_t offset;
+		};
+		uint32_t u32_array[2];
+	};
+} MoStringDescriptor;
+
+static void flip_fields(bool flip_endian, uint32_t *ptr, int n) {
+	if(flip_endian) {
+		for(int i = 0; i < n; i++) {
+			ptr[n] = SDL_Swap32(ptr[n]);
+		}
 	}
 }
 
-void intl_translation_destroy(IntlTranslation *translation) {
-	if(!translation->plural) {
-		mem_free(translation->text);
+IntlTextDomain *intl_read_mo(const char* path) {
+	SDL_IOStream *stream = vfs_open(path, VFS_MODE_READ);
+	if(!stream) {
+		log_error("VFS error: %s", vfs_get_error());
+		return NULL;
+	}
+
+	int filesize = SDL_GetIOSize(stream);
+	void *content = mem_alloc(filesize);
+	if(SDL_ReadIO(stream, content, filesize) != filesize) {
+		log_error("%s: error reading MO file: %s", path, SDL_GetError());
+		SDL_CloseIO(stream);
+		return NULL;
+	}
+	SDL_CloseIO(stream);
+
+	MoHeader *header = content;
+	bool flip_endian;
+	if(header->magic == 0x950412de) {
+		flip_endian = false;
+	} else if(header->magic == 0x12de9504) {
+		flip_endian = true;
 	} else {
-		mem_free(translation->plural);
+		log_error("%s is not a MO file: magic number mismatch", path);
+		mem_free(content);
+		return NULL;
 	}
-}
 
-static void translation_copy(IntlTranslation *dest, IntlTranslation *src) {
-	if(!src->plural) {
-		dest->text = mem_strdup(src->text);
-		dest->plural = NULL;
-	} else {
-		int plural_length = src->text - src->plural;
-		int length = strlen(src->text) + 1 + plural_length;
-		dest->plural = memdup(src->plural, length);
-		dest->text = dest->plural + plural_length;
+	flip_fields(flip_endian, header->u32_array, ARRAY_SIZE(header->u32_array));
+
+	if(header->revision != 0) {
+		log_error("%s: Unsupported MO version", path);
+		mem_free(content);
+		return NULL;
 	}
-}
 
-IntlTextDomain *intl_textdomain_new(int number_of_strings, char **original_strings, IntlTranslation* translated_strings) {
-	IntlTextDomain *domain = mem_alloc(sizeof(IntlTextDomain));
-	domain->number_of_strings = number_of_strings;
-	domain->translated_strings = mem_alloc_array(number_of_strings, sizeof(IntlTranslation));
-
-	ht_str2ptr_ts_create(&domain->table);
-	for(int i = 0; i < number_of_strings; i++) {
-		translation_copy(&domain->translated_strings[i], &translated_strings[i]);
-
-		ht_str2ptr_ts_set(&domain->table, original_strings[i], &domain->translated_strings[i]);
+	if(header->original_strings_offset + sizeof(MoStringDescriptor) * header->number_of_strings > filesize
+	|| header->translated_strings_offset + sizeof(MoStringDescriptor) * header->number_of_strings > filesize) {
+		log_error("%s: MO file has nonsense offsets", path);
+		mem_free(content);
+		return NULL;
 	}
+
+	MoStringDescriptor *original_strings = content + header->original_strings_offset;
+	MoStringDescriptor *translated_strings = content + header->translated_strings_offset;
+
+	flip_fields(flip_endian, original_strings[0].u32_array, header->number_of_strings * ARRAY_SIZE(original_strings[0].u32_array));
+	flip_fields(flip_endian, translated_strings[0].u32_array, header->number_of_strings * ARRAY_SIZE(translated_strings[0].u32_array));
+
+
+	int strings_offset = filesize;
+	int max_offset = 0;
+	for(int i = 0; i < header->number_of_strings; i++) {
+		strings_offset = min(strings_offset, translated_strings[i].offset);
+		max_offset = max(max_offset, translated_strings[i].offset + translated_strings[i].length + 1);
+		max_offset = max(max_offset, original_strings[i].offset + original_strings[i].length + 1);
+	}
+	if(max_offset > filesize) {
+		log_error("%s: MO file has nonsense string offsets", path);
+		mem_free(content);
+		return NULL;
+	}
+
+	IntlTextDomain *domain = ALLOC(IntlTextDomain);
+	domain->strings = memdup(content + strings_offset, filesize - strings_offset);
+	domain->table = ht_str2trans_new();
+
+	char *strcontent = content;
+
+	for(int i = 0; i < header->number_of_strings; i++) {
+		Translation t;
+		MoStringDescriptor *ostr = &original_strings[i];
+		MoStringDescriptor *tstr = &translated_strings[i];
+
+		if(strcontent[ostr->offset + ostr->length] != '\0'
+		|| strcontent[tstr->offset + tstr->length] != '\0') {
+			log_error("%s: MO string missing NUL termination", path);
+			intl_textdomain_destroy(domain);
+			mem_free(content);
+			return NULL;
+		}
+		int textlen = strlen(&domain->strings[translated_strings[i].offset-strings_offset]);
+		if(textlen != translated_strings[i].length) {
+			t.plural = &domain->strings[tstr->offset - strings_offset];
+			t.text = t.plural + textlen + 1;
+		} else {
+			t.text = &domain->strings[tstr->offset - strings_offset];
+		}
+
+		ht_str2trans_set(domain->table, &strcontent[ostr->offset], t);
+	}
+
+	mem_free(content);
 	return domain;
 }
 
 void intl_textdomain_destroy(IntlTextDomain *domain) {
-	for(int i = 0; i < domain->number_of_strings; i++) {
-		intl_translation_destroy(&domain->translated_strings[i]);
-	}
-	mem_free(domain->translated_strings);
-	ht_str2ptr_ts_destroy(&domain->table);
+	ht_str2trans_destroy(domain->table);
+	mem_free(domain->table);
+	mem_free(domain->strings);
 }
 
 void intl_set_textdomain(IntlTextDomain *domain) {
@@ -78,11 +180,12 @@ const char *intl_gettext(IntlTextDomain *domain, const char *msgid) {
 		return msgid;
 	}
 
-	IntlTranslation *result = ht_str2ptr_ts_get(&domain->table, msgid, NULL);
-	if(!result) {
+	Translation no_translation = {};
+	Translation result = ht_str2trans_get(domain->table, msgid, no_translation);
+	if(!result.text) {
 		return msgid;
 	}
-	return result->text;
+	return result.text;
 }
 
 const char *intl_ngettext(IntlTextDomain *domain, const char *msgid1, const char *msgid2, unsigned long int n) {
@@ -90,12 +193,16 @@ const char *intl_ngettext(IntlTextDomain *domain, const char *msgid1, const char
 		return n == 1 ? msgid1 : msgid2;
 	}
 
-	IntlTranslation *result = ht_str2ptr_ts_get(&domain->table, msgid1, NULL);
-	if(!result) {
+	Translation no_translation = {};
+	Translation result = ht_str2trans_get(domain->table, msgid1, no_translation);
+	if(!result.plural) {
+		log_fatal("i18n: plural original string '%s':'%s' has singular-only translation '%s'", msgid1, msgid2, result.text);
+	}
+	if(!result.text) {
 		return n == 1 ? msgid1 : msgid2;
 	}
 
-	return n == 1 ? result->text : result->plural;
+	return n == 1 ? result.text : result.plural;
 }
 
 const char *gettext(const char *msgid) {
