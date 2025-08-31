a52f56286ae730e0e00778d4160b5ac5a39c8bc7
laochailan <239670+laochailan@users.noreply.github.com>
2025-08-31 16:17:50 -0400

    i18n: add config setting for language

--- src/resource/i18n.c
+++ src/resource/i18n.c
@@ -35,6 +35,7 @@ static void i18n_load(ResourceLoadState *st) {
 
 static void i18n_unload(void *domain) {
 	intl_textdomain_destroy(domain);
+	mem_free(domain);
 }
 
 ResourceHandler i18n_res_handler = {
@@ -50,9 +51,67 @@ ResourceHandler i18n_res_handler = {
 	},
 };
 
+static int fuzzy_match_system_locale(int n, const char *languages[n]) {
+	int count;
+	SDL_Locale **locales = SDL_GetPreferredLocales(&count);
+
+	int chosen = -1;
+	for(int i = 0; i < count; i++) {
+		char *locale_str = strfmt("%s_%s", locales[i]->language, locales[i]->country);
+
+		for(int j = 0; j < n; j++) {
+			// complete match: good to go
+			if(strcmp(languages[j], locale_str) == 0) {
+				chosen = j;
+				break;
+			}
+
+			// only languages match: ok but move on
+			if(strncmp(languages[j], locales[i]->language, strlen(locales[i]->language)) == 0) {
+				chosen = j;
+			}
+		}
+		mem_free(locale_str);
+	}
+
+	SDL_free(locales);
+	return chosen;
+}
+
+void i18n_set_textdomain_from_string(const char *name) {
+	const char *languages[] = TAISEI_BUILDCONF_LANGUAGES;
+	int chosen = -1;
+
+	if(strcmp(name, "system") == 0) {
+		chosen = fuzzy_match_system_locale(ARRAY_SIZE(languages), languages);
+	} else if(strcmp(name, "en") == 0) {
+		chosen = -1; // use builtin strings
+	} else {
+		for(int i = 0; i < ARRAY_SIZE(languages); i++) {
+			if(strcmp(name, languages[i]) == 0) {
+				chosen = i;
+				break;
+			}
+		}
+		if(chosen < 0) {
+			log_error("No translation for %s available.", name);
+		}
+	}
+
+	if(chosen >= 0) {
+		intl_set_textdomain(res_i18n(languages[chosen]));
+	} else {
+		intl_set_textdomain(NULL);
+	}
+}
+
 void i18n_init(ResourceGroup *rg) {
-	res_group_preload(rg, RES_I18N, RESF_DEFAULT,
-		"zh_CN",
-	NULL);
-	intl_set_textdomain(res_i18n("zh_CN"));
+	const char *languages[] = TAISEI_BUILDCONF_LANGUAGES;
+	for(int i = 0; i < ARRAY_SIZE(languages); i++) {
+		res_group_preload(rg, RES_I18N, RESF_DEFAULT,
+			languages[i],
+		NULL);
+	}
+
+	i18n_set_textdomain_from_string(config_get_str(CONFIG_LANGUAGE));
 }
