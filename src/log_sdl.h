1d3252c8ddfb58dfd2860ea1c3a19ec6ac508315
Andrei Alexeyev <akari@taisei-project.org>
2026-02-19 06:18:10 +0100

    main: move SDL log hookup into separate file

---
+++ src/log_sdl.h
@@ -0,0 +1,14 @@
+/*
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ * ---
+ * Copyright (c) 2011-2026, Lukas Weber <laochailan@web.de>.
+ * Copyright (c) 2012-2026, Andrei Alexeyev <akari@taisei-project.org>.
+ */
+
+#pragma once
+#include "taisei.h"
+
+#include <SDL3/SDL_log.h>
+
+void log_sdl_init(SDL_LogPriority prio);
