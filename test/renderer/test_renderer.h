1a7d2d4fc68d7ef32538dd75b44e487ade07056b
Andrei Alexeyev <akari@taisei-project.org>
2026-02-19 06:19:04 +0100

    test: add test_common.h

--- test/renderer/test_renderer.h
+++ test/renderer/test_renderer.h
@@ -2,45 +2,14 @@
 #include "taisei.h"
 
 #include "config.h"
-#include "events.h"
 #include "log.h"
 #include "memory/scratch.h"
 #include "renderer/api.h"
 #include "resource/iqm_loader/iqm_loader.h"
-#include "rwops/rwops_stdiofp.h"
 #include "util/compat.h"
 #include "video.h"
 
-#include <locale.h>
-
-static void test_init_log(void) {
-	log_init(LOG_ALL);
-	log_add_output(LOG_ALL, SDL_RWFromFP(stderr, false), log_formatter_console);
-}
-
-static void test_init_sdl(void) {
-	mem_install_sdl_callbacks();
-
-	if(!SDL_Init(SDL_INIT_EVENTS)) {
-		log_fatal("SDL_Init() failed: %s", SDL_GetError());
-	}
-
-	events_init();
-}
-
-static void test_init_basic(void) {
-	setlocale(LC_ALL, "C");
-	thread_init();
-	test_init_log();
-	test_init_sdl();
-}
-
-static void test_shutdown_basic(void) {
-	log_shutdown();
-	events_shutdown();
-	thread_shutdown();
-	SDL_Quit();
-}
+#include "test_common.h"
 
 static ShaderObject *test_renderer_load_glsl(ShaderStage stage, const char *src) {
 	// TODO: This is mostly copypasted from resource/shader_object; add a generic API for this
