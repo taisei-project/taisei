2843799fd6c9c4482a1b76a5dbda78bd9201df24
Andrei Alexeyev <akari@taisei-project.org>
2026-02-19 01:22:42 +0100

    vfs/platform_paths/cache_apple: another attempt to work around macOS header clashes

--- src/vfs/platform_paths/cache_apple.m
+++ src/vfs/platform_paths/cache_apple.m
@@ -6,16 +6,20 @@
  * Copyright (c) 2012-2019, Andrei Alexeyev <akari@taisei-project.org>.
 */
 
-#include "platform_paths.h"
-#include "util.h"
-#include "../syspath_public.h"
-
 // HACK: work around name clashes with macOS SDK
 #define Rect __apple_Rect
 #define thread_create __apple_thread_create
 #define ThreadID __apple_ThreadID
 
 #include <Foundation/Foundation.h>
+
+#undef Rect
+#undef thread_create
+#undef ThreadID
+
+#include "platform_paths.h"
+#include "util.h"
+#include "../syspath_public.h"
 
 const char *vfs_platformpath_cache(void) {
 	static char *cached;
