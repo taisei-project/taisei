5410e9710def17264be3adc4ffc4ab1f9b6a540f
Andrei Alexeyev <akari@taisei-project.org>
2026-06-27 07:08:26 +0200

    mkbasis,shaders: contort cubemaps into the right shape and nuke fixCubeCoord()
    Requires rebuilding of all cubemap textures, follow-up in next commit

--- resources/00-taisei.pkgdir/shader/stage6_sky.frag.glsl
+++ resources/00-taisei.pkgdir/shader/stage6_sky.frag.glsl
@@ -7,7 +7,7 @@ VARYING(3) vec3 posRaw;
 UNIFORM(1) samplerCube skybox;
 
 void main(void) {
-	fragColor = texture(skybox, fixCubeCoord(posRaw));
+	fragColor = texture(skybox, posRaw);
 
 	fragColor.r = linear_to_srgb(fragColor.r);
 	fragColor.g = linear_to_srgb(fragColor.g);
